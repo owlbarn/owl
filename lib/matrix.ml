2d90fad881f8e59d4e41eee1c275d11a7293d1c8
Liang Wang <ryanrhymes@gmail.com>
2016-06-23 01:02:34 +0100

    coding ...

--- lib/matrix.ml
+++ lib/matrix.ml
@@ -6,6 +6,8 @@ module BA = Bigarray
 module LL = Lacaml.D
 module LM = LL.Mat
 
+type area = { a : int; b : int; c : int; d : int }
+
 module Matrix = struct
 
   (* matrix creation operations *)
@@ -72,19 +74,55 @@ module Matrix = struct
 
   (* matrix manipulations *)
 
-  let concat_vertical = None
-  let ( @|| ) = concat_vertical
-
-  let concat_horizontal = None
-  let ( @= ) = concat_horizontal
-
-  let transpose x =
-    LL.Mat.transpose_copy x
-
   let size (x : LM.t) =
     let m = BA.Array2.dim1 x in
     let n = BA.Array2.dim2 x in
     m, n
+
+  let area a b c d = { a = a; b = b; c = c; d= d }
+
+  let area_of x =
+    let m, n = size x in
+    { a = 0; b = 0; c = m - 1; d= n - 1 }
+
+  let assert_equal_area r1 r2 =
+    assert ((r1.c-r1.a = r2.c-r2.a) && (r1.d-r1.b = r2.d-r2.b))
+
+  let copy_area_to x1 r1 x2 r2 =
+    LL.lacpy ~ar:(r1.a+1) ~ac:(r1.b+1) ~br:(r2.a+1) ~bc:(r2.b+1)
+      ~m:(r1.c-r1.a+1) ~n:(r1.d-r1.b+1) ~b:x2 x1
+
+  let copy_to x1 x2 =
+    copy_area_to x1 (area_of x1) x2 (area_of x2)
+
+  let ( >> ) = copy_to
+
+  let ( << ) x1 x2 = copy_to x2 x1
+
+  let concat_vertical x1 x2 =
+    let r1, r2 = area_of x1, area_of x2 in
+    let _ = assert (r1.d = r2.d) in
+    let r3 = area (r1.c + 1) 0 (r1.c + r2.c + 1) r1.d in
+    let x3 = zeros (r1.c + r2.c + 2) (r1.d + 1) in
+    let _ = copy_area_to x1 r1 x3 r1 in
+    let _ = copy_area_to x2 r2 x3 r3 in
+    x3
+
+  let ( @|| ) = concat_vertical
+
+  let concat_horizontal x1 x2 =
+    let r1, r2 = area_of x1, area_of x2 in
+    let _ = assert (r1.c = r2.c) in
+    let r3 = area 0 (r1.d + 1) r1.c (r1.d + r2.d + 1) in
+    let x3 = zeros (r1.c + 1) (r1.d + r2.d + 2) in
+    let _ = copy_area_to x1 r1 x3 r1 in
+    let _ = copy_area_to x2 r2 x3 r3 in
+    x3
+
+  let ( @= ) = concat_horizontal
+
+  let transpose x =
+    LL.Mat.transpose_copy x
 
   let shape = size
 
@@ -96,20 +134,24 @@ module Matrix = struct
 
   let row_num x = snd (size x)
 
-  let part ~a1 ~b1 ~a2 ~b2 x =
-    LL.lacpy ~ar:(a1+1) ~ac:(b1+1) ~m:(a2-a1+1) ~n:(b2-b1+1) x
+  let part x r =
+    let a, b, c, d = r.a, r.b, r.c, r.d in
+    LL.lacpy ~ar:(a+1) ~ac:(b+1) ~m:(c-a+1) ~n:(d-b+1) x
 
   let duplicate x =
     let m, n = size x in
-    part 0 0 (m - 1) (n - 1) x
+    let r = area 0 0 (m - 1) (n - 1) in
+    part x r
 
   let col x i =
     let m, n = size x in
-    part 0 i (m - 1) i x
+    let r = area 0 i (m - 1) i in
+    part x r
 
   let row x i =
     let m, n = size x in
-    part i 0 i (n - 1) x
+    let r = area i 0 i (n - 1) in
+    part x r
 
   let cols = None
 
