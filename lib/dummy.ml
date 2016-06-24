2745043544b0dd0387cd146f360a0508d811cf28
Liang Wang <ryanrhymes@gmail.com>
2016-06-24 11:27:31 +0100

    coding ...

--- lib/dummy.ml
+++ lib/dummy.ml
@@ -1,8 +1,12 @@
 module M = Matrix.Matrix
 
 let test_01 () =
-  let x = M.random 10000 1000 in
+  let x = M.random 10000 10000 in
+  M.sum x
+
+let test_02 () =
+  let x = M.random 10000 10000 in
   M.(x *$ 10.)
 
 let _ = print_endline "matrix test";
-  test_01 ()
+  test_02 ()
---
+++ lib/test/testpy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+def test_01 ():
+    x = np.random.rand (10000, 10000)
+    y = x * 10
+    pass
+
+if __name__ == "__main__":
+    print "test python"
+    test_01 ()
