9f107cf36eb53bc4ffef4b83d20745330d91e343
Liang Wang <ryanrhymes@gmail.com>
2016-06-27 18:02:15 +0100

    add filter and map array functions to utils

--- lib/learn.ml
+++ lib/learn.ml
@@ -13,7 +13,7 @@ module Cluster = struct
     let cpts0 = draw_rows ~replacement:true x c in
     let cpts1 = zeros c (col_num x) in
     let assignment = Array.make (row_num x) (0, max_float) in
-    try for counter = 1 to 100 do
+    let _ = try for counter = 1 to 100 do
     Printf.printf "iteration %i ...\n" counter; flush stdout;
     iteri_rows (fun i v ->
       iteri_rows (fun j u ->
@@ -22,13 +22,13 @@ module Cluster = struct
       ) cpts0
     ) x;
     iteri_rows (fun j u ->
-      let l = UT.filter_array (fun i y -> fst y = j, i + 1) assignment in
+      let l = UT.filteri_array (fun i y -> fst y = j, i + 1) assignment in
       let z = average_rows (rows x (Array.to_list l)) in
       let _ = copy_to_row z j cpts1 in ()
     ) cpts0;
     if cpts0 =@ cpts1 then failwith "converged" else ignore (cpts0 << cpts1)
-    done; assignment
-    with exn -> assignment
+    done with exn -> () in
+    cpts1, UT.map_array fst assignment
 
 end;;
 
--- lib/utils.ml
+++ lib/utils.ml
@@ -17,12 +17,24 @@ let sublist a b l =
   for i = a to b do r.(i - a) <- (List.nth l (i - a)) done;
   Array.to_list r
 
-let filter_array f x =
-  let r = Array.make (Array.length x) 0 and c = ref 0 in
+let filteri_array f x =
+  let atype = snd (f 0 x.(0)) in
+  let r = Array.make (Array.length x) atype and c = ref 0 in
   for i = 0 to Array.length x - 1 do
     let y, z = f i x.(i) in
     if y = true then (r.(!c) <- z; c := !c + 1)
   done;
   Array.sub r 0 !c
+
+let filter_array f x = filteri_array (fun _ y -> f y) x
+
+let mapi_array f x =
+  let atype = f 0 x.(0) in
+  let r = Array.make (Array.length x) atype in
+  for i = 0 to Array.length x - 1 do
+    r.(i) <- f i x.(i)
+  done; r
+
+let map_array f x = mapi_array (fun _ y -> f y) x
 
 let log s = None
