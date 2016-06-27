880c8903fdb406869149c4436cff5b41da7218fa
Liang Wang <ryanrhymes@gmail.com>
2016-06-27 18:20:46 +0100

    add kmeans data

--- lib/test_kmeans.ml
+++ lib/test_kmeans.ml
@@ -1,30 +1,12 @@
-module M = Matrix.Matrix
-module L = Learn.Cluster
-
-let test_op s c op =
-  let ttime = ref 0. in
-  for i = 1 to c do
-    let t0 = Unix.gettimeofday () in
-    let _ = op () in
-    let t1 = Unix.gettimeofday () in
-    ttime := !ttime +. (t1 -. t0)
-  done;
-  let _ = ttime := !ttime /. (float_of_int c) in
-  Printf.printf "| %s :\t %.4fs \n" s !ttime;
-  flush stdout
-
-let test_kmeans () =
-  let z3 = M.random 10000 8 in
-  L.kmeans z3 3
+module MM = Matrix.Matrix
+module ML = Learn.Cluster
 
 let _ =
-  let _ = Random.self_init () in
-  let m, n = 5000, 5000 and c = 1 in
+  let x = MM.load "data.txt" in
+  let m, n = MM.shape x in
   print_endline (Bytes.make 60 '+');
-  Printf.printf "| test matrix size: %i x %i    exps: %i\n" m n c;
+  Printf.printf "| test matrix size: %i x %i\n" m n;
   print_endline (Bytes.make 60 '-');
-  (* let x, y = (M.random m n), (M.random m n) in
-  test_op "pretty print    " c (fun () -> M.pprint x);
-  test_op "load matrix    " c (fun () -> M.load "zmatrix.txt"); *)
+  let centers, _ = ML.kmeans x 2 in
+  MM.pprint centers;
   print_endline (Bytes.make 60 '+');
-  test_kmeans ();
