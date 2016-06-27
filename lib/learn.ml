918fee78bfdf0c634b091c49ba985224ee2e3e0c
Liang Wang <ryanrhymes@gmail.com>
2016-06-27 15:16:19 +0100

    coding ...

--- lib/learn.ml
+++ lib/learn.ml
@@ -11,7 +11,7 @@ let print_list l = List.iter (print_int) l
 module Cluster = struct
 
   let kmeans x c = let open M in
-    let cpts0 = draw_rows ~replacement:false x c in
+    let cpts0 = draw_rows ~replacement:true x c in
     let cpts1 = zeros c (col_num x) in
     let assignment = Array.make (row_num x) (0, max_float) in
     for counter = 1 to 100 do
@@ -24,8 +24,11 @@ module Cluster = struct
     ) x;
     iteri_rows (fun j u ->
       let l = Array.to_list assignment in
+            let _ = print_endline "here" in
       let l = List.filter (fun y -> fst y = j) l in
+            let _ = print_endline "heree" in
       let l = List.map (fun y -> fst y) l in
+            let _ = print_endline "hereee" in
       let z = average_rows (rows x l) in
       let _ = copy_to_row z j cpts1 in ()
     ) cpts0;
