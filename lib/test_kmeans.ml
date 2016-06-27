331e65ff15572be27a4b8c53cd77de5eda62d128
Liang Wang <ryanrhymes@gmail.com>
2016-06-27 18:24:45 +0100

    add test code

--- lib/test_kmeans.ml
+++ lib/test_kmeans.ml
@@ -2,11 +2,10 @@ module MM = Matrix.Matrix
 module ML = Learn.Cluster
 
 let _ =
-  let x = MM.load "data.txt" in
+  let x = MM.load "test_kmeans.data" in
   let m, n = MM.shape x in
   print_endline (Bytes.make 60 '+');
   Printf.printf "| test matrix size: %i x %i\n" m n;
   print_endline (Bytes.make 60 '-');
   let centers, _ = ML.kmeans x 2 in
-  MM.pprint centers;
-  print_endline (Bytes.make 60 '+');
+  MM.pprint centers
