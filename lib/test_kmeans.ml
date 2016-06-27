0b0782f5d599a8e0baae62d93af2f879f753067c
Liang Wang <ryanrhymes@gmail.com>
2016-06-27 18:33:00 +0100

    tweak test code

--- lib/test_kmeans.ml
+++ lib/test_kmeans.ml
@@ -7,5 +7,7 @@ let _ =
   print_endline (Bytes.make 60 '+');
   Printf.printf "| test matrix size: %i x %i\n" m n;
   print_endline (Bytes.make 60 '-');
-  let centers, _ = ML.kmeans x 2 in
-  MM.pprint centers
+  print_endline "test on input data ...";
+  let centers, _ = ML.kmeans x 2 in MM.pprint centers;
+  print_endline "test on random data ...";
+  let x = MM.random 100000 8 in ML.kmeans x 3
