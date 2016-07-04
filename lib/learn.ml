eac7bf6246d0d45607dbafa08d23b96db5bd7040
Liang Wang <ryanrhymes@gmail.com>
2016-07-04 23:33:35 +0100

    move kmeans out of cluster module

--- lib/learn.ml
+++ lib/learn.ml
@@ -6,29 +6,24 @@
 module MM = Matrix.Dense
 module UT = Utils
 
-module Cluster = struct
 
-  let kmeans x c = let open MM in
-    let cpts0 = draw_rows x c in
-    let cpts1 = zeros c (col_num x) in
-    let assignment = Array.make (row_num x) (0, max_float) in
-    let _ = try for counter = 1 to 100 do
-    Printf.printf "iteration %i ...\n" counter; flush stdout;
-    iteri_rows (fun i v ->
-      iteri_rows (fun j u ->
-        let e = sum((v -@ u) **@ 2.) in
-        if e < snd assignment.(i) then assignment.(i) <- (j, e)
-      ) cpts0
-    ) x;
+let kmeans x c = let open MM in
+  let cpts0 = draw_rows x c in
+  let cpts1 = zeros c (col_num x) in
+  let assignment = Array.make (row_num x) (0, max_float) in
+  let _ = try for counter = 1 to 100 do
+  Printf.printf "iteration %i ...\n" counter; flush stdout;
+  iteri_rows (fun i v ->
     iteri_rows (fun j u ->
-      let l = UT.filteri_array (fun i y -> fst y = j, i) assignment in
-      let z = average_rows (rows x l) in
-      let _ = copy_row_to z cpts1 j in ()
-    ) cpts0;
-    if cpts0 =@ cpts1 then failwith "converged" else ignore (cpts0 << cpts1)
-    done with exn -> () in
-    cpts1, UT.map_array fst assignment
-
-end;;
-
-module C = Cluster;;
+      let e = sum((v -@ u) **@ 2.) in
+      if e < snd assignment.(i) then assignment.(i) <- (j, e)
+    ) cpts0
+  ) x;
+  iteri_rows (fun j u ->
+    let l = UT.filteri_array (fun i y -> fst y = j, i) assignment in
+    let z = average_rows (rows x l) in
+    let _ = copy_row_to z cpts1 j in ()
+  ) cpts0;
+  if cpts0 =@ cpts1 then failwith "converged" else ignore (cpts0 << cpts1)
+  done with exn -> () in
+  cpts1, UT.map_array fst assignment
