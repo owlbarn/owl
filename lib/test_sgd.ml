7f0414e4489ffe9bb552374e8c30419e640a49ea
Liang Wang <ryanrhymes@gmail.com>
2016-07-06 18:28:58 +0100

    coding ...

--- lib/test_sgd.ml
+++ lib/test_sgd.ml
@@ -1,9 +1,14 @@
 module MX = Matrix.Dense
 module LL = Learn
 
+let centerise x =
+  let open MX in
+  let v = average_rows x in
+  map_by_row (fun x -> x -@ v) x
+
 let test () =
   let p = MX.uniform_int 4 3 in
-  let x = MX.uniform 100 4 in
+  let x = centerise (MX.uniform 100 4) in
   let y = MX.(x $@ p) in
   let q = MX.uniform_int 4 3 in
   let p' = LL.sgd ~r:LL.l2 q x y in
