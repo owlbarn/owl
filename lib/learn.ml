4c7bded50567e83853e526dcbd2c80a54c208ba0
Liang Wang <ryanrhymes@gmail.com>
2016-07-07 15:07:08 +0100

    change function names

--- lib/learn.ml
+++ lib/learn.ml
@@ -63,7 +63,7 @@ let l2 p = MX.(0.5 $* (average_rows (p *@ p)))
 let l2_grad p = p
 
 (** [ Elastic net regularisation and its gradient
-  "a" is l1 ration ]  *)
+  "a" is the weight on l1 regularisation term. ]  *)
 let elastic a x = MX.(a $* (l1 x) +@ ((1. -. a) $* (l2 x)))
 
 let elastic_grad a x =
@@ -71,12 +71,28 @@ let elastic_grad a x =
   let g2 = l2_grad x in
   MX.(a $* g1 +@ (a $* g2))
 
-(** [ No regularisation ]  *)
+(** [ No regularisation and its gradient ]  *)
 let noreg x = MX.(zeros 1 (col_num x))
 
 let noreg_grad x = MX.(zeros (row_num x) (col_num x))
 
+
 (* Loss functions *)
+
+(** [ least square loss function ]  *)
+let square_loss y y' =
+  let open MX in
+  average_rows ((y' -@ y) **@ 2.)
+  (*if a = 0. then l else l +$ (a *. (r p))*)
+
+let square_grad x y y' =
+  let open MX in
+  let z = y' -@ y in
+  mapi_by_col ~d:(col_num x)
+  (fun i v ->
+    let k = mapi_by_row ~d:(col_num x) (fun j u -> u *$ v.{j,0}) x in
+    transpose (average_rows k)
+  ) z
 
 (** [ hinge loss function ]  *)
 let hinge_loss y y' =
@@ -96,28 +112,6 @@ let softmax_loss y y' = None
 let softmax_grad y y' = None
 
 let log_loss y y' = None
-
-
-(** [
-  least square loss function for testing
-  p is the model parameters.
-  y' is the prediction.
-  y is the labeled data.
-]  *)
-
-let leastsquare_loss y y' =
-  let open MX in
-  average_rows ((y' -@ y) **@ 2.)
-  (*if a = 0. then l else l +$ (a *. (r p))*)
-
-let leastsquare_grad x y y' =
-  let open MX in
-  let z = y' -@ y in
-  mapi_by_col ~d:(col_num x)
-  (fun i v ->
-    let k = mapi_by_row ~d:(col_num x) (fun j u -> u *$ v.{j,0}) x in
-    transpose (average_rows k)
-  ) z
 
 (* learning rate scheduling *)
 
@@ -167,9 +161,9 @@ let _sgd_basic b s t l g r o a p x y =
 (** [
   wrapper for _sgd_basic fucntion
 ]  *)
-let _sgd ?(b=1) ?(s=0.1) ?(t=0.00001) ?(l=leastsquare_loss) ?(g=leastsquare_grad) ?(r=noreg) ?(o=noreg_grad) ?(a=0.) p x y = _sgd_basic b s t l g r o a p x y
+let _sgd ?(b=1) ?(s=0.1) ?(t=0.00001) ?(l=square_loss) ?(g=square_grad) ?(r=noreg) ?(o=noreg_grad) ?(a=0.) p x y = _sgd_basic b s t l g r o a p x y
 
-let sgd ?(b=1) ?(s=0.1) ?(t=0.00001) ?(l=leastsquare_loss) ?(g=leastsquare_grad) ?(r=noreg) ?(o=noreg_grad) ?(a=0.0001) p x y = _sgd_basic b s t l g r o a p x y
+let sgd ?(b=1) ?(s=0.1) ?(t=0.00001) ?(l=square_loss) ?(g=square_grad) ?(r=noreg) ?(o=noreg_grad) ?(a=0.0001) p x y = _sgd_basic b s t l g r o a p x y
 
 
 (* TODO: step size scheduling needs to be implemented *)
