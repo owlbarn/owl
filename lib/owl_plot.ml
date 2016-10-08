5fcaeb94711b3b2fdb0b43607e49cb1d5481294e
Liang Wang <ryanrhymes@gmail.com>
2016-10-08 13:43:13 +0100

    testing plplot

--- lib/owl_plot.ml
+++ lib/owl_plot.ml
@@ -9,7 +9,65 @@ open Plplot
 
 module MX = Owl_dense
 
+(* types in plot module *)
+
 type dsmat = Owl_dense.dsmat
+
+type device = AQT | PNG | PDF | SVG
+
+type plot_typ = {
+  mutable title : string;
+  mutable xlabel : string;
+  mutable ylabel : string;
+  mutable xrange : float option * float option;
+  mutable yrange : float option * float option;
+}
+
+(* module functions to simplify plotting *)
+
+let set_device d =
+  match d with
+  | AQT -> plsdev "aqt"
+  | PNG -> plsdev "png"
+  | PDF -> plsdev "pdf"
+  | SVG -> plsdev "svg"
+
+let create () = {
+  title = "";
+  xlabel = "x";
+  ylabel = "y";
+  xrange = (None, None);
+  yrange = (None, None);
+}
+
+let set_title h s = h.title <- s
+
+let set_xlabel h s = h.xlabel <- s
+
+let set_ylabel h s = h.ylabel <- s
+
+let set_xrange h a b = h.xrange <- (a, b)
+
+let set_yrange h a b = h.yrange <- (a, b)
+
+let _update_range r x =
+  match r with
+  | Some a, None -> a, Owl_stats.max x
+  | None, Some b -> Owl_stats.min x, b
+  | Some a, Some b -> a, b
+  | None, None -> Owl_stats.minmax x
+
+let my_plot h x y =
+  let x = MX.to_array x in
+  let y = MX.to_array y in
+  let _ = plsfnam "default.pdf" in
+  let _ = plinit () in
+  let xmin, xmax = _update_range h.xrange x in
+  let ymin, ymax = _update_range h.yrange y in
+  let _ = plenv xmin xmax ymin ymax 0 0 in
+  let _ = pllab h.xlabel h.ylabel h.title in
+  let _ = plline x y in
+  plend ()
 
 let plot x y =
   let x = MX.to_array x in
--- lib/owl_plot.mli
+++ lib/owl_plot.mli
@@ -2,6 +2,32 @@
 
 type dsmat = Owl_dense.dsmat
 
+type device = AQT | PNG | PDF | SVG
+
+type plot_typ = {
+  mutable title : string;
+  mutable xlabel : string;
+  mutable ylabel : string;
+  mutable xrange : float option * float option;
+  mutable yrange : float option * float option;
+}
+
+val set_device : device -> unit
+
+val create : unit -> plot_typ
+
+val set_title : plot_typ -> string -> unit
+
+val set_xlabel : plot_typ -> string -> unit
+
+val set_ylabel : plot_typ -> string -> unit
+
+val set_xrange : plot_typ -> float option -> float option -> unit
+
+val set_yrange : plot_typ -> float option -> float option -> unit
+
+val my_plot : plot_typ -> dsmat -> dsmat -> unit
+
 val plot : dsmat -> dsmat -> unit
 
 val plot_fun : (float -> float) -> float -> float -> unit
