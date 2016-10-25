6d67334d4570529226f34d970883521c9bfee5b9
Liang Wang <ryanrhymes@gmail.com>
2016-10-25 15:11:33 +0100

    experiment

--- lib/owl_types.ml
+++ lib/owl_types.ml
@@ -136,10 +136,11 @@ module Complex_sparse = struct
 
   let gsl_spmatrix_alloc = foreign "gsl_spmatrix_alloc" (int @-> int @-> returning (ptr spmat_struct))
   let gsl_spmatrix_alloc_nzmax = foreign "gsl_spmatrix_alloc_nzmax" (int @-> int @-> int @-> int @-> returning (ptr spmat_struct))
+(*
   let gsl_spmatrix_set = foreign "gsl_spmatrix_set" (ptr spmat_struct @-> int @-> int @-> complex64 @-> returning int)
   let gsl_spmatrix_get = foreign "gsl_spmatrix_get" (ptr spmat_struct @-> int @-> int @-> returning complex64)
   let gsl_spmatrix_set_zero = foreign "gsl_spmatrix_set_zero" (ptr spmat_struct @-> returning int)
-
+*)
 end
 
 
