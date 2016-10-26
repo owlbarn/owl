f0f2d21c3c581d0af34f9d8da279f43f3af35743
Liang Wang <ryanrhymes@gmail.com>
2016-10-26 23:47:01 +0100

    add header

--- lib/ffi_bindings.ml
+++ lib/ffi_bindings.ml
@@ -1,3 +1,9 @@
+(*
+ * OWL - an OCaml math library for scientific computing
+ * Copyright (c) 2016 Liang Wang <liang.wang@cl.cam.ac.uk>
+ *
+ *)
+
 open Ctypes
 
 module Bindings (F : Cstubs.FOREIGN) = struct
