259fbcd7aaced30c621e611b5ae003f062d160eb
Liang Wang <ryanrhymes@gmail.com>
2016-11-03 21:55:39 +0000

    add fft function

--- lib/owl_fft.ml
+++ lib/owl_fft.ml
@@ -6,19 +6,20 @@
 
 let _fft_real_rad2 x =
   let open Gsl_fft in
-  let y = Owl_dense_real.to_array x in
-  let z = { layout = Real; data = y } in
-  Gsl_fft.Real.transform_rad2 z;
-  let d = unpack z in
-  let _, n = Owl_dense_real.shape x in
-  let z = Owl_dense_complex.empty 1 n in
-  print_int n;
-  for j = 0 to n - 1 do
-    let i = 2 * j in
-    let re, im = d.(i), d.(i + 1) in
-    Owl_dense_complex.set z 0 j Complex.({re; im})
-  done;
-  z
+  let m, n = Owl_dense_real.shape x in
+  let y = Owl_dense_complex.empty m n in
+  Owl_dense_real.iteri_rows (fun i v ->
+    let v = Owl_dense_real.to_array v in
+    let z = { layout = Real; data = v } in
+    Gsl_fft.Real.transform_rad2 z;
+    let d = unpack z in
+    for j = 0 to n - 1 do
+      let k = 2 * j in
+      let re, im = d.(k), d.(k + 1) in
+      Owl_dense_complex.set y i j Complex.({re; im})
+    done
+  ) x;
+  y
 
 let _fft_real_radn x = _fft_real_rad2 x
 
