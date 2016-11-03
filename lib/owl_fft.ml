1e1ddf34ed789038b8d219d500749b50f7ab30c6
Liang Wang <ryanrhymes@gmail.com>
2016-11-03 22:59:09 +0000

    coding ...

--- lib/owl_fft.ml
+++ lib/owl_fft.ml
@@ -4,15 +4,16 @@
  *)
 
 
+let _is_power_of_two x = x land (x - 1) = 0
+
 let _fft_real_rad2 x =
-  let open Gsl_fft in
   let m, n = Owl_dense_real.shape x in
   let y = Owl_dense_complex.empty m n in
   Owl_dense_real.iteri_rows (fun i v ->
     let v = Owl_dense_real.to_array v in
-    let z = { layout = Real; data = v } in
+    let z = Gsl_fft.({ layout = Real; data = v }) in
     Gsl_fft.Real.transform_rad2 z;
-    let d = unpack z in
+    let d = Gsl_fft.unpack z in
     for j = 0 to n - 1 do
       let k = 2 * j in
       let re, im = d.(k), d.(k + 1) in
@@ -21,11 +22,27 @@ let _fft_real_rad2 x =
   ) x;
   y
 
-let _fft_real_radn x = _fft_real_rad2 x
+let _fft_real_radn x =
+  let m, n = Owl_dense_real.shape x in
+  let y = Owl_dense_complex.empty m n in
+  let w0 = Gsl_fft.Real.make_wavetable n in
+  let w1 = Gsl_fft.Real.make_workspace n in
+  Owl_dense_real.iteri_rows (fun i v ->
+    let v = Owl_dense_real.to_array v in
+    let z = Gsl_fft.({ layout = Real; data = v }) in
+    Gsl_fft.Real.transform z w0 w1;
+    let d = Gsl_fft.unpack z in
+    for j = 0 to n - 1 do
+      let k = 2 * j in
+      let re, im = d.(k), d.(k + 1) in
+      Owl_dense_complex.set y i j Complex.({re; im})
+    done
+  ) x;
+  y
 
 let fft x =
   let _, n = Owl_dense_real.shape x in
-  match (n mod 2) = 0 with
+  match (_is_power_of_two n) with
   | true  -> _fft_real_rad2 x
   | false -> _fft_real_radn x
 
