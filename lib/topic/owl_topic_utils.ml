2933111472c7255404df36d1b2d4754d2f007eff
Liang Wang <ryanrhymes@gmail.com>
2016-11-11 23:21:59 +0000

    coding

--- lib/topic/owl_topic_utils.ml
+++ lib/topic/owl_topic_utils.ml
@@ -1,11 +1,22 @@
 (** [ Topic Model Utils ]  *)
 
+let _allocate_space x =
+  Log.info "allocate more space ...";
+  let l = Array.length x in
+  let y = Array.make l [] in
+  Array.append x y
+
 let load_data f =
-  let l = ref [||] in
+  let x = ref (Array.make 1024 []) in
+  let c = ref 0 in
   let h = open_in f in
-  try while true do
-    let s = Str.split (Str.regexp " ") (input_line h) in
-    let _ = l := Array.append !l [|s|] in
-    Log.info "%i" (List.length s)
-  done with End_of_file -> ();
-  close_in h
+  (
+    try while true do
+      if !c = (Array.length !x) - 1 then x := _allocate_space !x;
+      let s = Str.split (Str.regexp " ") (input_line h) in
+      !x.(!c) <- s;
+      c := !c + 1;
+    done with End_of_file -> ()
+  );
+  close_in h;
+  Array.sub !x 0 !c
