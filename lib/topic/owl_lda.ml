00cf0f46b7ab71c86953ef8266ccb413fbde9df8
Liang Wang <ryanrhymes@gmail.com>
2016-11-12 15:06:39 +0000

    coding ...

--- lib/topic/owl_lda.ml
+++ lib/topic/owl_lda.ml
@@ -9,18 +9,18 @@ module Model = struct
   let n_k = ref 1          (* number of topics *)
   let n_v = ref 0          (* number of vocabulary *)
 
-  let alpha = ref 0.0      (* model hyper-parameters *)
-  let beta = ref 0.0       (* model hyper-parameters *)
-  let alpha_k = ref 0.0
-  let beta_v = ref 0.0
+  let alpha = ref 0.1      (* model hyper-parameters *)
+  let beta = ref 0.1       (* model hyper-parameters *)
+  let alpha_k = ref 0.1
+  let beta_v = ref 0.1
 
   let t_dk = ref (MS.zeros 1 1)        (* document-topic table: num of tokens assigned to each topic in each doc *)
   let t_wk = ref (MS.zeros 1 1)        (* word-topic table: num of tokens assigned to each topic for each word *)
   let t__k = ref (MS.zeros 1 1)        (* number of tokens assigned to a topic: k = sum_w t_wk = sum_d t_dk *)
   let t__z = ref [| [||] |]            (* table of topic assignment of each token in each document *)
 
-  let data = ref (MS.zeros 1 1)  (* training data *)
-  let n_iter = 1_000       (* number of iterations *)
+  let data = ref [| [ ] |]  (* training data *)
+  let n_iter = 1_000        (* number of iterations *)
 
   let init_lda () = ()
 
@@ -36,8 +36,7 @@ module Model = struct
 
   let sampling d =
     let p = MD.zeros 1 !n_k in
-    MS.iteri_nz (fun _ i w ->
-      let w = int_of_float w in
+    List.iteri (fun i w ->
       let k = !t__z.(d).(i) in
       exclude_token w d k;
       (* make cdf function *)
@@ -52,7 +51,7 @@ module Model = struct
       while (MD.get p 0 !k) < u do k := !k + 1 done;
       include_token w d !k;
       !t__z.(d).(i) <- !k;
-    ) (MS.row !data d)
+    ) !data.(d)
 
   let train () =
     for i = 0 to n_iter - 1 do
@@ -63,9 +62,10 @@ module Model = struct
 
   (* init the model based on: vocabulary, topics, tokens *)
   let init k v d =
-    n_d := Array.length d;
-    n_v := v;
-    n_k := k;
+    data := d;
+    n_d  := Array.length d;
+    n_v  := v;
+    n_k  := k;
     t_dk := MS.zeros !n_d !n_k;
     t_wk := MS.zeros !n_v !n_k;
     t__k := MS.zeros 1 !n_k;
