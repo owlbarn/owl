b1d69a4ede3883be4503840e9d51d130505c9eda
Liang Wang <ryanrhymes@gmail.com>
2016-11-13 11:52:49 +0000

    change module name

--- examples/test_lda.ml
+++ examples/test_lda.ml
@@ -1,8 +1,12 @@
+(** [ Test Topic Module ]
+  The test only works when you have the corresponding text corpus on your
+  mahcine. Appologies for the temporary hard-coding path.
+ *)
 
 let _ =
   let s = Owl_topic_utils.load_stopwords "/Users/liang/code/owl/lib/topic/stopwords.txt" in
   let x = Owl_topic_utils.load_data ~stopwords:s "/Users/liang/code/experimental-lda/data/nips.test" in
   let v = Owl_topic_utils.build_vocabuary x in
   let d = Owl_topic_utils.tokenisation v x in
-  let _ = Owl_lda.init 100 (Hashtbl.length v) d in
-  Owl_lda.(train SimpleLDA)
+  let _ = Owl_topic_lda.init 100 (Hashtbl.length v) d in
+  Owl_topic_lda.(train SimpleLDA)
