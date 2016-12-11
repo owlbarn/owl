f5d120c6c53a5551a67aa8cefe0a8dcfa9418278
Liang Wang <ryanrhymes@gmail.com>
2016-12-11 17:12:10 +0000

    coding

--- lib/parallel/parmap.ml
+++ lib/parallel/parmap.ml
@@ -1,16 +1,3 @@
-(**************************************************************************)
-(* ParMap: a simple library to perform Map computations on a multi-core   *)
-(*                                                                        *)
-(*  Author(s):  Marco Danelutto, Roberto Di Cosmo                         *)
-(*                                                                        *)
-(*  This library is free software: you can redistribute it and/or modify  *)
-(*  it under the terms of the GNU Lesser General Public License as        *)
-(*  published by the Free Software Foundation, either version 2 of the    *)
-(*  License, or (at your option) any later version.  A special linking    *)
-(*  exception to the GNU Lesser General Public License applies to this    *)
-(*  library, see the LICENSE file for more information.                   *)
-(**************************************************************************)
-
 open Bigarray
 
 module Utils = Parmap_utils
