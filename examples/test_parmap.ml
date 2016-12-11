e83d924e2bf2c49d85e653392a50b4677e05709e
Liang Wang <ryanrhymes@gmail.com>
2016-12-11 16:49:13 +0000

    coding

--- examples/test_parmap.ml
+++ examples/test_parmap.ml
@@ -1,11 +1,12 @@
 (* Test parallel map *)
 
+open Bigarray
+
 let print_array x =
   Array.iter (fun a -> Printf.printf "%.2f " a) x;
   print_endline ""
 
 let _ =
  let x = Array.init 10 (fun i -> float_of_int i) in
- print_array x;
- let y = Parmap.parmap ~ncores:2 (fun a -> a +. 1.) (Parmap.A x) in
- print_array (Array.of_list y);
+ let x = Array1.of_array Float64 c_layout x in
+ ()
