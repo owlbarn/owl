94c8a3ca6a0b96c6c181ce5e58e0284e8bad10d5
Liang Wang <ryanrhymes@gmail.com>
2017-01-14 00:55:17 +0000

    add owl_sparse_matrix.mli

--- lib/owl_sparse_common.ml
+++ lib/owl_sparse_common.ml
@@ -4,11 +4,16 @@
  *)
 
 open Bigarray
-(*
-let _eigen_create : type a b. (a, b) kind -> (int -> int ) = function
-  | Float32   ->
-  | Float64   ->
-  | Complex32 ->
-  | Complex64 ->
+open Eigen_types
+
+type ('a, 'b) kind = ('a, 'b) Bigarray.kind
+
+type ('a, 'b) eigen_mat = ('a, 'b) spmat
+
+let _eigen_create : type a b . (a, b) kind -> int -> int -> (a, b) eigen_mat =
+  fun k m n -> match k with
+  | Float32   -> SPMAT_S (Eigen.Sparse.S.create m n)
+  | Float64   -> SPMAT_D (Eigen.Sparse.D.create m n)
+  | Complex32 -> SPMAT_C (Eigen.Sparse.C.create m n)
+  | Complex64 -> SPMAT_Z (Eigen.Sparse.Z.create m n)
   | _         -> failwith "_eigen_create: unsupported operation"
-*)
--- lib/owl_sparse_matrix.ml
+++ lib/owl_sparse_matrix.ml
@@ -8,18 +8,16 @@ open Owl_sparse_common
 
 type ('a, 'b) kind = ('a, 'b) Bigarray.kind
 
-type ('a, 'b) tt = ('a, 'b) Eigen_types.spmat
-
 type ('a, 'b) t = {
-  mutable m   : int;                             (* number of rows *)
-  mutable n   : int;                             (* number of columns *)
-  mutable k   : ('a, 'b) kind;                   (* type of sparse matrices *)
-  mutable d   : int;                             (* point to eigen struct *)
+  mutable m : int;                             (* number of rows *)
+  mutable n : int;                             (* number of columns *)
+  mutable k : ('a, 'b) kind;                   (* type of sparse matrices *)
+  mutable d : ('a, 'b) eigen_mat;              (* point to eigen struct *)
 }
 
-let create : type a b . (a, b) kind -> int -> int -> (a, b) tt  = fun k m n ->
-  match k with
-  | Float32   -> SPMAT_S (Eigen.Sparse.S.create m n)
-  | Float64   -> SPMAT_D (Eigen.Sparse.D.create m n)
-  | Complex32 -> SPMAT_C (Eigen.Sparse.C.create m n)
-  | Complex64 -> SPMAT_Z (Eigen.Sparse.Z.create m n)
+let create k m n = {
+  m = m;
+  n = n;
+  k = k;
+  d = (_eigen_create) k m n;
+}
