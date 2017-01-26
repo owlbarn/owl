a80a850a1f48fedd231100b64cdfb8f332c9066c
Liang Wang <ryanrhymes@gmail.com>
2017-01-26 23:32:40 +0000

    coding autograd

--- lib/owl_autograd.ml
+++ lib/owl_autograd.ml
@@ -7,14 +7,14 @@ type node = {
   value : float;
   func : float -> float;
   vjp : float -> float;
-  parent : node list;
-  progenitor : node list ref;
+  mutable parent : node list;
+  mutable progenitor : node list; (* FIXME: use set *)
 }
 
 type scalar = Float of float | Node of node
 
-let wrap_fun f g arg =
-  let v, p = match arg with
+let wrap_fun f g args =
+  let v, p = match args with
   | Float v -> v, []
   | Node p -> p.value, [p]
   in
@@ -23,34 +23,54 @@ let wrap_fun f g arg =
     func = f;
     vjp = g;
     parent = p;
-    progenitor = ref [];
+    progenitor = [];
   }
   in
-  match !(x.progenitor) with
+  match x.progenitor with
   | [] -> Float x.value
   | _  -> Node x
+
+let identity x = x
 
 let sin x = wrap_fun sin cos x
 
 let log x = wrap_fun log (fun x -> 1. /. x) x
 
-let new_progenitor x = None
-
-let new_node v =
-let f = fun a -> a in
-let g = fun a -> a in
-{
-  value = v;
-  func = f;
-  vjp = g;
-  parent = [];
-  progenitor = ref [];
+let new_node value func args parent progenitor = {
+  value;
+  func;
+  vjp = identity;
+  parent;
+  progenitor;
 }
 
+let new_progenitor x =
+  let n = match x with
+    | Float y -> new_node y identity [y] [] []
+    | Node y  -> new_node y.value identity [y] [y] y.progenitor
+  in
+  n.progenitor <- n.progenitor @ [n];
+  n
+
+let wrap_fun' f g args =
+  let progenitor = ref [] in
+  let parent = ref [] in
+  List.iter (fun arg ->
+    match arg with
+    | Node x -> (
+      parent := !parent @ x.parent;
+      progenitor := !progenitor @ x.progenitor; (* FIXME *)
+      )
+    | _ -> ()
+  ) args
+  (* FIXME
+  match x.progenitor with
+  | [] -> Float x.value
+  | _  -> Node x
+*)
 let grad f =
   let f' = fun a -> (
-    let x = new_node a in
-    let _ = x.value = a in
+    let x = new_node a identity [] [] [] in
     f (Node x)
   )
   in
