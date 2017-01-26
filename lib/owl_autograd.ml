ddb11c93e38cbd78bff31d9426cd8e86948fe6a1
Liang Wang <ryanrhymes@gmail.com>
2017-01-26 19:07:35 +0000

    coding autograd

--- lib/owl_autograd.ml
+++ lib/owl_autograd.ml
@@ -8,28 +8,50 @@ type node = {
   func : float -> float;
   vjp : float -> float;
   parent : node list;
+  progenitor : node list ref;
 }
 
 type scalar = Float of float | Node of node
 
-module Owl_math_wrapper = struct
+let wrap_fun f g arg =
+  let v, p = match arg with
+  | Float v -> v, []
+  | Node p -> p.value, [p]
+  in
+  let x = {
+    value = f v;
+    func = f;
+    vjp = g;
+    parent = p;
+    progenitor = ref [];
+  }
+  in
+  match !(x.progenitor) with
+  | [] -> Float x.value
+  | _  -> Node x
 
-  let wrap_fun f g arg =
-    let v, p = match arg with
-    | Float v -> v, []
-    | Node p -> p.value, [p]
-    in
-    let x = {
-      value = f v;
-      func = f;
-      vjp = g;
-      parent = p;
-    }
-    in
-    Node x
+let sin x = wrap_fun sin cos x
 
-  let sin x = wrap_fun sin cos x
+let log x = wrap_fun log (fun x -> 1. /. x) x
 
-  let log x = wrap_fun log (fun x -> 1. /. x) x
+let new_progenitor x = None
 
-end
+let new_node v =
+let f = fun a -> a in
+let g = fun a -> a in
+{
+  value = v;
+  func = f;
+  vjp = g;
+  parent = [];
+  progenitor = ref [];
+}
+
+let grad f =
+  let f' = fun a -> (
+    let x = new_node a in
+    let _ = x.value = a in
+    f (Node x)
+  )
+  in
+  f'
