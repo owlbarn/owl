5d62ae1a1eefe96de6599e75ac48d5988d1ec0db
Liang Wang <ryanrhymes@gmail.com>
2017-01-28 21:00:05 +0000

    add owl_autograd_maths.ml

--- lib/owl_autograd_maths.ml
+++ lib/owl_autograd_maths.ml
@@ -2,3 +2,35 @@
  * OWL - an OCaml numerical library for scientific computing
  * Copyright (c) 2016-2017 Liang Wang <liang.wang@cl.cam.ac.uk>
  *)
+
+let sin x = sin x.(0)
+
+let sin' g x = g *. cos x.(0)
+
+
+let log x = log x.(0)
+
+let log' g x = g /. x.(0)
+
+
+let add x = x.(0) +. x.(1)
+
+let add' ?(argnum=0) g x = g
+
+let sub x = x.(0) -. x.(1)
+
+let add' ?(argnum=0) g x = -.g
+
+let mul x = x.(0) *. x.(1)
+
+let mul' ?(argnum=0) g x =
+  match argnum with
+  | 0 -> g *. x.(1)
+  | _ -> g *. x.(0)
+
+let div x = x.(0) /. x.(1)
+
+let div' ?(argnum=0) g x =
+  match argnum with
+  | 0 -> g /. x.(1)
+  | _ -> (-.g *. x.(0)) /. (x.(1) *. x.(1))
