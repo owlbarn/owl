a257a260e24ae8b219580ff798288af43b386ca7
Liang Wang <ryanrhymes@gmail.com>
2017-01-29 01:42:12 +0000

    add owl_autograd_maths.ml

--- lib/owl_autograd.ml
+++ lib/owl_autograd.ml
@@ -3,71 +3,79 @@
  * Copyright (c) 2016-2017 Liang Wang <liang.wang@cl.cam.ac.uk>
  *)
 
-type op = Forward | Backward | Calculate
-
 type node = {
   mutable value : float;
   mutable dual : float;
   func : float -> float;
   vjp : float -> float;
-  mutable parent : node list;
-  mutable progenitor : node list; (* FIXME: use set *)
 }
 
 type scalar = Float of float | Node of node
 
+type df =
+  | FUNVEC of (scalar array -> scalar)
+  | FUN01X of (scalar -> scalar)
+  | FUN02X of (scalar -> scalar -> scalar)
+  | FUN03X of (scalar -> scalar -> scalar -> scalar)
+  | FUN04X of (scalar -> scalar -> scalar -> scalar -> scalar)
+
 let identity x = x
 
-let new_node value func args parent progenitor = {
+let new_node value func args = {
   value;
   dual = 0.;
   func;
   vjp = identity;
-  parent;
-  progenitor;
 }
 
-let new_progenitor x =
-  let n = match x with
-    | Float y -> new_node y identity [y] [] []
-    | Node y  -> new_node y.value identity [y] [y] y.progenitor
-  in
-  n.progenitor <- n.progenitor @ [n];
-  n
-
-let wrap_fun f g args =
-  let progenitor = ref [] in
+let wrap_fun f f' args =
   let parent = ref [] in
-  let argval = ref [] in
+  let argval = ref [||] in
+  let dualval = ref [||] in
   List.iter (fun arg ->
     match arg with
     | Node x -> (
-      print_endline "+++";
-      progenitor := !progenitor @ x.progenitor; (* FIXME *)
       parent := !parent @ [x];
-      argval := !argval @ [x.value];
+      argval := Array.append !argval [|x.value|];
+      dualval := Array.append !dualval [|x.dual|];
       )
-    | Float x -> argval := !argval @ [x]
+    | Float x -> argval := Array.append !argval [|x|]
   ) args;
-  let v = f (List.nth !argval 0) in
-  (* FIXME *)
+  let v = f !argval in
   match !parent with
   | [] -> Float v
   | _  -> (
-    let n = new_node v identity [] !parent !progenitor in
-    n.dual <- g (List.nth !argval 0);
+    let n = new_node v identity [] in
+    n.dual <- f' !dualval !argval;
     Node n
     )
 
-let grad f =
-  let f' = fun a -> (
-    let n = new_node a identity [] [] [] in
-    n.dual <- 1.0;
-    f (Node n)
+let prepare_fun f =
+  match f with
+  | FUNVEC f -> f
+  | FUN01X f -> fun x -> f x.(0)
+  | FUN02X f -> fun x -> f x.(0) x.(1)
+  | FUN03X f -> fun x -> f x.(0) x.(1) x.(2)
+  | FUN04X f -> fun x -> f x.(0) x.(1) x.(2) x.(3)
+
+let grad ?(argnum=0) f =
+  let f = prepare_fun f in
+  let f' = fun args -> (
+    let l = Array.mapi (fun i a ->
+      let n = new_node a identity [] in
+      if argnum = i then n.dual <- 1. else n.dual <- 0.;
+      Node n
+    ) args
+    in
+    f l
   )
   in
   f'
 
-let sin x = wrap_fun sin cos [x]
-
-let log x = wrap_fun log (fun x -> 1. /. x) [x]
+let sin x = wrap_fun Owl_autograd_maths.sin Owl_autograd_maths.sin' [x]
+let cos x = wrap_fun Owl_autograd_maths.cos Owl_autograd_maths.cos' [x]
+let log x = wrap_fun Owl_autograd_maths.log Owl_autograd_maths.log' [x]
+let ( +. ) x0 x1 = wrap_fun Owl_autograd_maths.add Owl_autograd_maths.add' [x0; x1]
+let ( -. ) x0 x1 = wrap_fun Owl_autograd_maths.sub Owl_autograd_maths.sub' [x0; x1]
+let ( *. ) x0 x1 = wrap_fun Owl_autograd_maths.mul Owl_autograd_maths.mul' [x0; x1]
+let ( /. ) x0 x1 = wrap_fun Owl_autograd_maths.div Owl_autograd_maths.div' [x0; x1]
--- lib/owl_autograd_maths.ml
+++ lib/owl_autograd_maths.ml
@@ -5,32 +5,34 @@
 
 let sin x = sin x.(0)
 
-let sin' g x = g *. cos x.(0)
+let sin' g x = Pervasives.(g.(0) *. cos x.(0))
+
+
+let cos x = cos x.(0)
+
+let cos' g x = Pervasives.(-.g.(0) *. sin x.(0))
 
 
 let log x = log x.(0)
 
-let log' g x = g /. x.(0)
+let log' g x = g.(0) /. x.(0)
 
 
 let add x = x.(0) +. x.(1)
 
-let add' ?(argnum=0) g x = g
+let add' g x = g.(0) +. g.(1)
+
 
 let sub x = x.(0) -. x.(1)
 
-let add' ?(argnum=0) g x = -.g
+let sub' g x = g.(0) -.g.(1)
+
 
 let mul x = x.(0) *. x.(1)
 
-let mul' ?(argnum=0) g x =
-  match argnum with
-  | 0 -> g *. x.(1)
-  | _ -> g *. x.(0)
+let mul' g x = (g.(0) *. x.(1)) +. (g.(1) *. x.(0))
+
 
 let div x = x.(0) /. x.(1)
 
-let div' ?(argnum=0) g x =
-  match argnum with
-  | 0 -> g /. x.(1)
-  | _ -> (-.g *. x.(0)) /. (x.(1) *. x.(1))
+let div' g x = (g.(0) /. x.(1)) -. ((g.(1) *. x.(0)) /. (x.(1) *. x.(1)))
