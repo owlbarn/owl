318d5013a70fe05a0f09e4771622b65b1dddd012
Liang Wang <ryanrhymes@gmail.com>
2017-01-31 19:08:19 +0000

    coding autograd

--- lib/owl_autograd.ml
+++ lib/owl_autograd.ml
@@ -6,51 +6,18 @@
 (* experimental ... *)
 
 type node = {
-  mutable value : float;
-  mutable dual : float;
-  func : float -> float;
-  vjp : float -> float;
+  mutable v : float;
+  mutable d : float;
 }
 
 type scalar = Float of float | Node of node
 
 type df =
-  | FUNVEC of (scalar array -> scalar)
-  | FUN01X of (scalar -> scalar)
-  | FUN02X of (scalar -> scalar -> scalar)
-  | FUN03X of (scalar -> scalar -> scalar -> scalar)
-  | FUN04X of (scalar -> scalar -> scalar -> scalar -> scalar)
-
-let identity x = x
-
-let new_node value func args = {
-  value;
-  dual = 0.;
-  func;
-  vjp = identity;
-}
-
-let wrap_fun f f' args =
-  let parent = ref [||] in
-  let argval = ref [||] in
-  let dualval = ref [||] in
-  Array.iter (fun arg ->
-    match arg with
-    | Node x -> (
-      parent := Array.append !parent [|x|];
-      argval := Array.append !argval [|x.value|];
-      dualval := Array.append !dualval [|x.dual|];
-      )
-    | Float x -> argval := Array.append !argval [|x|]
-  ) args;
-  let v = f !argval in
-  match !parent with
-  | [||] -> Float v
-  | _  -> (
-    let n = new_node v identity [] in
-    n.dual <- f' !dualval !argval;
-    Node n
-    )
+  | FUNVEC of (scalar array -> scalar array)
+  | FUN01X of (scalar -> scalar array)
+  | FUN02X of (scalar -> scalar -> scalar array)
+  | FUN03X of (scalar -> scalar -> scalar -> scalar array)
+  | FUN04X of (scalar -> scalar -> scalar -> scalar -> scalar array)
 
 let prepare_fun f =
   match f with
@@ -60,16 +27,43 @@ let prepare_fun f =
   | FUN03X f -> fun x -> f x.(0) x.(1) x.(2)
   | FUN04X f -> fun x -> f x.(0) x.(1) x.(2) x.(3)
 
-(* USE GADT to fix *)
+let new_node v d = { v; d; }
+
+let wrap_fun f f' args =
+  let dr_mode = ref false in
+  let argsval = ref [||] in
+  let dualval = ref [||] in
+  Array.iter (fun arg ->
+    match arg with
+    | Node x -> (
+      dr_mode := true;
+      argsval := Array.append !argsval [|x.v|];
+      dualval := Array.append !dualval [|x.d|];
+      )
+    | Float x -> argsval := Array.append !argsval [|x|]
+  ) args;
+  let v = f !argsval in
+  match !dr_mode with
+  | true -> (
+    let d = f' !dualval !argsval in
+    let n = new_node v d in
+    Node n
+    )
+  | false -> Float v
+
 let forward_ad ?(argnum=0) f =
   let f' = fun args -> (
     let l = Array.mapi (fun i a ->
-      let n = new_node a identity [] in
-      if argnum = i then n.dual <- 1. else n.dual <- 0.;
+      let n = match argnum = i with
+      | true  -> new_node a 1.
+      | false -> new_node a 0.
+      in
       Node n
     ) args
     in
-    f l
+    Array.map (fun x -> match x with
+      | Node n -> n.d
+      | Float v -> v) (f l)
   )
   in
   f'
@@ -77,9 +71,8 @@ let forward_ad ?(argnum=0) f =
 let grad ?(argnum=0) f =
   let f = prepare_fun f in
   let g = fun args -> (
-    match (forward_ad ~argnum f) args with
-    | Node n -> n.dual
-    | _ -> failwith "grad:g:wrong output"
+    let r = (forward_ad ~argnum f) args in
+    r.(0)
   )
   in
   g
