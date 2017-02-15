0e24baeb657bb85600dda02487ffce4bcd5d8083
Liang Wang <ryanrhymes@gmail.com>
2017-02-15 22:30:12 +0000

    algodiff : experiment

--- lib/owl_algodiff_matrix.ml
+++ lib/owl_algodiff_matrix.ml
@@ -35,11 +35,13 @@ let rec zero = function
 
 let rec one = function
   | Float _ -> Float 1.
+  | Matrix _ -> failwith "Error: one does not take matrix."
   | Dual x -> make_dual (one x.v) (zero x.d)
 
 let is_zero x =
   let rec _is_zero = function
     | Float a -> if a <> 0. then failwith "not zero"
+    | Matrix a -> if M.is_zero a = false then failwith "not zero"
     | Dual x -> (_is_zero x.v; _is_zero x.d)
   in
   try (_is_zero x; true)
@@ -49,6 +51,7 @@ let is_const x = is_zero (dual x)
 
 let rec sign = function
   | Float a -> Float (if a = 0. then 0. else if a > 0. then 1. else -1.)
+  | Matrix a -> failwith "Error: sign does not take matrix."
   | Dual x -> sign x.v
 
 (* overload operators, module signature *)
@@ -78,24 +81,44 @@ module rec Maths : MathsSig = struct
     | Float x0, Dual x1 -> make_dual (_add (Float x0) x1.v) x1.d
     | Dual x0, Float x1 -> make_dual (_add x0.v (Float x1)) x0.d
     | Dual x0, Dual x1 -> make_dual (_add x0.v x1.v) (_add x0.d x1.d)
+    | Matrix x0, Matrix x1 -> Matrix M.(x0 +@ x1)
+    | Matrix x0, Float x1 -> Matrix M.(x0 +$ x1)
+    | Float x0, Matrix x1 -> Matrix M.(x0 $+ x1)
+    | Matrix x0, Dual x1 -> make_dual (_add (Matrix x0) x1.v) x1.d
+    | Dual x0, Matrix x1 -> make_dual (_add x0.v (Matrix x1)) x0.d
 
   let rec _sub x0 x1 = match x0, x1 with
     | Float x0, Float x1 -> Float Pervasives.(x0 -. x1)
     | Float x0, Dual x1 -> make_dual (_sub (Float x0) x1.v) (_sub (Float 0.) x1.d)
     | Dual x0, Float x1 -> make_dual (_sub x0.v (Float x1)) x0.d
     | Dual x0, Dual x1 -> make_dual (_sub x0.v x1.v) (_sub x0.d x1.d)
+    | Matrix x0, Matrix x1 -> Matrix M.(x0 -@ x1)
+    | Matrix x0, Float x1 -> Matrix M.(x0 -$ x1)
+    | Float x0, Matrix x1 -> Matrix M.(x0 $- x1)
+    | Matrix x0, Dual x1 -> make_dual (_sub (Matrix x0) x1.v) x1.d
+    | Dual x0, Matrix x1 -> make_dual (_sub x0.v (Matrix x1)) x0.d
 
   let rec _mul x0 x1 = match x0, x1 with
     | Float x0, Float x1 -> Float Pervasives.(x0 *. x1)
     | Float x0, Dual x1 -> make_dual (_mul (Float x0) x1.v) (_mul (Float x0) x1.d)
     | Dual x0, Float x1 -> make_dual (_mul x0.v (Float x1)) (_mul x0.d (Float x1))
     | Dual x0, Dual x1 -> make_dual (_mul x0.v x1.v) (_add (_mul x0.v x1.d) (_mul x0.d x1.v))
+    | Matrix x0, Matrix x1 -> Matrix M.(x0 *@ x1)
+    | Matrix x0, Float x1 -> Matrix M.(x0 *$ x1)
+    | Float x0, Matrix x1 -> Matrix M.(x0 $* x1)
+    | Matrix x0, Dual x1 -> make_dual (_mul (Matrix x0) x1.v) (_mul (Matrix x0) x1.d)
+    | Dual x0, Matrix x1 -> make_dual (_mul x0.v (Matrix x1)) (_mul x0.d (Matrix x1))
 
   let rec _div x0 x1 = match x0, x1 with
     | Float x0, Float x1 -> Float Pervasives.(x0 /. x1)
     | Float x0, Dual x1 -> let y = _div (Float x0) x1.v in make_dual y (_mul (Float (-1.)) (_mul (_div y x1.v) x1.d))
     | Dual x0, Float x1 -> make_dual (_div x0.v (Float x1)) (_div x0.d (Float x1))
     | Dual x0, Dual x1 -> make_dual (_div x0.v x1.v) (_sub (_div x0.d x1.v) (_div (_mul x0.v x1.d) (_mul x1.v x1.v)))
+    | Matrix x0, Matrix x1 -> Matrix M.(x0 /@ x1)
+    | Matrix x0, Float x1 -> Matrix M.(x0 /$ x1)
+    | Float x0, Matrix x1 -> Matrix M.(x0 $/ x1)
+    | Matrix x0, Dual x1 -> let y = _div (Matrix x0) x1.v in make_dual y (_mul (Float (-1.)) (_mul (_div y x1.v) x1.d))
+    | Dual x0, Matrix x1 -> make_dual (_div x0.v (Matrix x1)) (_div x0.d (Matrix x1))
 
   let ( +. ) x0 x1 = _add x0 x1
 
