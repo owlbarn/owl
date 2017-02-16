64750d2e657f301faf0d106a4e378e2b18c3ecca
Liang Wang <ryanrhymes@gmail.com>
2017-02-16 23:59:14 +0000

    algodiff : experiment

--- lib/owl_algodiff_backward.ml
+++ lib/owl_algodiff_backward.ml
@@ -7,8 +7,15 @@ module M = Owl_dense_real
 
 type mat = Owl_dense_real.mat
 
-type t = Float of float | Matrix of mat | Dual of dual
+type t = Float of float | Matrix of mat | Adjt of adjt
 and adjt = {
   v : t;       (* primal value *)
   d : t;       (* tangent value *)
 }
+and trace_op =
+  | Add of t * t
+  | Sub of t * t
+  | Mul of t * t
+  | Div of t * t
+  | Sin of t
+  | Cos of t
