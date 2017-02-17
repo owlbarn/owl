6bebe0a53e7dfabb29e6b9e7a22f5d25128cab31
Liang Wang <ryanrhymes@gmail.com>
2017-02-17 23:26:07 +0000

    algodiff : experiment

--- lib/owl_algodiff_backward.ml
+++ lib/owl_algodiff_backward.ml
@@ -31,37 +31,157 @@ and trace_op =
 
 let make_dual p t = D { p; t }
 
+let dual = function
+  | Float a -> Float 0.
+  | Matrix a -> Float 0.
+  | D a -> a.t
+
+let rec zero = function
+  | Float _ -> Float 0.
+  | Matrix _ -> Float 0.
+  | D a -> make_dual (zero a.p) (zero a.t)
+
+let rec one = function
+  | Float _ -> Float 1.
+  | Matrix _ -> failwith "Error: one does not take matrix."
+  | D a -> make_dual (one a.p) (zero a.t)
+
 
 module Maths = struct
 
-  let rec _add a b = match a, b with
+  let rec op_d_d a ff fd df =
+    match a with
+    | D a -> let cp = fd a.p in make_dual cp (df cp a.p a.t)
+    | A a -> failwith "error: not implemented."
+    | ap -> ff ap
+
+  and add a b = match a, b with
     | Float a, Float b -> Float Pervasives.(a +. b)
-    | Float a, D b -> make_dual (_add (Float a) b.p) b.t
-    | D a, Float b -> make_dual (_add a.p (Float b)) a.t
-    | D a, D b -> make_dual (_add a.p b.p) (_add a.t b.t)
+    | Float a, D b -> make_dual (add (Float a) b.p) b.t
+    | D a, Float b -> make_dual (add a.p (Float b)) a.t
+    | D a, D b -> make_dual (add a.p b.p) (add a.t b.t)
     | Matrix a, Matrix b -> Matrix M.(a +@ b)
     | Matrix a, Float b -> Matrix M.(a +$ b)
     | Float a, Matrix b -> Matrix M.(a $+ b)
-    | Matrix a, D b -> make_dual (_add (Matrix a) b.p) b.t
-    | D a, Matrix b -> make_dual (_add a.p (Matrix b)) a.t
+    | Matrix a, D b -> make_dual (add (Matrix a) b.p) b.t
+    | D a, Matrix b -> make_dual (add a.p (Matrix b)) a.t
 
-  and _sub a b = match a, b with
+  and ( +. ) a b = add a b
+
+  and add' cp ap at bp bt = at +. bt
+
+  and add'_da cp ap at = at
+
+  and add'_db cp bp bt = bt
+
+  and sub a b = match a, b with
     | Float a, Float b -> Float Pervasives.(a -. b)
-    | Float a, D b -> make_dual (_sub (Float a) b.p) (_sub (Float 0.) b.t)
-    | D a, Float b -> make_dual (_sub a.p (Float b)) a.t
-    | D a, D b -> make_dual (_sub a.p b.p) (_sub a.t b.t)
+    | Float a, D b -> make_dual (sub (Float a) b.p) (sub (Float 0.) b.t)
+    | D a, Float b -> make_dual (sub a.p (Float b)) a.t
+    | D a, D b -> make_dual (sub a.p b.p) (sub a.t b.t)
     | Matrix a, Matrix b -> Matrix M.(a -@ b)
     | Matrix a, Float b -> Matrix M.(a -$ b)
     | Float a, Matrix b -> Matrix M.(a $- b)
-    | Matrix a, D b -> make_dual (_sub (Matrix a) b.p) b.t
-    | D a, Matrix b -> make_dual (_sub a.p (Matrix b)) a.t
+    | Matrix a, D b -> make_dual (sub (Matrix a) b.p) b.t
+    | D a, Matrix b -> make_dual (sub a.p (Matrix b)) a.t
 
+  and ( -. ) a b = sub a b
+
+  and sub' cp ap at bp bt = at -. bt
+
+  and sub'_da cp ap at = at
+
+  and sub'_db cp bp bt = Float 0. -. bt
+
+  and mul a b = match a, b with
+    | Float a, Float b -> Float Pervasives.(a *. b)
+    | Float a, D b -> make_dual (mul (Float a) b.p) (mul (Float a) b.t)
+    | D a, Float b -> make_dual (mul a.p (Float b)) (mul a.t (Float b))
+    | D a, D b -> make_dual (mul a.p b.p) (add (mul a.p b.t) (mul a.t b.p))
+    | Matrix a, Matrix b -> Matrix M.(a *@ b)
+    | Matrix a, Float b -> Matrix M.(a *$ b)
+    | Float a, Matrix b -> Matrix M.(a $* b)
+    | Matrix a, D b -> make_dual (mul (Matrix a) b.p) (mul (Matrix a) b.t)
+    | D a, Matrix b -> make_dual (mul a.p (Matrix b)) (mul a.t (Matrix b))
+
+  and ( *. ) a b = mul a b
+
+  and mul' cp ap at bp bt = (ap *. bt) +. (at *. bp)
+
+  and mul'_da cp ap at b = at *. b
+
+  and mul'_db cp bp bt a = a *. bt
+
+  and div a b = match a, b with
+    | Float a, Float b -> Float Pervasives.(a /. b)
+    | Float a, D b -> let y = div (Float a) b.p in make_dual y (mul (Float (-1.)) (mul (div y b.p) b.t))
+    | D a, Float b -> make_dual (div a.p (Float b)) (div a.t (Float b))
+    | D a, D b -> make_dual (div a.p b.p) (sub (div a.t b.p) (div (mul a.p b.t) (mul b.p b.p)))
+    | Matrix a, Matrix b -> Matrix M.(a /@ b)
+    | Matrix a, Float b -> Matrix M.(a /$ b)
+    | Float a, Matrix b -> Matrix M.(a $/ b)
+    | Matrix a, D b -> let y = div (Matrix a) b.p in make_dual y (mul (Float (-1.)) (mul (div y b.p) b.t))
+    | D a, Matrix b -> make_dual (div a.p (Matrix b)) (div a.t (Matrix b))
+
+  and ( /. ) a b = div a b
+
+  and div' cp ap at bp bt = (at -. bt *. cp) /. bp
+
+  and div'_da cp ap at b = at /. b
+
+  and div'_db cp bp bt a = Float 0. -. (bt *. cp /. bp)
+
+  and signum a =
+    let ff = function
+      | Float a -> Float Owl_maths.(signum a)
+      | Matrix a -> Matrix M.(signum a)
+    in
+    let fd a = signum a
+    in
+    let df cp ap at = zero ap
+    in
+    op_d_d a ff fd df
+
+  and sin a =
+    let ff = function
+      | Float a -> Float Pervasives.(sin a)
+      | Matrix a -> Matrix M.(sin a)
+    in
+    let fd a = sin a
+    in
+    let df cp ap at = at *. cos ap
+    in
+    op_d_d a ff fd df
+
+  and cos a =
+    let ff = function
+      | Float a -> Float Pervasives.(cos a)
+      | Matrix a -> Matrix M.(cos a)
+    in
+    let fd a = cos a
+    in
+    let df cp ap at = Float 0. -. (at *. sin ap)
+    in
+    op_d_d a ff fd df
+
+(*
+  and sin = function
+    | Float a -> Float Pervasives.(sin a)
+    | Matrix a -> Matrix M.(sin a)
+    | D a -> make_dual (sin a.p) ((sin' a.p a.p) *. a.t)
+
+  and sin' cp ap at = at *. cos ap
+
+  and cos = function
+    | Float a -> Float Pervasives.(cos a)
+    | Matrix a -> Matrix M.(cos a)
+    | D a -> make_dual (cos a.p) ((cos' a.p) *. a.t)
+
+  and cos' cp ap at = Float 0. -. (at *. sin ap)
+*)
 
 end
 
-module Add_ = struct
-  let f a b = a +. b
-  let df_da cp ap at = at
-  let df_db cp bp bt = bt
-  let df_dab cp ap at bp bt = at +. bt
-end
+let diff f = fun x ->
+  let x = make_dual x (one x) in
+  f x |> dual
