5d0352970b6131661978a66278256966cee7cc20
Liang Wang <ryanrhymes@gmail.com>
2017-02-20 18:46:53 +0000

    algodiff : experiment

--- lib/owl_algodiff_backward.ml
+++ lib/owl_algodiff_backward.ml
@@ -11,10 +11,10 @@ type mat = Owl_dense_real.mat
 (* type definitions *)
 
 type t =
-  | Float of float
+  | Float  of float
   | Matrix of mat
-  | DF of t * t * int                            (* primal, tangent, tag *)
-  | DR of t * t ref * trace_op * int ref * int   (* primal, adjoint, op, fanout, tag *)
+  | DF     of t * t * int                            (* primal, tangent, tag *)
+  | DR     of t * t ref * trace_op * int ref * int   (* primal, adjoint, op, fanout, tag *)
 and trace_op =
   | Noop
   | Add_D_D  of t * t
@@ -33,13 +33,13 @@ and trace_op =
   | Cos_D    of t
   | Signum_D of t
   | Item     of t * int * int
-  | Add_Item_D_D of t * int * int * t
-  | Add_Item_D_C of t * int * int * t
-  | Add_Item_C_D of t * int * int * t
+  | AddI_D_D of t * int * int * t
+  | AddI_D_C of t * int * int * t
+  | AddI_C_D of t * int * int * t
 
 
 let _global_tag = ref 0
-let new_tag () = _global_tag := !_global_tag + 1; !_global_tag
+let tag () = _global_tag := !_global_tag + 1; !_global_tag
 
 (* FIXME *)
 let cmp_tag ai bi =
@@ -72,8 +72,6 @@ let adjoint = function
   | DF _                -> failwith "error: no adjoint for DF"
   | DR (_, at, _, _, _) -> at
   | ap                  -> ref (zero ap)
-
-
 
 
 (* overload operators *)
@@ -132,6 +130,7 @@ module Maths = struct
       | Float a, Matrix b  -> Matrix M.(a $+ b)
       | Matrix a, Float b  -> Matrix M.(a +$ b)
       | Matrix a, Matrix b -> Matrix M.(a +@ b)
+      | _                 -> failwith "error: add: ff"
     in
     let fd a b = a +. b
     in
@@ -157,6 +156,7 @@ module Maths = struct
       | Float a, Matrix b  -> Matrix M.(a $- b)
       | Matrix a, Float b  -> Matrix M.(a -$ b)
       | Matrix a, Matrix b -> Matrix M.(a -@ b)
+      | _                  -> failwith "error: sub: ff"
     in
     let fd a b = a -. b
     in
@@ -182,6 +182,7 @@ module Maths = struct
       | Float a, Matrix b  -> Matrix M.(a $* b)
       | Matrix a, Float b  -> Matrix M.(a *$ b)
       | Matrix a, Matrix b -> Matrix M.(a *@ b)
+      | _                  -> failwith "error: mul: ff"
     in
     let fd a b = a *. b
     in
@@ -207,6 +208,7 @@ module Maths = struct
       | Float a, Matrix b  -> Matrix M.(a $/ b)
       | Matrix a, Float b  -> Matrix M.(a /$ b)
       | Matrix a, Matrix b -> Matrix M.(a /@ b)
+      | _                  -> failwith "error: div: ff"
     in
     let fd a b = a /. b
     in
@@ -226,8 +228,9 @@ module Maths = struct
 
   and signum a =
     let ff = function
-      | Float a -> Float Owl_maths.(signum a)
+      | Float a  -> Float Owl_maths.(signum a)
       | Matrix a -> Matrix M.(signum a)
+      | _        -> failwith "error: signum: ff"
     in
     let fd a = signum a
     in
@@ -239,8 +242,9 @@ module Maths = struct
 
   and sin a =
     let ff = function
-      | Float a -> Float S.(sin a)
+      | Float a  -> Float S.(sin a)
       | Matrix a -> Matrix M.(sin a)
+      | _        -> failwith "error: sin: ff"
     in
     let fd a = sin a
     in
@@ -252,8 +256,9 @@ module Maths = struct
 
   and cos a =
     let ff = function
-      | Float a -> Float S.(cos a)
+      | Float a  -> Float S.(cos a)
       | Matrix a -> Matrix M.(cos a)
+      | _        -> failwith "error: cos: ff"
     in
     let fd a = cos a
     in
@@ -268,19 +273,12 @@ module Maths = struct
     | Matrix ap            -> Float (M.get ap i j)
     | DF (ap, at, ai)      -> DF (item ap i j, item at i j, ai)
     | DR (ap, _, _, _, ai) -> DR (item ap i j, ref (Float 0.), Item (a, i, j), ref 0, ai)
-    | _                    -> failwith "error: item only applies to matrices"
-
-  and set_item a i j b =
-    match a, b with
-    | Matrix ap, Float bp -> ap.{i,j} <- bp
-    | _, _ -> failwith "error: set_item"
+    | _                    -> failwith "error: item"
 
   and add_item a i j b =
     let ff a b = match a, b with
       | Matrix a, Float b -> let aa = M.clone a in aa.{i,j} <- S.(aa.{i,j} +. b); Matrix aa
-      | Float a, Matrix b -> Printf.printf "%i %i %g\n" i j a; failwith "+++"
-      | Float a, Float b -> Printf.printf "%i %i %g %g\n" i j a b; failwith "==="
-      | Matrix a, Matrix b -> M.pp_dsmat a; M.pp_dsmat b; failwith "***"
+      | _                 -> failwith "error: add_item: ff"
     in
     let fd a b = add_item a i j b
     in
@@ -290,18 +288,18 @@ module Maths = struct
     in
     let df_dab cp ap at bp bt = add_item at i j bt
     in
-    let r_d_d a b = Add_Item_D_D (a, i, j, b)
+    let r_d_d a b = AddI_D_D (a, i, j, b)
     in
-    let r_d_c a b = Add_Item_D_C (a, i, j, b)
+    let r_d_c a b = AddI_D_C (a, i, j, b)
     in
-    let r_c_d a b = Add_Item_C_D (a, i, j, b)
+    let r_c_d a b = AddI_C_D (a, i, j, b)
     in
     op_d_d_d a b ff fd df_da df_db df_dab r_d_d r_d_c r_c_d
 
 end
 
 
-(* wrappers *)
+(* core of the reverse mode *)
 
 let reverse_reset x =
   let rec reset xs =
@@ -314,25 +312,25 @@ let reverse_reset x =
           af := !af + 1;
           if !af = 1 then (
             match ao with
-            | Add_D_D (a, b)            -> reset (a :: b :: t)
-            | Add_D_C (a, _)            -> reset (a :: t)
-            | Add_C_D (_, b)            -> reset (b :: t)
-            | Sub_D_D (a, b)            -> reset (a :: b :: t)
-            | Sub_D_C (a, _)            -> reset (a :: t)
-            | Sub_C_D (_, b)            -> reset (b :: t)
-            | Mul_D_D (a, b)            -> reset (a :: b :: t)
-            | Mul_D_C (a, _)            -> reset (a :: t)
-            | Mul_C_D (_, b)            -> reset (b :: t)
-            | Div_D_D (a, b)            -> reset (a :: b :: t)
-            | Div_D_C (a, _)            -> reset (a :: t)
-            | Div_C_D (_, b)            -> reset (b :: t)
-            | Sin_D a                   -> reset (a :: t)
-            | Cos_D a                   -> reset (a :: t)
-            | Item (a, _, _)            -> reset (a :: t)
-            | Add_Item_D_D (a, _, _, b) -> reset (a :: b :: t)
-            | Add_Item_D_C (a, _, _, _) -> reset (a :: t)
-            | Add_Item_C_D (_, _, _, b) -> reset (b :: t)
-            | _                         -> reset t
+            | Add_D_D (a, b)        -> reset (a :: b :: t)
+            | Add_D_C (a, _)        -> reset (a :: t)
+            | Add_C_D (_, b)        -> reset (b :: t)
+            | Sub_D_D (a, b)        -> reset (a :: b :: t)
+            | Sub_D_C (a, _)        -> reset (a :: t)
+            | Sub_C_D (_, b)        -> reset (b :: t)
+            | Mul_D_D (a, b)        -> reset (a :: b :: t)
+            | Mul_D_C (a, _)        -> reset (a :: t)
+            | Mul_C_D (_, b)        -> reset (b :: t)
+            | Div_D_D (a, b)        -> reset (a :: b :: t)
+            | Div_D_C (a, _)        -> reset (a :: t)
+            | Div_C_D (_, b)        -> reset (b :: t)
+            | Sin_D a               -> reset (a :: t)
+            | Cos_D a               -> reset (a :: t)
+            | Item (a, _, _)        -> reset (a :: t)
+            | AddI_D_D (a, _, _, b) -> reset (a :: b :: t)
+            | AddI_D_C (a, _, _, _) -> reset (a :: t)
+            | AddI_C_D (_, _, _, b) -> reset (b :: t)
+            | _                     -> reset t
             )
           else reset t
           )
@@ -353,25 +351,25 @@ let reverse_push v x =
           af := !af - 1;
           if !af = 0 then (
             match ao with
-            | Add_D_D (a, b) -> push ((!aa, a) :: (!aa, b) :: t)
-            | Add_D_C (a, _) -> push ((!aa, a) :: t)
-            | Add_C_D (_, b) -> push ((!aa, b) :: t)
-            | Sub_D_D (a, b) -> push ((!aa, a) :: (Float 0. -. !aa, b) :: t)
-            | Sub_D_C (a, _) -> push ((!aa, a) :: t)
-            | Sub_C_D (_, b) -> push ((Float 0. -. !aa, b) :: t)
-            | Mul_D_D (a, b) -> push (((!aa *. primal b), a) :: ((!aa *. primal a), b) :: t)
-            | Mul_D_C (a, b) -> push (((!aa *. b), a) :: t)
-            | Mul_C_D (a, b) -> push (((!aa *. a), b) :: t)
-            | Div_D_D (a, b) -> push (((!aa /. (primal b)), a) :: ((!aa *. ((Float 0. -. (primal a)) /. ((primal b) *. (primal b)))), b) :: t)
-            | Div_D_C (a, b) -> push (((!aa /. b), a) :: t)
-            | Div_C_D (a, b) -> push (((!aa *. ((Float 0. -. (primal a)) /. ((primal b) *. (primal b)))), b) :: t)
-            | Sin_D a        -> push (((!aa *. cos (primal a)), a) :: t)
-            | Cos_D a        -> push (((!aa *. (Float 0. -. sin (primal a))), a) :: t)
-            | Item (a, i, j) -> (adjoint a) := add_item !(adjoint a) i j !aa; push ((zero a, a) :: t)
-            | Add_Item_D_D (a, i, j, b) -> push ((!aa, a) :: (item !aa i j, b) :: t)
-            | Add_Item_D_C (a, _, _, _) -> push ((!aa, a) :: t)
-            | Add_Item_C_D (_, i, j, b) -> push ((item !aa i j, b) :: t)
-            | _              -> push t
+            | Add_D_D (a, b)        -> push ((!aa, a) :: (!aa, b) :: t)
+            | Add_D_C (a, _)        -> push ((!aa, a) :: t)
+            | Add_C_D (_, b)        -> push ((!aa, b) :: t)
+            | Sub_D_D (a, b)        -> push ((!aa, a) :: (Float 0. -. !aa, b) :: t)
+            | Sub_D_C (a, _)        -> push ((!aa, a) :: t)
+            | Sub_C_D (_, b)        -> push ((Float 0. -. !aa, b) :: t)
+            | Mul_D_D (a, b)        -> push (((!aa *. primal b), a) :: ((!aa *. primal a), b) :: t)
+            | Mul_D_C (a, b)        -> push (((!aa *. b), a) :: t)
+            | Mul_C_D (a, b)        -> push (((!aa *. a), b) :: t)
+            | Div_D_D (a, b)        -> push (((!aa /. (primal b)), a) :: ((!aa *. ((Float 0. -. (primal a)) /. ((primal b) *. (primal b)))), b) :: t)
+            | Div_D_C (a, b)        -> push (((!aa /. b), a) :: t)
+            | Div_C_D (a, b)        -> push (((!aa *. ((Float 0. -. (primal a)) /. ((primal b) *. (primal b)))), b) :: t)
+            | Sin_D a               -> push (((!aa *. cos (primal a)), a) :: t)
+            | Cos_D a               -> push (((!aa *. (Float 0. -. sin (primal a))), a) :: t)
+            | Item (a, i, j)        -> (adjoint a) := add_item !(adjoint a) i j !aa; push ((zero a, a) :: t)
+            | AddI_D_D (a, i, j, b) -> push ((!aa, a) :: (item !aa i j, b) :: t)
+            | AddI_D_C (a, _, _, _) -> push ((!aa, a) :: t)
+            | AddI_C_D (_, i, j, b) -> push ((item !aa i j, b) :: t)
+            | _                     -> push t
             )
           else push t
           )
@@ -380,16 +378,19 @@ let reverse_push v x =
   in
   push [(v, x)]
 
+
+(* convenient wrappers *)
+
 let make_forward p t i = DF (p, t, i)
 
 let make_reverse p i = DR (p, ref (zero p), Noop, ref 0, i)
 
 let diff f = fun x ->
-  let x = make_forward x (one x) (new_tag ()) in
+  let x = make_forward x (one x) (tag ()) in
   f x |> tangent
 
 let grad f = fun x ->
-  let x = make_reverse x (new_tag ()) in
+  let x = make_reverse x (tag ()) in
   let y = f x in
   reverse_reset y;
   reverse_push (Float 1.) y;
