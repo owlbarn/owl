5e0f3c68555a42d438276efc8615dd36799f08d4
Liang Wang <ryanrhymes@gmail.com>
2017-03-02 00:51:14 +0000

    owl type design

--- lib/type/owl_type_math_bin.ml
+++ lib/type/owl_type_math_bin.ml
@@ -38,6 +38,22 @@ type dac = (Complex.t, complex32_elt, c_layout) Genarray.t
 
 type daz = (Complex.t, complex64_elt, c_layout) Genarray.t
 
+type sms = (float, float32_elt) Owl_sparse_matrix.t
+
+type smd = (float, float64_elt) Owl_sparse_matrix.t
+
+type smc = (Complex.t, complex32_elt) Owl_sparse_matrix.t
+
+type smz = (Complex.t, complex64_elt) Owl_sparse_matrix.t
+
+type sas = (float, float32_elt) Owl_sparse_ndarray.t
+
+type sad = (float, float64_elt) Owl_sparse_ndarray.t
+
+type sac = (Complex.t, complex32_elt) Owl_sparse_ndarray.t
+
+type saz = (Complex.t, complex64_elt) Owl_sparse_ndarray.t
+
 
 type ('a, 'b, 'c, 'd) t =
   | F : float -> (num, flt, p64, dns) t
@@ -50,7 +66,14 @@ type ('a, 'b, 'c, 'd) t =
   | DAD : dad -> (arr, flt, p64, dns) t
   | DAC : dac -> (arr, cpx, p32, dns) t
   | DAZ : daz -> (arr, cpx, p64, dns) t
-
+  | SMS : sms -> (mat, flt, p32, sps) t
+  | SMD : smd -> (mat, flt, p64, sps) t
+  | SMC : sms -> (mat, cpx, p32, sps) t
+  | SMZ : smd -> (mat, cpx, p64, sps) t
+  | SAS : sas -> (arr, flt, p32, sps) t
+  | SAD : sad -> (arr, flt, p64, sps) t
+  | SAC : sac -> (arr, cpx, p32, sps) t
+  | SAZ : saz -> (arr, cpx, p64, sps) t
 
 
 (** some experiment code *)
