ada23da362e5884b482a94c5162dfb56862f7be2
Liang Wang <ryanrhymes@gmail.com>
2017-03-05 00:24:33 +0000

    typing experiment

--- lib/type/owl_ext_dense_matrix_z.ml
+++ lib/type/owl_ext_dense_matrix_z.ml
@@ -16,6 +16,9 @@ let _elt = Complex64
 let pack_elt x = C x
 let unpack_elt = function C x -> x | _ -> failwith "error: unknown type"
 
+let pack_elt_f x = F x
+let unpack_elt_f = function F x -> x | _ -> failwith "error: unknown type"
+
 let pack_box x = DMZ x
 let unpack_box = function DMZ x -> x | _ -> failwith "error: unknown type"
 
@@ -111,11 +114,162 @@ let tile x reps = M.tile (unpack_box x) reps |> pack_box
 let repeat ?axis x reps = M.repeat ?axis (unpack_box x) reps |> pack_box
 
 
+let iteri f x = M.iteri f (unpack_box x)
+
+let iter f x = M.iter f (unpack_box x)
+
+let mapi f x = M.mapi f (unpack_box x) |> pack_box
+
+let map f x = M.mapi f (unpack_box x) |> pack_box
+
+let map2i f x y = M.map2i f (unpack_box x) (unpack_box y) |> pack_box
+
+let map2 f x y = M.map2 f (unpack_box x) (unpack_box y) |> pack_box
+
+let foldi f a x = M.foldi f a (unpack_box x)
+
+let fold f a x = M.fold f a (unpack_box x)
+
+let filteri f x = M.filteri f (unpack_box x)
+
+let filter f x = M.filter f (unpack_box x)
+
+let iteri_rows f x = M.iteri_rows f (unpack_box x)
+
+let iter_rows f x = M.iter_rows f (unpack_box x)
+
+let iteri_cols f x = M.iteri_cols f (unpack_box x)
+
+let iter_cols f x = M.iter_cols f (unpack_box x)
+
+let filteri_rows f x = M.filteri_rows f (unpack_box x)
+
+let filter_rows f x = M.filter_rows f (unpack_box x)
+
+let filteri_cols f x = M.filteri_cols f (unpack_box x)
+
+let filter_cols f x = M.filter_cols f (unpack_box x)
+
+let fold_rows f a x = M.fold_rows f a (unpack_box x)
+
+let fold_cols f a x = M.fold_cols f a (unpack_box x)
+
+let mapi_rows f x = M.mapi_rows f (unpack_box x)
+
+let map_rows f x = M.map_rows f (unpack_box x)
+
+let mapi_cols f x = M.mapi_cols f (unpack_box x)
+
+let map_cols f x = M.map_cols f (unpack_box x)
+
+let mapi_by_row d f x = M.mapi_by_row d f (unpack_box x) |> pack_box
+
+let map_by_row d f x = M.map_by_row d f (unpack_box x) |> pack_box
+
+let mapi_by_col d f x = M.mapi_by_col d f (unpack_box x) |> pack_box
+
+let map_by_col d f x = M.map_by_col d f (unpack_box x) |> pack_box
+
+let mapi_at_row f x i = M.mapi_at_row f (unpack_box x) i |> pack_box
+
+let map_at_row f x i = M.map_at_row f (unpack_box x) i |> pack_box
+
+let mapi_at_col f x j = M.mapi_at_col f (unpack_box x) j |> pack_box
+
+let map_at_col f x j = M.map_at_col f (unpack_box x) j |> pack_box
+
+
+let exists f x = M.exists f (unpack_box x)
+
+let not_exists f x = M.not_exists f (unpack_box x)
+
+let for_all f x = M.for_all f (unpack_box x)
+
+let is_zero x = M.is_zero (unpack_box x)
+
+let is_positive x = M.is_positive (unpack_box x)
+
+let is_negative x = M.is_negative (unpack_box x)
+
+let is_nonpositive x = M.is_nonpositive (unpack_box x)
+
+let is_nonnegative x = M.is_nonnegative (unpack_box x)
+
+let is_equal x y = M.is_equal (unpack_box x) (unpack_box y)
+
+let is_unequal x y = M.is_unequal (unpack_box x) (unpack_box y)
+
+let is_greater x y = M.is_greater (unpack_box x) (unpack_box y)
+
+let is_smaller x y = M.is_smaller (unpack_box x) (unpack_box y)
+
+let equal_or_greater x y = M.equal_or_greater (unpack_box x) (unpack_box y)
+
+let equal_or_smaller x y = M.equal_or_smaller (unpack_box x) (unpack_box y)
+
+
+let draw_rows ?(replacement=true) x c = let r, i = M.draw_rows ~replacement (unpack_box x) c in (pack_box r), i
+
+let draw_cols ?(replacement=true) x c = let r, i = M.draw_cols ~replacement (unpack_box x) c in (pack_box r), i
+
+let shuffle_rows x = M.shuffle_rows (unpack_box x)
+
+let shuffle_cols x = M.shuffle_cols (unpack_box x)
+
+let shuffle x = M.shuffle (unpack_box x)
+
+
+let to_array x = M.to_array (unpack_box x)
+
+let of_array x m n = M.of_array _elt x m n |> pack_box
+
+let to_arrays x = M.to_arrays (unpack_box x)
+
+let of_arrays x = M.of_arrays _elt x |> pack_box
+
+let print x = M.print (unpack_box x)
+
+let pp_dsmat x = M.pp_dsmat (unpack_box x)
+
+let save x f = M.save (unpack_box x) f
+
+let load f = M.load _elt f |> pack_box
+
+
+let min x = M.min (unpack_box x) |> pack_elt
+
+let max x = M.max (unpack_box x) |> pack_elt
+
+let minmax x = let (a, b) = M.minmax (unpack_box x) in (pack_elt a, pack_elt b)
+
+let trace x = M.trace (unpack_box x) |> pack_elt
+
+let sum x = M.sum (unpack_box x) |> pack_elt
+
+let prod x = M.prod (unpack_box x) |> pack_elt
+
+let average x = M.average (unpack_box x) |> pack_elt
+
+let sum_rows x = M.sum_rows (unpack_box x) |> pack_box
+
+let sum_cols x = M.sum_rows (unpack_box x) |> pack_box
+
+let average_rows x = M.sum_rows (unpack_box x) |> pack_box
+
+let average_cols x = M.sum_rows (unpack_box x) |> pack_box
+
 
 let neg x = M.neg (unpack_box x) |> pack_box
 
 let reci x = M.reci (unpack_box x) |> pack_box
 
+let sqr_nrm2 x = M.sqr_nrm2 (unpack_box x) |> pack_elt_f
+
+let l1norm x = M.l1norm (unpack_box x) |> pack_elt_f
+
+let l2norm x = M.l2norm (unpack_box x) |> pack_elt_f
+
+let l2norm_sqr x = M.l2norm_sqr (unpack_box x) |> pack_elt_f
 
 
 let add x y = M.add (unpack_box x) (unpack_box y) |> pack_box
@@ -128,12 +282,33 @@ let div x y = M.div (unpack_box x) (unpack_box y) |> pack_box
 
 let add_scalar x a = M.add_scalar (unpack_box x) (unpack_elt a) |> pack_box
 
-let sub_scalar x a = M.sub_scalar (unpack_box x) (unpack_elt a) |> pack_box
+let sub_scalar x a = M.add_scalar (unpack_box x) (unpack_elt a) |> pack_box
 
-let mul_scalar x a = M.mul_scalar (unpack_box x) (unpack_elt a) |> pack_box
+let mul_scalar x a = M.add_scalar (unpack_box x) (unpack_elt a) |> pack_box
 
-let div_scalar x a = M.div_scalar (unpack_box x) (unpack_elt a) |> pack_box
+let div_scalar x a = M.add_scalar (unpack_box x) (unpack_elt a) |> pack_box
 
+let pow x y = M.pow (unpack_box x) (unpack_box y) |> pack_box
+
+let pow0 a x = M.pow0 (unpack_elt a) (unpack_box x) |> pack_box
+
+let pow1 x a = M.pow1 (unpack_box x) (unpack_elt a) |> pack_box
+
+let atan2 x y = M.atan2 (unpack_box x) (unpack_box y) |> pack_box
+
+let atan20 a x = M.atan20 (unpack_elt a) (unpack_box x) |> pack_box
+
+let atan21 x a = M.atan21 (unpack_box x) (unpack_elt a) |> pack_box
+
+let hypot x y = M.hypot (unpack_box x) (unpack_box y) |> pack_box
+
+let min2 x y = M.min2 (unpack_box x) (unpack_box y) |> pack_box
+
+let max2 x y = M.max2 (unpack_box x) (unpack_box y) |> pack_box
+
+let ssqr x a = M.ssqr (unpack_box x) (unpack_elt a) |> pack_elt
+
+let ssqr_diff x y = M.ssqr_diff (unpack_box x) (unpack_box y) |> pack_elt
 
 
 (* ends here *)
