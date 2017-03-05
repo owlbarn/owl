1000474b9718a7786861ebd415e3991695824ebf
Liang Wang <ryanrhymes@gmail.com>
2017-03-05 01:39:13 +0000

    type experiment

---
+++ lib/ext/owl_ext_lift.ml
@@ -0,0 +1,83 @@
+(*
+ * OWL - an OCaml numerical library for scientific computing
+ * Copyright (c) 2016-2017 Liang Wang <liang.wang@cl.cam.ac.uk>
+ *)
+
+
+(* lift precision *)
+
+module DAS_DAD = struct
+
+  let lift x = None
+
+end
+
+
+module DAC_DAZ = struct
+
+  let lift x = None
+
+end
+
+
+module DMS_DMD = struct
+
+  let lift x = None
+
+end
+
+
+module DMC_DMZ = struct
+
+  let lift x = None
+
+end
+
+
+(* lift element type *)
+
+module DAS_DAC = struct
+
+  let lift x = None
+
+end
+
+
+module DAD_DAZ = struct
+
+  let lift x = None
+
+end
+
+
+module DMS_DMC = struct
+
+  let lift x = None
+
+end
+
+
+module DMD_DMZ = struct
+
+  let lift x = None
+
+end
+
+
+(* lift both precision and element type *)
+
+module DAS_DAZ = struct
+
+  let lift x = None
+
+end
+
+
+module DMS_DMZ = struct
+
+  let lift x = None
+
+end
+
+
+(* ends here *)
