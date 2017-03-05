e3493b993ac29cd91bb4075fc0692a8e6b3fb0fc
Liang Wang <ryanrhymes@gmail.com>
2017-03-05 01:17:05 +0000

    typing experiment

--- lib/type/owl_ext_binop.ml
+++ lib/type/owl_ext_binop.ml
@@ -1,6 +1,11 @@
+(*
+ * OWL - an OCaml numerical library for scientific computing
+ * Copyright (c) 2016-2017 Liang Wang <liang.wang@cl.cam.ac.uk>
+ *)
 
 open Owl_ext_types
 
+(* trivial cases *)
 
 module F_F = struct
 
@@ -49,15 +54,16 @@ module C_C = struct
 
 end
 
+(* real numbers *)
 
-module DAS_DAS = struct
+module F_DAS = struct
 
   module M = Owl_ext_dense_ndarray.S
 
-  let ( + ) x y = M.add x y
-  let ( - ) x y = M.sub x y
-  let ( * ) x y = M.mul x y
-  let ( / ) x y = M.div x y
+  let ( + ) a x = M.add_scalar x a
+  let ( - ) a x = M.(sub_scalar x a |> neg)
+  let ( * ) a x = M.mul_scalar x a
+  let ( / ) a x = M.(mul_scalar (reci x) a)
 
 end
 
@@ -74,9 +80,21 @@ module DAS_F = struct
 end
 
 
-module F_DAS = struct
+module DAS_DAS = struct
 
   module M = Owl_ext_dense_ndarray.S
+
+  let ( + ) x y = M.add x y
+  let ( - ) x y = M.sub x y
+  let ( * ) x y = M.mul x y
+  let ( / ) x y = M.div x y
+
+end
+
+
+module F_DAD = struct
+
+  module M = Owl_ext_dense_ndarray.D
 
   let ( + ) a x = M.add_scalar x a
   let ( - ) a x = M.(sub_scalar x a |> neg)
@@ -85,27 +103,27 @@ module F_DAS = struct
 
 end
 
-(* FIXME : lift type *)
-module C_DAS = struct
 
-  module M = Owl_ext_dense_ndarray.S
+module DAD_F = struct
 
-  let ( + ) a x = M.add_scalar x a
-  let ( - ) a x = M.(sub_scalar x a |> neg)
-  let ( * ) a x = M.mul_scalar x a
-  let ( / ) a x = M.(mul_scalar (reci x) a)
+  module M = Owl_ext_dense_ndarray.D
+
+  let ( + ) x a = M.add_scalar x a
+  let ( - ) x a = M.sub_scalar x a
+  let ( * ) x a = M.mul_scalar x a
+  let ( / ) x a = M.div_scalar x a
 
 end
 
-(* FIXME : lift type *)
-module DAS_C = struct
 
-  module M = Owl_ext_dense_ndarray.S
+module DAD_DAD = struct
 
-  let ( + ) a x = M.add_scalar x a
-  let ( - ) a x = M.(sub_scalar x a |> neg)
-  let ( * ) a x = M.mul_scalar x a
-  let ( / ) a x = M.(mul_scalar (reci x) a)
+  module M = Owl_ext_dense_ndarray.D
+
+  let ( + ) x y = M.add x y
+  let ( - ) x y = M.sub x y
+  let ( * ) x y = M.mul x y
+  let ( / ) x y = M.div x y
 
 end
 
@@ -146,6 +164,187 @@ module DMS_DMS = struct
 end
 
 
+module F_DMD = struct
+
+  module M = Owl_ext_dense_matrix.D
+
+  let ( + ) a x = M.add_scalar x a
+  let ( - ) a x = M.(sub_scalar x a |> neg)
+  let ( * ) a x = M.mul_scalar x a
+  let ( / ) a x = M.(mul_scalar (reci x) a)
+
+end
+
+
+module DMD_F = struct
+
+  module M = Owl_ext_dense_matrix.D
+
+  let ( + ) x a = M.add_scalar x a
+  let ( - ) x a = M.sub_scalar x a
+  let ( * ) x a = M.mul_scalar x a
+  let ( / ) x a = M.div_scalar x a
+
+end
+
+
+module DMD_DMD = struct
+
+  module M = Owl_ext_dense_matrix.D
+
+  let ( + ) x y = M.add x y
+  let ( - ) x y = M.sub x y
+  let ( * ) x y = M.mul x y
+  let ( / ) x y = M.div x y
+
+end
+
+(* complex numbers *)
+
+module C_DAC = struct
+
+  module M = Owl_ext_dense_ndarray.C
+
+  let ( + ) a x = M.add_scalar x a
+  let ( - ) a x = M.(sub_scalar x a |> neg)
+  let ( * ) a x = M.mul_scalar x a
+  let ( / ) a x = M.(mul_scalar (reci x) a)
+
+end
+
+
+module DAC_C = struct
+
+  module M = Owl_ext_dense_ndarray.C
+
+  let ( + ) x a = M.add_scalar x a
+  let ( - ) x a = M.sub_scalar x a
+  let ( * ) x a = M.mul_scalar x a
+  let ( / ) x a = M.div_scalar x a
+
+end
+
+
+module DAC_DAC = struct
+
+  module M = Owl_ext_dense_ndarray.C
+
+  let ( + ) x y = M.add x y
+  let ( - ) x y = M.sub x y
+  let ( * ) x y = M.mul x y
+  let ( / ) x y = M.div x y
+
+end
+
+
+module C_DAZ = struct
+
+  module M = Owl_ext_dense_ndarray.Z
+
+  let ( + ) a x = M.add_scalar x a
+  let ( - ) a x = M.(sub_scalar x a |> neg)
+  let ( * ) a x = M.mul_scalar x a
+  let ( / ) a x = M.(mul_scalar (reci x) a)
+
+end
+
+
+module DAZ_C = struct
+
+  module M = Owl_ext_dense_ndarray.Z
+
+  let ( + ) x a = M.add_scalar x a
+  let ( - ) x a = M.sub_scalar x a
+  let ( * ) x a = M.mul_scalar x a
+  let ( / ) x a = M.div_scalar x a
+
+end
+
+
+module DAZ_DAZ = struct
+
+  module M = Owl_ext_dense_ndarray.Z
+
+  let ( + ) x y = M.add x y
+  let ( - ) x y = M.sub x y
+  let ( * ) x y = M.mul x y
+  let ( / ) x y = M.div x y
+
+end
+
+
+module C_DMC = struct
+
+  module M = Owl_ext_dense_matrix.C
+
+  let ( + ) a x = M.add_scalar x a
+  let ( - ) a x = M.(sub_scalar x a |> neg)
+  let ( * ) a x = M.mul_scalar x a
+  let ( / ) a x = M.(mul_scalar (reci x) a)
+
+end
+
+
+module DMC_C = struct
+
+  module M = Owl_ext_dense_matrix.C
+
+  let ( + ) x a = M.add_scalar x a
+  let ( - ) x a = M.sub_scalar x a
+  let ( * ) x a = M.mul_scalar x a
+  let ( / ) x a = M.div_scalar x a
+
+end
+
+
+module DMC_DMC = struct
+
+  module M = Owl_ext_dense_matrix.C
+
+  let ( + ) x y = M.add x y
+  let ( - ) x y = M.sub x y
+  let ( * ) x y = M.mul x y
+  let ( / ) x y = M.div x y
+
+end
+
+
+module C_DMZ = struct
+
+  module M = Owl_ext_dense_matrix.Z
+
+  let ( + ) a x = M.add_scalar x a
+  let ( - ) a x = M.(sub_scalar x a |> neg)
+  let ( * ) a x = M.mul_scalar x a
+  let ( / ) a x = M.(mul_scalar (reci x) a)
+
+end
+
+
+module DMZ_C = struct
+
+  module M = Owl_ext_dense_matrix.Z
+
+  let ( + ) x a = M.add_scalar x a
+  let ( - ) x a = M.sub_scalar x a
+  let ( * ) x a = M.mul_scalar x a
+  let ( / ) x a = M.div_scalar x a
+
+end
+
+
+module DMZ_DMZ = struct
+
+  module M = Owl_ext_dense_matrix.Z
+
+  let ( + ) x y = M.add x y
+  let ( - ) x y = M.sub x y
+  let ( * ) x y = M.mul x y
+  let ( / ) x y = M.div x y
+
+end
+
+
 (* overload binary operators *)
 
 let ( + ) x y = match x, y with
@@ -155,13 +354,56 @@ let ( + ) x y = match x, y with
   | C x, C y     -> C_C.(x + y)
   | F _, DAS _   -> F_DAS.(x + y)
   | DAS _, F _   -> DAS_F.(x + y)
-  | C _, DAS _   -> C_DAS.(x + y)
-  | DAS _, C _   -> DAS_C.(x + y)
   | DAS _, DAS _ -> DAS_DAS.(x + y)
+  | F _, DAD _   -> F_DAD.(x + y)
+  | DAD _, F _   -> DAD_F.(x + y)
+  | DAD _, DAD _ -> DAD_DAD.(x + y)
   | F _, DMS _   -> F_DMS.(x + y)
   | DMS _, F _   -> DMS_F.(x + y)
   | DMS _, DMS _ -> DMS_DMS.(x + y)
+  | F _, DMD _   -> F_DMD.(x + y)
+  | DMD _, F _   -> DMD_F.(x + y)
+  | DMD _, DMD _ -> DMD_DMD.(x + y)
+  | C _, DAC _   -> C_DAC.(x + y)
+  | DAC _, C _   -> DAC_C.(x + y)
+  | DAC _, DAC _ -> DAC_DAC.(x + y)
+  | C _, DAZ _   -> C_DAZ.(x + y)
+  | DAZ _, C _   -> DAZ_C.(x + y)
+  | DAZ _, DAZ _ -> DAZ_DAZ.(x + y)
+  | C _, DMC _   -> C_DMC.(x + y)
+  | DMC _, C _   -> DMC_C.(x + y)
+  | DMC _, DMC _ -> DMC_DMC.(x + y)
+  | C _, DMZ _   -> C_DMZ.(x + y)
+  | DMZ _, C _   -> DMZ_C.(x + y)
+  | DMZ _, DMZ _ -> DMZ_DMZ.(x + y)
   | _ -> failwith "( + ) : unknown type"
+
+
+
+(* FIXME : lift type *)
+module C_DAS = struct
+
+  module M = Owl_ext_dense_ndarray.S
+
+  let ( + ) a x = M.add_scalar x a
+  let ( - ) a x = M.(sub_scalar x a |> neg)
+  let ( * ) a x = M.mul_scalar x a
+  let ( / ) a x = M.(mul_scalar (reci x) a)
+
+end
+
+
+(* FIXME : lift type *)
+module DAS_C = struct
+
+  module M = Owl_ext_dense_ndarray.S
+
+  let ( + ) a x = M.add_scalar x a
+  let ( - ) a x = M.(sub_scalar x a |> neg)
+  let ( * ) a x = M.mul_scalar x a
+  let ( / ) a x = M.(mul_scalar (reci x) a)
+
+end
 
 
 
