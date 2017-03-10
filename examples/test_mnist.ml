7d1bd510b4ab07888d3b54774cb05726f1e37251
Liang Wang <ryanrhymes@gmail.com>
2017-03-10 16:36:37 +0000

    experiment AD

--- examples/test_mnist.ml
+++ examples/test_mnist.ml
@@ -12,13 +12,19 @@ let print_random x m =
   let open Owl in
   Mat.draw_rows x m |> fst |> Mat.iter_rows print_image
 
-let load_train_data () =
+let load_mnist_train_data () =
   let open Owl in
   let p = Owl_utils.local_data_path () in
   Mat.load (p ^ "mnist-train-images"),
   Mat.load (p ^ "mnist-train-labels")
   |> Mat.transpose
   |> Mat.map_by_row 10 (fun a -> Vec.unit_basis ~typ:Row 10 (int_of_float a.{0,0}))
+
+let load_mnist_test_data () =
+  let open Owl in
+  let p = Owl_utils.local_data_path () in
+  Mat.load (p ^ "mnist-test-images"),
+  Mat.load (p ^ "mnist-test-labels") |> Mat.transpose
 
 let draw_samples x y n =
   let open Owl in
@@ -88,8 +94,8 @@ let backprop' nn eta epoch x y =
       l.w <- make_reverse l.w t;
       l.b <- make_reverse l.b t;
     ) nn.layers;
-    Maths.(cross_entropy y (run_network x nn) / (F (Mat.row_num x |> float_of_int)))
-    |> reverse_prop (F 1.);
+    let loss = Maths.(cross_entropy y (run_network x nn) / (F (Mat.row_num x |> float_of_int))) in
+    reverse_prop (F 1.) loss;
     Array.iter (fun l ->
       l.w <- Maths.(primal ((primal l.w) - (eta * (adjval l.w))));
       l.b <- Maths.(primal ((primal l.b) - (eta * (adjval l.b))));
@@ -99,9 +105,21 @@ let backprop' nn eta epoch x y =
     | _ -> print_endline "error"
   done
 
+let test_model nn x y =
+  Mat.iter2_rows (fun u v ->
+    print_image (unpack_mat u);
+    let p = run_network u nn |> unpack_mat in
+    Owl.Mat.print p;
+    Printf.printf "prediction: %i\n" (let _, _, j = Owl.Mat.max_i p in j);
+    flush_all ()
+  ) x y
+
 
 let _ =
   print_endline "test MNIST";
-  let x, y = load_train_data () in
+  let x, y = load_mnist_train_data () in
   let x, y = draw_samples x y 1000 in
-  backprop' nn (F 0.01) 50 (Mat x) (Mat y)
+  backprop' nn (F 0.01) 50 (Mat x) (Mat y);
+  let x, y = load_mnist_test_data () in
+  let x, y = draw_samples x y 10 in
+  test_model nn (Mat x) (Mat y)
