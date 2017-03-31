c65b4215a8f956e69c96eeebc3af309002d51333
Liang Wang <ryanrhymes@gmail.com>
2017-04-01 00:52:38 +0100

    redesign owl_neural

--- lib/neural/owl_neural.ml
+++ lib/neural/owl_neural.ml
@@ -6,20 +6,25 @@
 open Owl_algodiff_ad
 type t = Owl_algodiff_ad.t
 
-type init_typ =
-  | Init_Uniform of float * float
-  | Init_Gaussian of float * float
-  | Init_Custom of (int -> int -> float)
 
-let initialise t m n = match t with
-  | Init_Uniform (a, b) -> Mat.uniform m n
-  | Init_Gaussian (a, b) -> Mat.uniform m n
-  | Init_Custom f -> Mat.uniform m n
+module Init = struct
 
-type activation =
-  | Sigmoid
-  | Tanh
-  | Custom_Act of (t -> t)
+  type typ =
+    | Uniform  of float * float
+    | Gaussian of float * float
+    | Custom   of (int -> int -> float)
+
+  let initialise t m n = match t with
+    | Uniform (a, b)  -> Mat.uniform m n
+    | Gaussian (a, b) -> Mat.uniform m n
+    | Custom f        -> Mat.uniform m n
+
+  let to_string = function
+    | Uniform (a, b)  -> Printf.sprintf "uniform (%g, %g)" a b
+    | Gaussian (a, b) -> Printf.sprintf "gaussian (%g, %g)" a b
+    | Custom _        -> Printf.sprintf "customise"
+
+end
 
 
 module Linear = struct
@@ -27,7 +32,7 @@ module Linear = struct
   type layer = {
     mutable w : t;
     mutable b : t;
-    mutable init_typ : init_typ;
+    mutable init_typ : Init.typ;
   }
 
   let create m n init_typ = {
@@ -38,7 +43,7 @@ module Linear = struct
 
   let init l =
     let m, n = Mat.shape l.w in
-    l.w <- initialise l.init_typ m n;
+    l.w <- Init.initialise l.init_typ m n;
     l.b <- Mat.zeros 1 n
 
   let reset l =
@@ -47,6 +52,17 @@ module Linear = struct
 
   let run x l = Maths.(x * l.w + l.b)
 
+  let to_string l =
+    let wm, wn = Mat.shape l.w in
+    let bn = Mat.col_num l.b in
+    Printf.sprintf "Linear layer:
+    init : %s
+    params : %i
+    w : %i x %i
+    b : %i
+    "
+    (Init.to_string l.init_typ) (wm * wn + bn) wm wn bn
+
 end
 
 
@@ -54,7 +70,7 @@ module LTSM = struct
 
   type layer = {
     mutable wxi : t;
-    mutable init_typ : init_typ;
+    mutable init_typ : Init.typ;
   }
 
   let create init_typ = {
@@ -77,7 +93,7 @@ module Recurrent = struct
   type layer = {
     mutable w : t;
     mutable b : t;
-    mutable init_typ : init_typ;
+    mutable init_typ : Init.typ;
   }
 
   let create init_typ = {
@@ -125,11 +141,6 @@ let linear ~inputs ~outputs ~init_typ = Linear (Linear.create inputs outputs ini
 
 (* helper functions *)
 
-let _init_info = function
-  | Init_Uniform (a, b)  -> Printf.sprintf "uniform (%g, %g)" a b
-  | Init_Gaussian (a, b) -> Printf.sprintf "gaussian (%g, %g)" a b
-  | Init_Custom _        -> Printf.sprintf "customise"
-
 let _activation_info = function
   | Sigmoid      -> "sigmoid"
   | Tanh         -> "tanh"
@@ -137,18 +148,7 @@ let _activation_info = function
   | _            -> failwith "owl_neural._activation_info"
 
 let _layer_info = function
-  | Linear l    -> (
-      let open Linear in
-      let wm, wn = Mat.shape l.w in
-      let bn = Mat.col_num l.b in
-      Printf.sprintf "Linear layer:
-      init : %s
-      params : %i
-      w : %i x %i
-      b : %i
-      "
-      (_init_info l.init_typ) (wm * wn + bn) wm wn bn
-    )
+  | Linear l    -> Linear.to_string l
   | Recurrent _ -> "Recurrent"
   | LTSM _      -> "LTSM"
   | act  -> Printf.sprintf "Activation layer: %s\n" (_activation_info act)
