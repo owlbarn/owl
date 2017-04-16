da35a105bf791da249cba578ed3a9c137afa66a6
Liang Wang <ryanrhymes@gmail.com>
2017-04-17 00:22:03 +0100

    add norm to tfidf

--- examples/test_tfidf.ml
+++ examples/test_tfidf.ml
@@ -6,7 +6,7 @@ open Owl_nlp
 let _ =
   let stopwords = Dataset.load_stopwords () in
   let x = Corpus.build ~stopwords "/Users/liang/owl_dataset/news.train" in
-  let m = Tfidf.build x in
+  let m = Tfidf.build ~norm:true x in
   Tfidf.save m "news.tfidf"
 
   (* let x = Corpus.load "/Users/liang/owl_dataset/news.train.dat" in
