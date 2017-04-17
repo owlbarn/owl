deb79afb4b324fd2ffdf89a2cbfaaaa1ad2b5c3e
Bagmanov Pavel <bagman-2008@yandex.ru>
2017-04-17 20:51:39 +0300

    fix unit test

--- unittest/unit_stats.ml
+++ unittest/unit_stats.ml
@@ -1,19 +1,19 @@
 
 module M = Owl_stats
 
-
+let eps = 0.0000000001
 
 (* a module with functions to test *)
 module To_test = struct
-  let fisher_test_both_side alpha a b c d =
-    let (check, _, _) = M.fisher_test ~alpha:alpha a b c d in
-    check
-  let fisher_test_right_side alpha a b c d =
-    let (check, _, _) = M.fisher_test ~alpha:alpha ~side:M.RightSide a b c d in
-    check
-  let fisher_test_left_side alpha a b c d =
-    let (check, _, _) = M.fisher_test ~alpha:alpha ~side:M.LeftSide a b c d in
-    check
+  let fisher_test_both_side a b c d =
+    let (_, p, _) = M.fisher_test a b c d in
+    p
+  let fisher_test_right_side a b c d =
+    let (_, p, _) = M.fisher_test ~side:M.RightSide a b c d in
+    p
+  let fisher_test_left_side a b c d =
+    let (_, p, _) = M.fisher_test ~side:M.LeftSide a b c d in
+    p
 end
 
 
@@ -21,23 +21,20 @@ end
 let fisher_test_both_side () =
   Alcotest.(check bool)
     "fisher_test_both_side"
-    false
-    (To_test.fisher_test_both_side 0.05 45 25 10 15)
+    true
+    (abs_float((To_test.fisher_test_both_side 45 25 10 15) -. 0.057776279489461277) < eps)
 
 let fisher_test_right_side () =
   Alcotest.(check bool)
     "fisher_test_right_side"
     true
-    (To_test.fisher_test_right_side 0.05 45 25 10 15)
+    (abs_float((To_test.fisher_test_right_side 45 25 10 15) -. 0.0307852082455) < eps)
 
 let fisher_test_left_side () =
   Alcotest.(check bool)
     "fisher_test_left_side"
-    false
-    (To_test.fisher_test_left_side 0.05 45 25 10 15)
-
-
-
+    true
+    (abs_float((To_test.fisher_test_left_side 45 25 10 15) -. 0.990376800656) < eps)
 
 (* The tests *)
 let test_set = [
