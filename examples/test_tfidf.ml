4f5e45e34cc2e2488d7971a18afeb43a55a2ce4e
Liang Wang <ryanrhymes@gmail.com>
2017-04-17 19:46:42 +0100

    add corpus.unique fun

--- examples/test_tfidf.ml
+++ examples/test_tfidf.ml
@@ -12,3 +12,8 @@ let _ =
   (* let x = Corpus.load "/Users/liang/owl_dataset/news.train.dat" in
   let m = Tfidf.build x in
   Tfidf.save m "news.tfidf" *)
+
+  (* This is fast, achieves 120k docs/s on my slow macbook.
+  let x = "/Users/liang/owl_dataset/news.train" in
+  let y = "/Users/liang/owl_dataset/news.train.reduced" in
+  Corpus.unique x y *)
