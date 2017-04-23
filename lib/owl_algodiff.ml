855cff7671f7df235213bdeede644c4b15850580
Liang Wang <ryanrhymes@gmail.com>
2017-04-24 00:19:48 +0100

    adapt to new algodiff module

--- lib/owl_algodiff.ml
+++ lib/owl_algodiff.ml
@@ -5,10 +5,16 @@
 
 (** Algorithmic differentiation *)
 
-(* Alias of modules of algorithmic differentiation. *)
+(* Alias modules of algorithmic differentiation. *)
 
+
+(* AD module of Float32 type *)
 module S = Owl_algodiff_generic.Make (Owl_dense_matrix_s)
 
+
+(* AD module of Float64 type *)
 module D = Owl_algodiff_generic.Make (Owl_dense_matrix_d)
 
+
+(* Numerical differentiation *)
 module Numerical = Owl_algodiff_numerical
