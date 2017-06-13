7a529e281be92c7c7d292771f63a3bc3b3d0351f
Liang Wang <ryanrhymes@gmail.com>
2017-06-13 21:05:12 +0100

    add lapacke file

---
+++ lib/owl_lapacke.ml
@@ -0,0 +1,8 @@
+(* auto-generated lapacke interface file, timestamp:1497381661 *)
+
+open Ctypes
+
+external sbdsdc
+  : int -> char -> char -> int -> float ptr -> float ptr -> float ptr -> int -> float ptr -> int -> float ptr -> int ptr -> unit 
+ = "owl_stub_1_LAPACKE_sbdsdc_byte12" "owl_stub_1_LAPACKE_sbdsdc"
+
