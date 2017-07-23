9464c4c4c41d48e1d4145d805abfad8e2c14d377
Liang Wang <ryanrhymes@gmail.com>
2017-07-23 21:21:57 +0300

    design model zoo

--- lib/zoo/owl_zoo_specs_neural.ml
+++ lib/zoo/owl_zoo_specs_neural.ml
@@ -13,7 +13,7 @@ module ST = struct
 
   include Owl_zoo_specs_neural_t
 
-  let param
+  let make_param
     ?in_shape
     ?out_shape
     ?activation_typ
@@ -27,15 +27,65 @@ module ST = struct
 end
 
 
+module Input = struct
+
+  let to_specs x =
+    let typ = ST.(`Input) in
+    let param = ST.make_param in
+    typ, param ()
+
+  let of_specs spec = None
+
+end
+
+
 module Linear = struct
 
   let to_specs x =
     let typ = ST.(`Linear) in
-    let param = ST.param () in
-    typ, param
-
+    let param = ST.make_param in
+    typ, param ()
 
   let of_specs spec = None
+
+end
+
+
+module Activation = struct
+
+  let to_specs x =
+    let open NN.Activation in
+    let typ = ST.(`Activation) in
+    let param =
+      match x.activation with
+      | Relu    -> ST.(make_param ~activation_typ:`Relu)
+      | Sigmoid -> ST.(make_param ~activation_typ:`Sigmoid)
+      | Softmax -> ST.(make_param ~activation_typ:`Softmax)
+      | Tanh    -> ST.(make_param ~activation_typ:`Tanh)
+      | None    -> ST.(make_param ~activation_typ:`None)
+      | _       -> failwith "owl_zoo_specs_neural:activation:to_specs"
+    in
+    typ, param ()
+
+  let of_specs spec = None
+
+end
+
+
+
+module Neuron = struct
+
+  let to_specs = function
+    | NN.Input x      -> Input.to_specs x
+    | NN.Linear x     -> Linear.to_specs x
+    | NN.Activation x -> Activation.to_specs x
+    | _               -> failwith "owl_zoo_specs_neural:neuron:to_specs"
+
+  let to_string = function
+    | NN.Input x      -> "input"
+    | NN.Linear x     -> "linear"
+    | NN.Activation x -> "activation"
+    | _               -> failwith "owl_zoo_specs_neural:neuron:to_string"
 
 end
 
@@ -45,12 +95,29 @@ module Feedforward = struct
   let to_specs x =
     let layers = FN.(x.layers)
       |> Array.to_list
-      |> List.map _layer_to_specs
+      |> List.mapi (fun i l ->
+          let neuron, param = Neuron.to_specs l in
+          let name = Printf.sprintf "%s_%i" (Neuron.to_string l) i in
+          ST.({ name; neuron; param })
+        )
     in
     ST.({ name = "Feedforward"; layers })
 
 end
 
 
+let make_example_network () =
+  let open Owl_neural in
+  let open Feedforward in
+  let nn = input [|784|]
+    |> linear 300 ~act_typ:Activation.Tanh
+    |> linear 10  ~act_typ:Activation.Softmax
+  in
+  nn
+
+
 let _ =
-  print_endline "hello"
+  let nn = make_example_network () in
+  let nn = Feedforward.to_specs nn in
+  let s = SJ.string_of_feedforward nn in
+  print_endline (Yojson.Safe.prettify s)
