44b4696d9404af4ca5f8783eccee0fc9c9dceaca
Liang Wang <ryanrhymes@gmail.com>
2017-07-23 22:41:27 +0300

    design model zoo

--- lib/zoo/owl_zoo_specs_neural.ml
+++ lib/zoo/owl_zoo_specs_neural.ml
@@ -16,13 +16,31 @@ module ST = struct
   let make_param
     ?in_shape
     ?out_shape
+    ?init_typ
     ?activation_typ
     ()
     = {
       in_shape;
       out_shape;
+      init_typ;
       activation_typ;
     }
+
+end
+
+
+module Init = struct
+
+  let to_specs x =
+    let open NN.Init in
+    match x with
+    | Uniform (a, b)       -> ST.(`Uniform (a, b))
+    | Gaussian (mu, sigma) -> ST.(`Gaussian (mu, sigma))
+    | Standard             -> ST.(`Standard)
+    | Tanh                 -> ST.(`Tanh)
+    | _                    -> failwith "owl_zoo_specs_neural:init:to_specs"
+
+  let of_specs spec = None
 
 end
 
@@ -31,7 +49,8 @@ module Input = struct
 
   let to_specs x =
     let typ = ST.(`Input) in
-    let param = ST.make_param in
+    let in_shape = NN.Input.(x.in_shape) |> Array.to_list in
+    let param = ST.make_param ~in_shape in
     typ, param ()
 
   let of_specs spec = None
@@ -43,7 +62,10 @@ module Linear = struct
 
   let to_specs x =
     let typ = ST.(`Linear) in
-    let param = ST.make_param in
+    let in_shape = NN.Linear.(x.in_shape) |> Array.to_list in
+    let out_shape = NN.Linear.(x.out_shape) |> Array.to_list in
+    let init_typ = NN.Linear.(x.init_typ) |> Init.to_specs in
+    let param = ST.make_param ~in_shape ~out_shape ~init_typ in
     typ, param ()
 
   let of_specs spec = None
@@ -56,13 +78,15 @@ module Activation = struct
   let to_specs x =
     let open NN.Activation in
     let typ = ST.(`Activation) in
+    let in_shape = x.in_shape |> Array.to_list in
+    let out_shape = x.out_shape |> Array.to_list in
     let param =
       match x.activation with
-      | Relu    -> ST.(make_param ~activation_typ:`Relu)
-      | Sigmoid -> ST.(make_param ~activation_typ:`Sigmoid)
-      | Softmax -> ST.(make_param ~activation_typ:`Softmax)
-      | Tanh    -> ST.(make_param ~activation_typ:`Tanh)
-      | None    -> ST.(make_param ~activation_typ:`None)
+      | Relu    -> ST.(make_param ~in_shape ~out_shape ~activation_typ:`Relu)
+      | Sigmoid -> ST.(make_param ~in_shape ~out_shape ~activation_typ:`Sigmoid)
+      | Softmax -> ST.(make_param ~in_shape ~out_shape ~activation_typ:`Softmax)
+      | Tanh    -> ST.(make_param ~in_shape ~out_shape ~activation_typ:`Tanh)
+      | None    -> ST.(make_param ~in_shape ~out_shape ~activation_typ:`None)
       | _       -> failwith "owl_zoo_specs_neural:activation:to_specs"
     in
     typ, param ()
