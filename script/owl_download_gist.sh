7f03d5a0fec952e48a3325eee4d2dd6095180b70
Liang Wang <ryanrhymes@gmail.com>
2017-07-25 20:24:19 +0300

    design zoo module

---
+++ script/owl_download_gist.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+GIST=$1
+DIRNAME=$2
+
+if [ -z $GIST ]; then
+  echo "usage: `basename $0` <gist_id> <dirname>"
+  exit 1
+fi
+
+GIST_DIR=$(echo $GIST | tr '/' '-')
+MODEL_DIR="$DIRNAME/$GIST_DIR"
+
+if [ -d $MODEL_DIR ]; then
+    echo "Warning: $MODEL_DIR already exists, overwriting!"
+fi
+
+echo "Downloading the gist to $MODEL_DIR ..."
+mkdir -p $MODEL_DIR
+wget https://gist.github.com/$GIST/download -O $MODEL_DIR/gist.zip
+unzip -j $MODEL_DIR/gist.zip -d $MODEL_DIR
+rm $MODEL_DIR/gist.zip
+echo "Succeed!"
