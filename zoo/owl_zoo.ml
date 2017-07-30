cb841e63f2b5e99018efa4fab9fcd6273967b0db
Liang Wang <ryanrhymes@gmail.com>
2017-07-30 13:10:38 +0300

    experiment zoo system

--- zoo/owl_zoo.ml
+++ zoo/owl_zoo.ml
@@ -11,40 +11,45 @@ let write_file f s =
 
 
 let preprocess script =
-  let new_script = "." ^ (Filename.basename script) in
+  let prefix = "." ^ (Filename.basename script) in
+  let tmp_script = Filename.temp_file prefix "ml" in
   let content =
     "#require \"owl\"\n" ^
     "#require \"owl_zoo\"\n" ^
     Printf.sprintf "#use \"%s\"\n" script
   in
-  write_file new_script content;
-  new_script
+  write_file tmp_script content;
+  tmp_script
 
 
-let run script =
+let run args script =
   let new_script = preprocess script in
-  let cmd = "utop " ^ new_script in
+  let cmd = Printf.sprintf "utop %s %s" args new_script in
   Sys.command cmd
 
 
+let print_info () =
+  let info =
+    "Owl's Zoo System (atop of utop)\n" ^
+    "Usage: owl [options] [script-file]"
+  in
+  print_endline info
+
+
+let _ =
+  if Array.length Sys.argv < 2 then
+    print_info ()
+  else (
+    let len = Array.length Sys.argv in
+    let script = Sys.argv.(len - 1) in
+    let args = Array.sub Sys.argv 1 (len - 2)
+      |> Array.fold_left (fun a s -> a ^ s ^ " ") ""
+    in
+    run args script |> ignore
+  )
+
+
 (*
-let run script =
-  Toploop.initialize_toplevel_env ();
-  Topdirs.dir_use Format.std_formatter "/Users/liang/Desktop/tmp/yyy/a.ml";
-  Toploop.use_file Format.std_formatter script
-  if (Toploop.run_script Format.std_formatter script [|""|]) then
-    print_endline "true"
-  else
-    print_endline "false"
-  let cmd = "ocaml " ^ script in
-  Sys.command cmd *)
-
-
-let _ = run Sys.argv.(1)
-
-(*
-open Owl
-
 let read_file f =
   let h = open_in f in
   let s = Utils.Stack.make () in
@@ -57,55 +62,4 @@ let read_file f =
   close_in h;
   Utils.Stack.to_array s
 
-
-let write_file f s =
-  let h = open_out f in
-  Printf.fprintf h "%s" s;
-  close_out h
-
-
-let process_macro_zoo x =
-  let id = String.(sub x 4 (length x - 4) |> trim) in
-  let dir = Sys.getenv "HOME" ^ "/.owl/zoo/" ^ id in
-  (* only include ml files *)
-  let mls = Sys.readdir (dir)
-    |> Utils.array_filter (fun s ->
-        Filename.check_suffix s "ml"
-      )
-  in
-  Array.fold_left (fun a l ->
-    Printf.sprintf "%s#use \"%s/%s\"\n" a dir l
-  ) "" mls
-
-
-let preprocess script =
-  let lines = read_file script in
-  (* FIXME: drop the first line *)
-  let lines = Array.(sub lines 1 (length lines - 1)) in
-  (* insert default configurations *)
-  let lines = Utils.array_insert lines 0 "#use \"topfind\"" in
-  let lines = Utils.array_insert lines 1 "#require \"owl\"" in
-  (* process zoo macros *)
-  let lines = Array.map (fun l ->
-    if String.length l >= 4 && String.sub l 0 4 = "#zoo" then (
-      process_macro_zoo l
-    )
-    else l
-  ) lines in
-  (* join all the lines *)
-  let s = Array.fold_left (fun a l -> a ^ l ^ "\n") "" lines in
-  let new_script = "." ^ (Filename.basename script) in
-  write_file new_script s;
-  new_script
-
-
-let run script =
-  let cmd = "ocaml " ^ script in
-  Sys.command cmd
-
-
-let _ =
-  print_endline "Owl's Zoo System";
-  let processed = preprocess Sys.argv.(1) in
-  run processed
 *)
--- zoo/owl_zoo_toplevel.ml
+++ zoo/owl_zoo_toplevel.ml
@@ -3,6 +3,7 @@
  * Copyright (c) 2016-2017 Liang Wang <liang.wang@cl.cam.ac.uk>
  *)
 
+
 let process_dir_zoo gist =
   let dir = Sys.getenv "HOME" ^ "/.owl/zoo/" ^ gist in
   (* only include ml files *)
@@ -11,29 +12,20 @@ let process_dir_zoo gist =
   |> List.filter (fun s -> Filename.check_suffix s "ml")
   |> List.iter (fun l ->
       let f = Printf.sprintf "%s/%s" dir l in
-      Topdirs.dir_use Format.std_formatter f
+      Toploop.mod_use_file Format.std_formatter f
+      |> ignore
     )
 
 
 let add_dir_zoo () =
-  Hashtbl.add Toploop.directive_table "zoo"
-  (Toploop.Directive_string process_dir_zoo)
+  let section = "owl" in
+  let doc =
+    "owl's zoo system\n" ^
+    "ditribute code snippet via gist\n"
+  in
+  let info = Toploop.({ section; doc }) in
+  let dir_fun = Toploop.Directive_string process_dir_zoo in
+  Toploop.add_directive "zoo" dir_fun info
 
 
-let load_libs () =
-
-    Topdirs.dir_directory "/Users/liang/.opam/4.04.0/lib/findlib/";
-    Topdirs.dir_load Format.err_formatter ("/Users/liang/.opam/4.04.0/lib/findlib/findlib.cma");
-    Topdirs.dir_load Format.err_formatter ("/Users/liang/.opam/4.04.0/lib/findlib/findlib_top.cma");
-    print_endline "+++"
-
-  (*
-  Topdirs.dir_directory "/Users/liang/.opam/4.04.0/lib/ctypes";
-  Topdirs.dir_load Format.std_formatter "ctypes.cma";
-  Topdirs.dir_directory "/Users/liang/.opam/4.04.0/lib/owl";
-  Topdirs.dir_load Format.std_formatter "owl.cma"
-  *)
-
-
-let _ =
-  add_dir_zoo ()
+let _ = add_dir_zoo ()
