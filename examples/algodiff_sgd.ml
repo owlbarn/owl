8c19fb00ab59cf2378c5c280e0faef60d1d25767
Liang Wang <ryanrhymes@gmail.com>
2017-08-21 22:05:34 +0100

    clean up examples

--- examples/algodiff_sgd.ml
+++ examples/algodiff_sgd.ml
@@ -1,47 +1,4 @@
-#!/usr/bin/env owl
 (* This example demonstrates how to write a stochastic gradient descent
    algorithm from scratch using Algodiff module. The more advanced algorithms
    can also be build using the similar way.
  *)
-
-module LL = Owl_optimise
-
-let centerise x =
-  let open MX in
-  let v = average_rows x in
-  map_by_row (row_num v) (fun x -> x - v) x
-
-let test () =
-  let p = Mat.uniform_int 100 10 in
-  let x = centerise (Mat.uniform 10000 100) in
-  let y = Mat.(x *@ p) in
-  let q = Mat.uniform_int 100 10 in
-  let p' = LL.sgd q x y in
-  Printf.printf "error ==> %.4f\n" Mat.(sum (abs (p'- p)))
-
-let test_small () =
-  let p = Mat.uniform_int 5 3 in
-  let p = Mat.map_at_row (fun _ -> float_of_int (Random.int 30)) p 4 in
-  let x = Mat.uniform 1000 5 in
-  let x = Mat.map_at_col (fun _ -> 1.) x 4 in
-  let y = Mat.(x *@ p) in
-  let q = Mat.uniform_int 5 3 in
-  let p' = LL.sgd q x y in
-  Mat.(pp_dsmat (p));
-  Mat.(pp_dsmat (p'));
-  Mat.(pp_dsmat (p' - p));
-  Printf.printf "error ==> %.4f\n" Mat.(sum (abs (p'- p)))
-
-let test_intercept () =
-  let p = Mat.uniform_int 5 3 in
-  let p = Mat.map_at_row (fun _ -> float_of_int (Random.int 30)) p 4 in
-  let x = Mat.uniform 1000 5 in
-  let x = Mat.map_at_col (fun _ -> 1.) x 4 in
-  let y = Mat.(x *@ p) in
-  let q = Mat.uniform_int 4 3 in
-  let p' = LL.sgd ~i:true q Mat.(cols x [|0;1;2;3|]) y in
-  Printf.printf "error ==> %.4f\n" Mat.(sum (abs (p'- p)))
-
-let _ =
-  Random.self_init ();
-  test_small ()
