53d34347f88a75977a9c10030c007e83a7a5965f
Liang Wang <ryanrhymes@gmail.com>
2017-08-27 16:51:31 +0100

    improve computation graph example

--- examples/computation_graph.ml
+++ examples/computation_graph.ml
@@ -5,6 +5,8 @@
  *)
 
 #zoo "2e7c902812a7ae0547e24f7ea743c7e6"
+#zoo "217ef87bc36845c4e78e398d52bc4c5b"
+
 open Owl
 open Algodiff.S
 
@@ -28,10 +30,21 @@ let visualise_simple_02 () =
   to_dot [z]
 
 
-let visualise_neural_network () =
+let visualise_vgg () =
   let network = Cifar10.make_network [|32;32;3|] in
   let x, _, y = Dataset.load_cifar_train_data 1 in
-  let xt, yt = Optimise.S.Utils.draw_samples (Arr x) (Mat y) 10 in
+  let xt, yt = Optimise.S.Utils.draw_samples (Arr x) (Mat y) 5 in
+  let yt', _ = Neural.S.Graph.(init network; forward network xt) in
+  let loss = Maths.((cross_entropy yt yt') / (F (Mat.row_num yt |> float_of_int))) in
+  to_dot [loss]
+
+
+let visualise_lstm () =
+  let wndsz = 10 and stepsz = 1 in
+  let w2i, i2w, x, y = Lstm.prepare wndsz stepsz in
+  let vocabsz = Hashtbl.length w2i in
+  let network = Lstm.make_network wndsz vocabsz in
+  let xt, yt = Optimise.S.Utils.draw_samples (Mat x) (Mat y) 15 in
   let yt', _ = Neural.S.Graph.(init network; forward network xt) in
   let loss = Maths.((cross_entropy yt yt') / (F (Mat.row_num yt |> float_of_int))) in
   to_dot [loss]
@@ -43,6 +56,9 @@ let _ =
   Log.info "visualise simple computation graph to dot file ...";
   visualise_simple_02 () |> Owl_utils.write_file "plot_algodiff_graph1.dot";
   Sys.command "dot -Tpdf plot_algodiff_graph1.dot -o plot_algodiff_graph1.pdf";
-  Log.info "visualise neural network ...";
-  visualise_neural_network () |> Owl_utils.write_file "plot_algodiff_graph2.dot";
-  Sys.command "dot -Tpdf plot_algodiff_graph2.dot -o plot_algodiff_graph2.pdf"
+  Log.info "visualise VGG neural network ...";
+  visualise_vgg () |> Owl_utils.write_file "plot_algodiff_graph2.dot";
+  Sys.command "dot -Tpdf plot_algodiff_graph2.dot -o plot_algodiff_graph2.pdf";
+  Log.info "visualise LSTM neural network ...";
+  visualise_lstm () |> Owl_utils.write_file "plot_algodiff_graph3.dot";
+  Sys.command "dot -Tpdf plot_algodiff_graph3.dot -o plot_algodiff_graph3.pdf"
