0e94030c471650028883c35ef2ae57d513b6a9c7
jrzhao <rho.ajax@gmail.com>
2017-08-28 01:04:20 +0100

    add inception to example/compute_graph

--- examples/computation_graph.ml
+++ examples/computation_graph.ml
@@ -6,6 +6,8 @@
 
 #zoo "2e7c902812a7ae0547e24f7ea743c7e6"
 #zoo "217ef87bc36845c4e78e398d52bc4c5b"
+#zoo "e7d8b1f6fbe1d12bb4a769d8736454b9" (* LoadImage   *)
+#zoo "9428a62a31dbea75511882ab8218076f" (* InceptionV3 *)
 
 open Owl
 open Algodiff.S
@@ -49,6 +51,26 @@ let visualise_lstm () =
   let loss = Maths.((cross_entropy yt yt') / (F (Mat.row_num yt |> float_of_int))) in
   to_dot [loss]
 
+let visualise_inception () =
+  let fname = "panda1.ppm" in (* local file *)
+  let _, _, _, img = LoadImage.img_to_owl fname in
+  let shape = Arr.shape img in
+  let shape = Array.append [|1|] shape in
+  let img = Arr.reshape img shape
+           |> Dense.Ndarray.Generic.cast_d2s
+  in
+  let img = Dense.Ndarray.S.div_scalar img 255. in
+  let img = Dense.Ndarray.S.sub_scalar img 0.5  in
+  let img = Dense.Ndarray.S.mul_scalar img 2.   in
+
+  let yt = Dense.Matrix.S.zeros 1 1000 in
+  Dense.Matrix.S.set yt 0 388 1. in
+  let yt = Mat yt in
+
+  let network = InceptionV3.model ()  in
+  let yt', _ = Neural.S.Graph.(init network; forward network (Arr img)) in
+  let loss = Maths.((cross_entropy yt yt') / (F (Mat.row_num yt |> float_of_int))) in
+  to_dot [loss]
 
 let _ =
   Log.info "visualise simple computation graph to terminal ...";
@@ -62,3 +84,6 @@ let _ =
   Log.info "visualise LSTM neural network ...";
   visualise_lstm () |> Owl_utils.write_file "plot_algodiff_graph3.dot";
   Sys.command "dot -Tpdf plot_algodiff_graph3.dot -o plot_algodiff_graph3.pdf"
+  visualise_inception () |> Owl_utils.write_file "plot_algodiff_graph4.dot"(*;
+  (* this command takes ages on a ~60K .dot file *)
+  Sys.command "dot -Tpdf plot_algodiff_graph4.dot -o plot_algodiff_graph4.pdf"*)
