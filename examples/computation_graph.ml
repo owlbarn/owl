12d1b5a1febeae3347dded5489489ea45508af91
jrzhao <rho.ajax@gmail.com>
2017-08-31 20:46:32 +0100

    cleanup

--- examples/computation_graph.ml
+++ examples/computation_graph.ml
@@ -4,10 +4,8 @@
    computer. Or you can use other visualisation tools for the generated dot file.
  *)
 
-#zoo "2e7c902812a7ae0547e24f7ea743c7e6" (* Cifar10     *)
-#zoo "217ef87bc36845c4e78e398d52bc4c5b" (* Lstm        *)
-#zoo "e7d8b1f6fbe1d12bb4a769d8736454b9" (* LoadImage   *)
-#zoo "9428a62a31dbea75511882ab8218076f" (* InceptionV3 *)
+#zoo "2e7c902812a7ae0547e24f7ea743c7e6"
+#zoo "217ef87bc36845c4e78e398d52bc4c5b"
 
 open Owl
 open Algodiff.S
@@ -51,28 +49,6 @@ let visualise_lstm () =
   let loss = Maths.((cross_entropy yt yt') / (F (Mat.row_num yt |> float_of_int))) in
   to_dot [loss]
 
-let visualise_inception () =
-  (* Get picture "panda1.ppm" to local directory from gist
-   6dfed11c521fb2cd286f2519fb88d3bf *)
-  let fname = "panda1.ppm" in
-  let _, _, _, img = LoadImage.img_to_owl fname in
-  let shape = Arr.shape img in
-  let shape = Array.append [|1|] shape in
-  let img = Arr.reshape img shape
-           |> Dense.Ndarray.Generic.cast_d2s
-  in
-  let img = Dense.Ndarray.S.div_scalar img 255. in
-  let img = Dense.Ndarray.S.sub_scalar img 0.5  in
-  let img = Dense.Ndarray.S.mul_scalar img 2.   in
-
-  let yt = Dense.Matrix.S.zeros 1 1000 in
-  Dense.Matrix.S.set yt 0 388 1. in
-  let yt = Mat yt in
-
-  let network = InceptionV3.model ()  in
-  let yt', _ = Neural.S.Graph.(init network; forward network (Arr img)) in
-  let loss = Maths.((cross_entropy yt yt') / (F (Mat.row_num yt |> float_of_int))) in
-  to_dot [loss]
 
 let _ =
   Log.info "visualise simple computation graph to terminal ...";
@@ -86,6 +62,3 @@ let _ =
   Log.info "visualise LSTM neural network ...";
   visualise_lstm () |> Owl_utils.write_file "plot_algodiff_graph3.dot";
   Sys.command "dot -Tpdf plot_algodiff_graph3.dot -o plot_algodiff_graph3.pdf"
-  visualise_inception () |> Owl_utils.write_file "plot_algodiff_graph4.dot";
-  Sys.command "dot -Tpdf -Gnslimit=1 plot_algodiff_graph4.dot -o
-  plot_algodiff_graph4.pdf"
