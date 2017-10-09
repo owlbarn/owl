564e2d4b69a4f79e5d10f0e5cee318fa6f94b612
Liang Wang <ryanrhymes@gmail.com>
2017-10-09 21:47:01 +0100

    opencl: change owl_opencl_kernels.cl -> owl_opencl_kernel.cl

--- src/opencl/owl_opencl_context.ml
+++ src/opencl/owl_opencl_context.ml
@@ -24,7 +24,7 @@ let compile_kernels () =
   let ctx = Context.create_from_type cl_DEVICE_TYPE_GPU in
   let gpu = Context.((get_info ctx).devices).(0) in
   let cmdq = CommandQueue.create ctx gpu in
-  let prog_s = Owl.Utils.read_file_string "/Users/liang/code/owl/src/opencl/owl_opencl_kernels.cl" in
+  let prog_s = Owl.Utils.read_file_string "/Users/liang/code/owl/src/opencl/owl_opencl_kernel.cl" in
   let prog = Program.create_with_source ctx [|prog_s|] in
   Owl_opencl_base.Program.build prog [|gpu|];
   {
