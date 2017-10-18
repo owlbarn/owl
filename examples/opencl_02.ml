a0f7f8f3a598ba435e3f2dcfe15dc74a46087280
Liang Wang <ryanrhymes@gmail.com>
2017-10-18 23:33:49 +0100

    opencl: tweak example

--- examples/opencl_02.ml
+++ examples/opencl_02.ml
@@ -4,11 +4,16 @@
 #require "owl_opencl"
 open Owl
 
-let test () =
-  let x = Dense.Ndarray.S.uniform [|1000; 1000|] in
+let test_gpu x () =
   let x = Owl_opencl_dense.of_ndarray x in
-  let x = Owl_opencl_dense.(add x x |> sin |> cos |> sin |> neg) in
+  let x = Owl_opencl_dense.(mul x x |> sin |> cos |> tan |> neg) in
   let x = Owl_opencl_dense.to_ndarray float32 x in
   Dense.Ndarray.Generic.pp_dsnda Format.std_formatter x
 
-let _ = Printf.printf "\nTime: %f ms\n" (Utils.time test)
+let test_cpu x () =
+  ignore Dense.Ndarray.S.(mul x x |> sin |> cos |> tan |> neg)
+
+let _ =
+  let x = Dense.Ndarray.S.uniform [|1000; 1000|] in
+  Printf.printf "\nGPU time: %.3f ms  " (Utils.time (test_gpu x));
+  Printf.printf "\nCPU time: %.3f ms\n" (Utils.time (test_cpu x));
