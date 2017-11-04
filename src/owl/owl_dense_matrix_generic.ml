560660905df8107e3a8356facf18db47b668d0ea
Liang Wang <ryanrhymes@gmail.com>
2017-11-04 20:07:32 +0000

    core: replace sum_rows, sum_cols fun

--- src/owl/owl_dense_matrix_generic.ml
+++ src/owl/owl_dense_matrix_generic.ml
@@ -643,10 +643,10 @@ let sum_cols x = sum ~axis:1 x
 let sum_rows x = sum ~axis:0 x
 
 
-let mean_cols x = mean ~axis:0 x
+let mean_cols x = mean ~axis:1 x
 
 
-let mean_rows x = mean ~axis:1 x
+let mean_rows x = mean ~axis:0 x
 
 
 let min_cols x =
