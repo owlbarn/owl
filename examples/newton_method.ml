64ee88528e01988a9c41d511bccde387c37952f7
Liang Wang <ryanrhymes@gmail.com>
2017-11-21 23:02:05 +0000

    fix examples

--- examples/newton_method.ml
+++ examples/newton_method.ml
@@ -7,7 +7,7 @@ open Algodiff.D
 
 let rec newton ?(eta=F 0.01) ?(eps=1e-6) f x =
   let g, h = (gradhessian f) x in
-  if (Maths.l2norm g |> unpack_flt) < eps then x
+  if (Maths.l2norm' g |> unpack_flt) < eps then x
   else newton ~eta ~eps f Maths.(x - eta * g *@ (inv h))
 
 
