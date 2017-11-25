4d61ed9ad8c7cba5740f58f3b578f1f22b9bb3f5
Liang Wang <ryanrhymes@gmail.com>
2017-11-25 01:15:07 +0000

    lazy: improve lazy example

--- examples/lazy_eval.ml
+++ examples/lazy_eval.ml
@@ -14,7 +14,19 @@ let test_lazy x () =
 let test_eager x () =
   Arr.(add x x |> log |> sin |> neg |> cos |> abs |> sinh |> round |> atan)
 
+let test_incremental x =
+  let a = M.variable () in
+  let b = M.variable () in
+  let c = M.(a |> sin |> cos |> abs |> log |> sum' |> mul_scalar a |> scalar_add b) in
+  M.assign_arr a x;
+  M.assign_elt b 5.;
+  Printf.printf "Incremental#0:\t%.3f ms\n" (Utils.time (fun () -> M.eval c));
+  M.assign_elt b 6.;
+  Printf.printf "Incremental#1:\t%.3f ms\n" (Utils.time (fun () -> M.eval c))
+
+
 let () =
   let x = Arr.uniform [|1000; 1000|] in
-  Printf.printf "Lazy eval:  %.3f ms  " (Utils.time (test_lazy x));
-  Printf.printf "\nEager eval: %.3f ms\n" (Utils.time (test_eager x));
+  Printf.printf "Lazy eval:\t%.3f ms\n" (Utils.time (test_lazy x));
+  Printf.printf "Eager eval:\t%.3f ms\n" (Utils.time (test_eager x));
+  test_incremental x
