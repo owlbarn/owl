7b30924ebbc4b661f53fa45804ff5e6b247beb97
Liang Wang <ryanrhymes@gmail.com>
2017-12-23 20:01:37 +0200

    ppl: experiment design

--- src/owl/owl_mcmc.ml
+++ src/owl/owl_mcmc.ml
@@ -18,16 +18,32 @@ module Make (A : InpureSig) = struct
     let init () = ()
 
     let update model h =
-      let ratio = 0.0 in
+      let ratio = ref 0.0 in
       let model' = L.copy model in
       let sample = Hashtbl.find h "latent" |> List.map L.to_arr in
-
       (* Draw sample znew ~ g(znew | zold) *)
       let sample' = Hashtbl.find h "proposal" |> List.map L.to_arr in
-      let ratio = ratio -. 0.0 in
+      let g_x'_x = Hashtbl.find h "g(x'|x)" in
+      let g_x_x' = Hashtbl.find h "g(x|x')" in
 
-      let condition = Hashtbl.find h "condition" |> List.map L.to_arr in
-      let sample' = sample in
+      List.iter2 (fun g' g ->
+        ratio := !ratio +. (L.to_elt g') -. (L.to_elt g)
+      ) g_x'_x g_x_x';
+
+      let p_x' = Hashtbl.find h "p(x')" in
+      let p_x = Hashtbl.find h "p(x)" in
+
+      List.iter2 (fun p' p ->
+        ratio := !ratio +. (L.to_elt p') -. (L.to_elt p)
+      ) p_x' p_x;
+
+      let p_y_x' = Hashtbl.find h "p(y|x')" in
+      let p_y_x = Hashtbl.find h "p(y|x)" in
+
+      List.iter2 (fun p' p ->
+        ratio := !ratio +. (L.to_elt p') -. (L.to_elt p)
+      ) p_y_x' p_y_x;
+
       ()
 
   end
