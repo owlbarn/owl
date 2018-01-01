e898d0963468a485c66c5d940b77f0c03294546d
Liang Wang <ryanrhymes@gmail.com>
2018-01-01 17:01:35 +0200

    organise project folders

---
+++ src/owl/random/owl_random.h
@@ -0,0 +1,40 @@
+/*
+ * OWL - an OCaml numerical library for scientific computing
+ * Copyright (c) 2016-2017 Liang Wang <liang.wang@cl.cam.ac.uk>
+ */
+
+
+// SFMT PRNG and its internal state
+
+#include "SFMT.h"
+
+extern sfmt_t sfmt_state;
+
+#define sfmt_rand32 sfmt_genrand_uint32(&sfmt_state)
+
+#define sfmt_rand64 sfmt_genrand_uint64(&sfmt_state)
+
+#define sfmt_f32_1 sfmt_genrand_real4(&sfmt_state)
+
+#define sfmt_f32_2 sfmt_genrand_real5(&sfmt_state)
+
+#define sfmt_f32_3 sfmt_genrand_real6(&sfmt_state)
+
+#define sfmt_f64_1 sfmt_genrand_real1(&sfmt_state)
+
+#define sfmt_f64_2 sfmt_genrand_real2(&sfmt_state)
+
+#define sfmt_f64_3 sfmt_genrand_real3(&sfmt_state)
+
+
+// Ziggurat PRNG and functions
+
+extern void ziggurat_init ( );
+
+extern float ziggurat_exp ( );
+
+extern float ziggurat_gaussian ( );
+
+#define f32_gaussian ziggurat_gaussian()
+
+#define f64_gaussian ziggurat_gaussian()
