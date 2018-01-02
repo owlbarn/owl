f46b8b47f9640a470d1f8f6c96542abfa2720c84
Liang Wang <ryanrhymes@gmail.com>
2018-01-03 00:57:52 +0200

    core: restructure stats module

---
+++ src/owl/maths/owl_maths.h
@@ -0,0 +1,11 @@
+/*
+ * OWL - an OCaml numerical library for scientific computing
+ * Copyright (c) 2016-2017 Liang Wang <liang.wang@cl.cam.ac.uk>
+ */
+
+#ifndef OWL_STATS_H
+#define OWL_STATS_H
+
+
+
+#endif // OWL_MATHS_H
