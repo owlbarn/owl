e0da99cc64969deb52d906a18d36f9bc37ddaf70
Liang Wang <ryanrhymes@gmail.com>
2018-01-07 12:04:40 +0200

    ppl: coding distribution module

--- src/owl/stats/owl_stats_dist_wald.c
+++ src/owl/stats/owl_stats_dist_wald.c
@@ -19,3 +19,47 @@ double wald_rvs(double mu, double lambda) {
   else
     return (mu * mu / X);
 }
+
+double wald_pdf(double x, double mu, double lambda) {
+  double y = (x - mu) / mu;
+  return sqrt(lambda) / sqrt(OWL_2PI * x * x * x) * exp(-lambda * y * y / (2 * x));
+}
+
+double wald_logpdf(double x, double mu, double lambda) {
+  double y = (x - mu) / mu;
+  return 0.5 * (lambda - OWL_LOGE2 - OWL_LOGEPI - 3 * log(x)) - lambda * y * y / (2 * x);
+}
+
+double wald_cdf(double x, double mu, double lambda) {
+  double fac = sqrt(lambda / x);
+  double c1 = ndtr(fac * (x - mu) / mu);
+  c1 += exp(2 * lambda / mu) * ndtr(-fac * (x + mu) / mu);
+  return c1;
+}
+
+double wald_logcdf(double x, double mu, double lambda) {
+  return log(wald_cdf(x, mu, lambda));
+}
+
+double wald_ppf(double p, double mu, double lambda) {
+  // TODO
+  return 0.;
+}
+
+double wald_sf(double x, double mu, double lambda) {
+  return 1 - wald_cdf(x, mu, lambda);
+}
+
+double wald_logsf(double x, double mu, double lambda) {
+  return log(1 - wald_cdf(x, mu, lambda));
+}
+
+double wald_isf(double q, double mu, double lambda) {
+  // TODO
+  return 0.;
+}
+
+double wald_entroy(double mu, double lambda) {
+  // TODO
+  return 0.;
+}
