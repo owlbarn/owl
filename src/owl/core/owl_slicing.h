a9dfad56380349e7db66fccb22f7224ae6018f8b
Liang Wang <ryanrhymes@gmail.com>
2018-01-22 00:42:32 +0000

    coding fancy slicing ...

--- src/owl/core/owl_slicing.h
+++ src/owl/core/owl_slicing.h
@@ -7,38 +7,53 @@
 #define OWL_SLICING_H
 
 
-/* Define structure for copying a slice from x to y. */
+/* Define structure for copying a basic slice from x to y. */
 
 struct slice_pair {
-  int dim;      // number of dimensions, x and y must be the same
-  int dep;      // the depth of current recursion.
-  long *n;      // number of iteration in each dimension, i.e. y's shape
-  void *x;      // x, source if operation is get, destination if set.
-  int posx;     // current offest of x.
-  int *ofsx;    // offset of x in each dimension.
-  int *incx;    // stride size of x in each dimension.
-  void *y;      // y, destination if operation is get, source if set.
-  int posy;     // current offest of y.
-  int *ofsy;    // offset of y in each dimension.
-  int *incy;    // stride size of y in each dimension.
+  int dim;          // number of dimensions, x and y must be the same
+  int dep;          // the depth of current recursion.
+  long *n;          // number of iteration in each dimension, i.e. y's shape
+  void *x;          // x, source if operation is get, destination if set.
+  int posx;         // current offest of x.
+  int *ofsx;        // offset of x in each dimension.
+  int *incx;        // stride size of x in each dimension.
+  void *y;          // y, destination if operation is get, source if set.
+  int posy;         // current offest of y.
+  int *ofsy;        // offset of y in each dimension.
+  int *incy;        // stride size of y in each dimension.
 };
 
 
-/* Define structure for copying a fancy slice from x to y. */
+/** Define structure for copying a fancy slice from x to y.
+ * slice field contains a list of triplet with the format of (a,b,c). If a is
+ * non-negative, then (a,b,c) is a normal slice definition, i.e., the same as
+ * (start, stop, step).
+ *
+ * Otherwise, it indicates this dimension in the slice definition is defined by
+ * a list of indices, I set this value to (-1) because OCaml code will first
+ * re-format the slice definition to make sure both start and stop are greater
+ * than zero. In case of a list of indices, the latter two elements b and c is
+ * used to specify the starting and ending point of the list and in the index
+ * field. All the list will be flattened and saved in index before calling any
+ * fancy slicing functions.
+ *
+ * As a result, the fields in ofsx and incx are only meaningful for the
+ * dimension defined using normal slice definition.
+**/
 
 struct fancy_pair {
-  int dim;      // number of dimensions, x and y must be the same
-  int dep;      // the depth of current recursion.
-  long *slice;  //
-  long *index;  //
-  void *x;      // x, source if operation is get, destination if set.
-  int posx;     // current offest of x.
-  int *ofsx;    // offset of x in each dimension.
-  int *incx;    // stride size of x in each dimension.
-  void *y;      // y, destination if operation is get, source if set.
-  int posy;     // current offest of y.
-  int *ofsy;    // offset of y in each dimension.
-  int *incy;    // stride size of y in each dimension.
+  int dim;          // number of dimensions, x and y must be the same
+  int dep;          // the depth of current recursion.
+  int64_t *slice;   // (a,b,c) triplet, if a >= 0 then normal slice.
+  int64_t *index;   // combined use with slice, check the details above.
+  void *x;          // x, source if operation is get, destination if set.
+  int posx;         // current offest of x.
+  int *ofsx;        // offset of x in each dimension.
+  int *incx;        // stride size of x in each dimension.
+  void *y;          // y, destination if operation is get, source if set.
+  int posy;         // current offest of y.
+  int *ofsy;        // offset of y in each dimension.
+  int *incy;        // stride size of y in each dimension.
 };
 
 
--- src/owl/core/owl_slicing_impl.c
+++ src/owl/core/owl_slicing_impl.c
@@ -187,7 +187,7 @@ void FUNCTION (c, slice_4) (struct slice_pair *p) {
 }
 
 
-// slice x based on the slice definition and save to y.
+// slice x based on the basic slice definition and save to y.
 void FUNCTION (c, slice) (struct slice_pair *p) {
 
   if (p->dep == p->dim - 1)
@@ -200,17 +200,20 @@ void FUNCTION (c, slice) (struct slice_pair *p) {
     FUNCTION (c, slice_4) (p);
   else {
     const int d = p->dep;
+    const int n = p->n[d];
+    const int incx = p->incx[d];
+    const int incy = p->incy[d];
     const int save_posx = p->posx;
     const int save_posy = p->posy;
     p->posx += p->ofsx[d];
     p->posy += p->ofsy[d];
 
-    for (int i = 0; i < p->n[d]; i++) {
+    for (int i = 0; i < n; i++) {
       p->dep += 1;
       FUNCTION (c, slice) (p);
       p->dep -= 1;
-      p->posx += p->incx[d];
-      p->posy += p->incy[d];
+      p->posx += incx;
+      p->posy += incy;
     }
 
     p->posx = save_posx;
