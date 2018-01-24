e60221ca64e99fd3e88a2721e3380aad2b42af75
jrzhao <rho.ajax@gmail.com>
2018-01-24 15:45:11 +0000

    add unit tests for conv and pooling operations

--- test/test_runner.ml
+++ test/test_runner.ml
@@ -11,5 +11,9 @@ let () = begin
     "linear algebra", Unit_linalg.test_set;
     "slicing basic", Unit_slicing_basic.test_set;
     "slicing fancy", Unit_slicing_fancy.test_set;
+    "pooling2d operations", Unit_pool2d.test_set;
+    "pooling3d operations", Unit_pool3d.test_set;
+    "conv2d operations", Unit_conv2d.test_set;
+    "conv3d operations", Unit_conv3d.test_set;
   ]
 end
