7e9bc282e59afb7a380f58ac29ea4beb28348da1
Liang Wang <ryanrhymes@gmail.com>
2018-02-03 00:06:10 +0000

    doc: change to rst

--- src/base/optimise/owl_algodiff_generic.mli
+++ src/base/optimise/owl_algodiff_generic.mli
@@ -256,72 +256,72 @@ module Make
   (* core Algodiff functions for algorithmic differentiation *)
 
   val diff : (t -> t) -> t -> t
-  (** [diff f x] returns the exat derivative of a function [f : scalar -> scalar]
-    at point [x]. Simply calling [diff f] will return its derivative function [g]
-    of the same type, i.e. [g : scalar -> scalar].
+  (** ``diff f x`` returns the exat derivative of a function ``f : scalar -> scalar``
+    at point ``x``. Simply calling ``diff f`` will return its derivative function ``g``
+    of the same type, i.e. ``g : scalar -> scalar``.
 
-    Keep calling this function will give you higher-order derivatives of [f], i.e.
-    [f |> diff |> diff |> diff |> ...] *)
+    Keep calling this function will give you higher-order derivatives of ``f``, i.e.
+    ``f |> diff |> diff |> diff |> ...`` *)
 
   val diff' : (t -> t) -> t -> t * t
-  (** similar to [diff], but return [(f x, diff f x)]. *)
+  (** similar to ``diff``, but return ``(f x, diff f x)``. *)
 
   val grad : (t -> t) -> t -> t
-  (** gradient of [f] : (vector -> scalar) at [x], reverse ad. *)
+  (** gradient of ``f`` : (vector -> scalar) at ``x``, reverse ad. *)
 
   val grad' : (t -> t) -> t -> t * t
-  (** similar to [grad], but return [(f x, grad f x)]. *)
+  (** similar to ``grad``, but return ``(f x, grad f x)``. *)
 
   val jacobian : (t -> t) -> t -> t
-  (** jacobian of [f] : (vector -> vector) at [x], both [x] and [y] are row vectors. *)
+  (** jacobian of ``f`` : (vector -> vector) at ``x``, both ``x`` and ``y`` are row vectors. *)
 
   val jacobian' : (t -> t) -> t -> t * t
-  (** similar to [jacobian], but return [(f x, jacobian f x)] *)
+  (** similar to ``jacobian``, but return ``(f x, jacobian f x)`` *)
 
   val jacobianv : (t -> t) -> t -> t -> t
-  (** jacobian vector product of [f] : (vector -> vector) at [x] along [v],
-    forward ad. Namely, it calcultes [(jacobian x) v] *)
+  (** jacobian vector product of ``f`` : (vector -> vector) at ``x`` along ``v``,
+    forward ad. Namely, it calcultes ``(jacobian x) v`` *)
 
   val jacobianv' : (t -> t) -> t -> t -> t * t
-  (** similar to [jacobianv'], but return [(f x, jacobianv f x v)] *)
+  (** similar to ``jacobianv'``, but return ``(f x, jacobianv f x v)`` *)
 
   val jacobianTv : (t -> t) -> t -> t -> t
-  (** transposed jacobian vector product of [f] : (vector -> vector) at [x]
-    along [v], backward ad. Namely, it calculates [transpose ((jacobianv f x v))]. *)
+  (** transposed jacobian vector product of ``f : (vector -> vector)`` at ``x``
+    along ``v``, backward ad. Namely, it calculates ``transpose ((jacobianv f x v))``. *)
 
   val jacobianTv' : (t -> t) -> t -> t -> t * t
-  (** similar to [jacobianTv], but return [(f x, transpose (jacobianv f x v))] *)
+  (** similar to ``jacobianTv``, but return ``(f x, transpose (jacobianv f x v))`` *)
 
   val hessian : (t -> t) -> t -> t
-  (** hessian of [f] : (scalar -> scalar) at [x]. *)
+  (** hessian of ``f`` : (scalar -> scalar) at ``x``. *)
 
   val hessian' : (t -> t) -> t -> t * t
-  (** simiarl to [hessian], but return [(f x, hessian f x)] *)
+  (** simiarl to ``hessian``, but return ``(f x, hessian f x)`` *)
 
   val hessianv : (t -> t) -> t -> t -> t
-  (** hessian vector product of [f] : (scalar -> scalar) at [x] along [v].
-    Namely, it calculates [(hessian x) v]. *)
+  (** hessian vector product of ``f`` : (scalar -> scalar) at ``x`` along ``v``.
+    Namely, it calculates ``(hessian x) v``. *)
 
   val hessianv' : (t -> t) -> t -> t -> t * t
-  (** similar to [hessianv], but return [(f x, hessianv f x v)]. *)
+  (** similar to ``hessianv``, but return ``(f x, hessianv f x v)``. *)
 
   val laplacian : (t -> t) -> t -> t
-  (** laplacian of [f] : (scalar -> scalar) at [x]. *)
+  (** laplacian of ``f : (scalar -> scalar)`` at ``x``. *)
 
   val laplacian' : (t -> t) -> t -> t * t
-  (** simiar to [laplacian], but return [(f x, laplacian f x)]. *)
+  (** simiar to ``laplacian``, but return ``(f x, laplacian f x)``. *)
 
   val gradhessian : (t -> t) -> t -> t * t
-  (** return [(grad f x, hessian f x)], [f] : (scalar -> scalar) *)
+  (** return ``(grad f x, hessian f x)``, ``f : (scalar -> scalar)`` *)
 
   val gradhessian' : (t -> t) -> t -> t * t * t
-  (** return [(f x, grad f x, hessian f x)] *)
+  (** return ``(f x, grad f x, hessian f x)`` *)
 
   val gradhessianv : (t -> t) -> t -> t -> t * t
-  (** return [(grad f x v, hessian f x v)] *)
+  (** return ``(grad f x v, hessian f x v)`` *)
 
   val gradhessianv' : (t -> t) -> t -> t -> t * t * t
-  (** return [(f x, grad f x v, hessian f x v)] *)
+  (** return ``(f x, grad f x v, hessian f x v)`` *)
 
 
   (* low-level functions, only use them if you know what you are doing. *)
@@ -369,17 +369,17 @@ module Make
   (* functions for debugging *)
 
   val to_trace : t list -> string
-  (** [to_trace [t0; t1; ...]] outputs the trace of computation graph on the
+  (** ``to_trace [t0; t1; ...]`` outputs the trace of computation graph on the
       terminal in a human-readable format.
    *)
 
   val to_dot : t list -> string
-  (** [to_dot [t0; t1; ...]] outputs the trace of computation graph in the dot
+  (** `to_dot [t0; t1; ...]`` outputs the trace of computation graph in the dot
       file format which you can use other tools further visualisation, such as
       Graphviz.
    *)
 
    val pp_num : Format.formatter -> t -> unit
-   (** [pp_num t] pretty prints the abstract number used in [Algodiff]. *)
+   (** ``pp_num t`` pretty prints the abstract number used in ``Algodiff``. *)
 
 end
