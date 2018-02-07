120efe890b3f27e9dfea1efedb34913d65a8dd5b
tptiplea <tudor.tiplea96@gmail.com>
2018-02-07 01:33:30 +0000

    fixed unit tests

--- test/unit_base_conv2d.ml
+++ test/unit_base_conv2d.ml
@@ -1,4 +1,4 @@
 (** Unit test for Convolution3D operations *)
 
 
-include Unit_conv2d_generic.Make (Owl_base_dense_ndarray.NdarrayPureSingle)
+include Unit_conv2d_generic.Make (Owl_base_dense_ndarray_s)
