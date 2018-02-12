42a1b45a703d2558b54317e071e4ef0ca1053bb1
Liang Wang <ryanrhymes@gmail.com>
2018-02-12 23:29:46 +0000

    core: change matrix.init_nd -> matrix.init_2d

--- src/owl/dense/owl_dense_matrix_z.ml
+++ src/owl/dense/owl_dense_matrix_z.ml
@@ -23,7 +23,7 @@ let create m n a = M.create Complex64 m n a
 
 let init m n f = M.init Complex64 m n f
 
-let init_nd m n f = M.init_nd Complex64 m n f
+let init_2d m n f = M.init_2d Complex64 m n f
 
 let zeros m n = M.zeros Complex64 m n
 
