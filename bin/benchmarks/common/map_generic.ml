5076b6355c13a32053f68f3ad722d86c9c49ac99
tptiplea <tudor.tiplea96@gmail.com>
2018-02-15 01:05:47 +0000

    changed map

--- bin/benchmarks/common/map_generic.ml
+++ bin/benchmarks/common/map_generic.ml
@@ -2,12 +2,16 @@ module Make(M : Owl_types.Ndarray_Basic) = struct
   let wrap_fun () =
     let _ = Printf.printf "Evaluating map operation\n" in
 
-    let _ =
-      if ((Array.length Sys.argv) <= 1)
-      then failwith "ERROR! Must provide the size of the Ndarray." in
+    let rank = (Array.length Sys.argv) - 1 in
 
-    let size = int_of_string Sys.argv.(1) in
-    let _ = Printf.printf "Evaluating Ndarray of size %d\n" size in
+    let _ =
+      if (rank <= 0)
+      then failwith "ERROR! Must provide the dimensions of the Ndarray." in
+
+    let dims = Array.init rank (fun i -> int_of_string Sys.argv.(i + 1)) in
+    let _ = Printf.printf "Evaluating Ndarray of rank %d with dims: [" rank in
+    let _ = Array.iter (fun d -> Printf.printf "%d " d) dims in
+    let _ = Printf.printf "]\n" in
 
     let f0 = (fun _ -> 0.) in
     let f1 = (fun _ -> 1.) in
@@ -17,7 +21,7 @@ module Make(M : Owl_types.Ndarray_Basic) = struct
 
     let funs = [f0; f1; f2; f3; f4] in
 
-    let v = ref (M.empty [|size|]) in
+    let v = ref (M.empty dims) in
 
     let _ =
       begin
