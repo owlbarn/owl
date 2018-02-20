28ed5601b1741d7a7cde7d075aeae57a668feb46
Maria (Majava San) <majava.san@gmail.com>
2018-02-20 19:50:08 +0000

    add documentation for owl_maths_root.mli

--- src/owl/maths/owl_maths_root.mli
+++ src/owl/maths/owl_maths_root.mli
@@ -6,13 +6,56 @@
 (** Root finding algorithms *)
 
 val bisec : ?max_iter:int -> ?xtol:float -> (float -> float) -> float -> float -> float
-(** TODO *)
+(**
+``bisec f a b`` tries to find the root of univariate function ``f`` in the
+bracket defined by ``[a, b]``.
+
+Parameters:
+  * ``max_iter``: maximum number of iterations.
+  * ``xtol``: the tolerance of ``x`` on abscissa.
+  * ``f``: the univariate scalar function to find root.
+  * ``a``: boundary of bracket.
+  * ``b``: boundary of bracket.
+ *)
 
 val false_pos : ?max_iter:int -> ?xtol:float -> (float -> float) -> float -> float -> float
-(** TODO *)
+(**
+``false_pos f a b`` tries to find the root of univariate function ``f`` in the
+bracket defined by ``[a, b]``.
+
+Parameters:
+  * ``max_iter``: maximum number of iterations.
+  * ``xtol``: the tolerance of ``x`` on abscissa.
+  * ``f``: the univariate scalar function to find root.
+  * ``a``: boundary of bracket.
+  * ``b``: boundary of bracket.
+ *)
 
 val ridder : ?max_iter:int -> ?xtol:float -> (float -> float) -> float -> float -> float
-(** TODO *)
+(**
+``ridder f a b`` tries to find the root of univariate function ``f`` in the
+bracket defined by ``[a, b]``.
+
+Parameters:
+  * ``max_iter``: maximum number of iterations.
+  * ``xtol``: the tolerance of ``x`` on abscissa.
+  * ``f``: the univariate scalar function to find root.
+  * ``a``: boundary of bracket.
+  * ``b``: boundary of bracket.
+ *)
 
 val brent : ?max_iter:int -> ?xtol:float -> (float -> float) -> float -> float -> float
-(** TODO *)
+(**
+``brent f a b`` tries to find the root of univariate function ``f`` in the
+bracket defined by ``[a, b]``.
+
+Parameters:
+  * ``max_iter``: maximum number of iterations.
+  * ``xtol``: the tolerance of ``x`` on abscissa.
+  * ``f``: the univariate scalar function to find root.
+  * ``a``: boundary of bracket.
+  * ``b``: boundary of bracket.
+
+Refer to Brent, R. P., Algorithms for Minimization Without Derivatives. Englewood Cliffs, NJ: Prentice-Hall, 1973. Ch. 3-4.
+
+ *)
