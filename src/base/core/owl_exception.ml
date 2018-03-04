b115752237ace5657344d5a9180950b6c70aae89
Liang Wang <ryanrhymes@gmail.com>
2018-03-04 16:14:26 +0200

    add exception module

---
+++ src/base/core/owl_exception.ml
@@ -0,0 +1,11 @@
+(*
+ * OWL - an OCaml numerical library for scientific computing
+ * Copyright (c) 2016-2018 Liang Wang <liang.wang@cl.cam.ac.uk>
+ *)
+
+
+exception FOUND
+
+exception NOT_FOUND
+
+exception EMPTY_ARRAY
---
+++ src/base/core/owl_exception.mli
@@ -0,0 +1,14 @@
+(*
+ * OWL - an OCaml numerical library for scientific computing
+ * Copyright (c) 2016-2018 Liang Wang <liang.wang@cl.cam.ac.uk>
+ *)
+
+
+exception FOUND
+(** TODO *)
+
+exception NOT_FOUND
+(** TODO *)
+
+exception EMPTY_ARRAY
+(** Exception for empty array *)
