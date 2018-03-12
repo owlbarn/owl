1f2c833f4b1145696ec71e5f1b6bf806e72587bf
Liang Wang <ryanrhymes@gmail.com>
2018-03-12 18:02:28 +0000

    Update owl_zoo_cmd.mli

--- src/zoo/owl_zoo_cmd.mli
+++ src/zoo/owl_zoo_cmd.mli
@@ -10,7 +10,7 @@ val remove_gist : string -> unit
 (** Remove a local gist of given id. *)
 
 val upload_gist : string -> string
-(** Upload a zoo bundle of given path to the gist server and return a gist id *)
+(** Upload a zoo bundle of given path to the gist server and return a gist id. *)
 
 val download_gist : string -> unit
 (** Download the gist of given id. *)
