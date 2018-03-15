705ea160bbe4e20ac49192da1b8c4708890a7be9
Liang Wang <ryanrhymes@gmail.com>
2018-03-15 19:18:56 +0000

    core: minor fix

--- src/owl/core/owl_ndarray_conv_impl.c
+++ src/owl/core/owl_ndarray_conv_impl.c
@@ -98,7 +98,8 @@ value FUN_NATIVE (spatial) (
 value FUN_BYTE (spatial) (value * argv, int argn) {
   return FUN_NATIVE (spatial) (
     argv[0], argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7],
-    argv[8], argv[9], argv[10], argv[11], argv[12], argv[13], argv[14], argv[15], argv[16]
+    argv[8], argv[9], argv[10], argv[11], argv[12], argv[13], argv[14],
+    argv[15], argv[16]
   );
 }
 
@@ -414,7 +415,8 @@ value FUN_NATIVE (cuboid) (
 value FUN_BYTE (cuboid) (value * argv, int argn) {
   return FUN_NATIVE (cuboid) (
     argv[0], argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7],
-    argv[8], argv[9], argv[10], argv[11], argv[12], argv[13], argv[14], argv[15], argv[16], argv[17], argv[18]
+    argv[8], argv[9], argv[10], argv[11], argv[12], argv[13], argv[14],
+    argv[15], argv[16], argv[17], argv[18]
   );
 }
 
@@ -533,7 +535,8 @@ value FUN_NATIVE (cuboid_backward_kernel) (
 value FUN_BYTE (cuboid_backward_kernel) (value * argv, int argn) {
   return FUN_NATIVE (cuboid_backward_kernel) (
     argv[0], argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7],
-    argv[8], argv[9], argv[10], argv[11], argv[12], argv[13], argv[14], argv[15], argv[16], argv[17]
+    argv[8], argv[9], argv[10], argv[11], argv[12], argv[13], argv[14],
+    argv[15], argv[16], argv[17]
   );
 }
 
@@ -641,7 +644,8 @@ value FUN_NATIVE (cuboid_backward_input) (
 value FUN_BYTE (cuboid_backward_input) (value * argv, int argn) {
   return FUN_NATIVE (cuboid_backward_input) (
     argv[0], argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7],
-    argv[8], argv[9], argv[10], argv[11], argv[12], argv[13], argv[14], argv[15], argv[16], argv[17]
+    argv[8], argv[9], argv[10], argv[11], argv[12], argv[13], argv[14],
+    argv[15], argv[16], argv[17]
   );
 }
 
