e485149efc3a93e789127b3b8b26c6c62dd4b539
jrzhao <rho.ajax@gmail.com>
2018-03-28 00:45:57 +0100

    fix owl list cmd

--- bin/owl_list_gist.sh
+++ bin/owl_list_gist.sh
@@ -12,9 +12,18 @@ fi
 DIRS=`ls -d $DIRNAME/* | xargs echo`
 
 for DIR in $DIRS; do
+
+  if [ -f $DIR ]; then
+    continue;
+  fi
+
   ID=`basename $DIR`
-  if [ -f $DIR/readme.md ]; then
-    INFO=`head -n 1 $DIR/readme.md`
+  SUBDIR=`ls -dt $DIR/* | head -n 1 | xargs echo`
+
+  if [ -f $DIR/\#readme.md ]; then
+    INFO=`head -n 1 $DIR/\#readme.md`
+  elif [ -f $SUBDIR/\#readme.md ]; then
+    INFO=`head -n 1 $SUBDIR/\#readme.md`
   else
     INFO=""
   fi
