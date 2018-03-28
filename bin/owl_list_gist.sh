3f73293580dc933cd062b3ffa119f4d31e729ed6
jrzhao <rho.ajax@gmail.com>
2018-03-28 09:19:30 +0100

    minor fix

--- bin/owl_list_gist.sh
+++ bin/owl_list_gist.sh
@@ -12,14 +12,11 @@ fi
 DIRS=`ls -d $DIRNAME/* | xargs echo`
 
 for DIR in $DIRS; do
-
   if [ -f $DIR ]; then
-    continue;
+    continue
   fi
-
   ID=`basename $DIR`
   SUBDIR=`ls -dt $DIR/* | head -n 1 | xargs echo`
-
   if [ -f $DIR/\#readme.md ]; then
     INFO=`head -n 1 $DIR/\#readme.md`
   elif [ -f $SUBDIR/\#readme.md ]; then
