559b6994d16c70df973da225ec4993a9a28e2b01
Liang Wang <ryanrhymes@gmail.com>
2018-03-29 01:59:02 +0100

    tweak building

--- src/owl/config/discover.ml
+++ src/owl/config/discover.ml
@@ -1,13 +1,29 @@
-open Base
-open Stdio
 module C = Configurator
 
+
 let write_sexp fn sexp =
-  Out_channel.write_all fn ~data:(Sexp.to_string sexp)
+  Stdio.Out_channel.write_all fn ~data:(Base.Sexp.to_string sexp)
+
+
+let get_gcc_path () =
+  let p0 = "/usr/local/lib/gcc/7" in
+  if Sys.file_exists p0 then "-L" ^ p0
+  else ""
+
+
+let get_openblas_path () =
+  let p0 = "/usr/local/opt/openblas/lib" in
+  if Sys.file_exists p0 then "-L" ^ p0
+  else ""
+
 
 let () =
   C.main ~name:"owl" (fun c ->
-    let libs   = [] in
+    let libs = List.filter ((<>) "") [
+      get_gcc_path ();
+      get_openblas_path ();
+    ]
+    in
     let cflags = [] in
     let default : C.Pkg_config.package_conf = { cflags; libs } in
 
@@ -15,15 +31,16 @@ let () =
       match C.Pkg_config.get c with
       | None    -> default
       | Some pc -> (
-        Option.value_map (C.ocaml_config_var c "system") ~default ~f:(function
-          | "linux" | "linux_elf" -> default
-          | "macosx" ->
-              { default with libs = "-framework" :: "Accelerate" :: libs }
-          | "mingw64" -> default
-          | _ -> default)
+          Base.Option.value_map (C.ocaml_config_var c "system") ~default ~f:(function
+            | "linux"     -> default
+            | "linux_elf" -> default
+            | "macosx"    -> { default with libs = "-framework" :: "Accelerate" :: libs }
+            | "mingw64"   -> default
+            | _           -> default
+          )
         )
     in
 
-    write_sexp "c_flags.sexp"         (sexp_of_list sexp_of_string conf.cflags);
-    write_sexp "c_library_flags.sexp" (sexp_of_list sexp_of_string conf.libs)
+    write_sexp "c_flags.sexp"         Base.(sexp_of_list sexp_of_string conf.cflags);
+    write_sexp "c_library_flags.sexp" Base.(sexp_of_list sexp_of_string conf.libs)
   )
