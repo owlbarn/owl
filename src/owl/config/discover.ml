0fed1a3c513119d8291bdbb24071ced39447151f
Liang Wang <ryanrhymes@gmail.com>
2018-04-01 17:49:32 +0100

    improve configure and build

--- src/owl/config/discover.ml
+++ src/owl/config/discover.ml
@@ -15,6 +15,46 @@ let get_os_type c =
   match sys with Some s -> s | None -> ""
 
 
+let get_default_cflags c = [
+  (* Basic optimisation *)
+  "-g"; "-O3"; "-Ofast";
+  (* Experimental switches, -ffast-math may break IEEE754 semantics*)
+  "-march=native"; "-mfpmath=sse"; "-funroll-loops"; "-ffast-math";
+  (* Configure Mersenne Twister RNG *)
+  "-DSFMT_MEXP=19937"; "-msse2"; "-fno-strict-aliasing";
+]
+
+
+let get_default_libs c = [
+  "-lopenblas";
+  "-lgfortran";
+  "-lm";
+]
+
+
+let get_expmode_cflags c =
+  let enable_expmode = Sys.getenv "ENABLE_EXPMODE" |> int_of_string in
+  if enable_expmode = 0 then []
+  else if enable_expmode = 1 then [
+    "-flto";
+  ]
+  else failwith "Error: ENABLE_EXPMODE only accepts 0/1."
+
+
+let get_devmode_cflags c =
+  let enable_devmode = Sys.getenv "ENABLE_DEVMODE" |> int_of_string in
+  if enable_devmode = 0 then [
+    "-Wno-logical-op-parentheses"
+  ]
+  else if enable_devmode = 1 then [
+    "-Wall";
+    "-pedantic";
+    "-Wextra";
+    "-Wunused";
+  ]
+  else failwith "Error: ENABLE_DEVMODE only accepts 0/1."
+
+
 let get_gcc_path c =
   let p0 = "/usr/local/lib/gcc/7" in
   if Sys.file_exists p0 then ["-L" ^ p0]
@@ -66,6 +106,7 @@ let () =
   C.main ~name:"owl" (fun c ->
     (* configure link options *)
     let libs = []
+      @ get_default_libs c
       @ get_gcc_path c
       @ get_openblas_path c
       @ get_accelerate_libs c
@@ -74,6 +115,9 @@ let () =
 
     (* configure compile options *)
     let cflags = []
+      @ get_default_cflags c
+      @ get_devmode_cflags c
+      @ get_expmode_cflags c
       @ get_openmp_cflags c
     in
 
