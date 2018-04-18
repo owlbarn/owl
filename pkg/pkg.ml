3378c8c581835f9cbdfe136e014c8244dc40a6fd
Richard Mortier <mort@cantab.net>
2018-04-18 22:15:47 +0100

    prep release for all packages

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -4,8 +4,19 @@
 
 open Topkg
 
-let publish =
-  Pkg.publish ~artefacts:[`Distrib] ()
-
 let () =
-  Topkg_jbuilder.describe ~publish ()
+  let opams =
+    let install = false in [
+        Pkg.opam_file ~install "owl-base.opam";
+        Pkg.opam_file ~install "owl.opam";
+        Pkg.opam_file ~install "owl-zoo.opam";
+        Pkg.opam_file ~install "owl-top.opam";
+      ]
+  in
+  Pkg.describe ~opams "owl-base"
+  @@ fun c ->
+     match Conf.pkg_name c with
+     | ( "owl-base" | "owl" | "owl-zoo" | "owl-top")  as n
+       -> Ok [ Pkg.lib (n ^ ".opam") ~dst:"opam"; ]
+     | other
+       -> R.error_msgf "unknown package name: %s" other
