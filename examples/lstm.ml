cf0646735b49d5b50a337959f56b4a026bb41ca7
Liang Wang <ryanrhymes@gmail.com>
2018-04-22 00:30:53 +0300

    edit examples

--- examples/lstm.ml
+++ examples/lstm.ml
@@ -1,6 +1,6 @@
 #!/usr/bin/env owl
 (* Text Generation using LSTM.
- * The book used is Alice’s Adventures in Wonderland by Lewis Carroll.
+   The book used is Alice’s Adventures in Wonderland by Lewis Carroll.
  *)
 
 #zoo "217ef87bc36845c4e78e398d52bc4c5b"
@@ -8,17 +8,16 @@
 open Owl
 open Neural.S
 open Neural.S.Graph
-open Algodiff.S
 
 
 let prepare wndsz stepsz =
   Owl_log.info "build vocabulary ...";
-  let txt = load_file ~gist:"217ef87bc36845c4e78e398d52bc4c5b" "wonderland.txt" in
-  let chars = String.lowercase_ascii txt in
-  let vocab = Owl_nlp.Vocabulary.build_from_string ~alphabet:true chars in
+  let text = load_file ~gist:"217ef87bc36845c4e78e398d52bc4c5b" "wonderland.txt" in
+  let chars = String.lowercase_ascii text in
+  let vocab = Nlp.Vocabulary.build_from_string ~alphabet:true chars in
 
   Owl_log.info "tokenise ...";
-  let tokens = Owl_nlp.Vocabulary.tokenise vocab chars in
+  let tokens = Nlp.Vocabulary.tokenise vocab chars in
 
   Owl_log.info "make x matrix (indices) ...";
   let m = (String.length chars - wndsz) / stepsz in
@@ -30,14 +29,14 @@ let prepare wndsz stepsz =
   done;
 
   Owl_log.info "make y matrix (one-hot) ...";
-  let y = Dense.Matrix.S.zeros m (Owl_nlp.Vocabulary.length vocab) in
+  let y = Dense.Matrix.S.zeros m (Nlp.Vocabulary.length vocab) in
   for i = 0 to m - 1 do
     let j = int_of_float (float_of_int tokens.(i*stepsz + wndsz)) in
     Dense.Matrix.S.set y i j 1.
   done;
 
   Owl_log.info "chars:%i, symbols:%i, wndsz:%i, stepsz:%i"
-    (String.length txt) (Owl_nlp.Vocabulary.length vocab) wndsz stepsz;
+    (String.length text) (Nlp.Vocabulary.length vocab) wndsz stepsz;
   vocab, x, y
 
 
@@ -55,15 +54,14 @@ let test nn vocab wndsz tlen x =
   let nxt_char = Dense.Matrix.S.zeros 1 1 in
   for i = 0 to tlen - 1 do
     let xt = Dense.Matrix.S.get_slice [[];[i;i+wndsz-1]] !all_char in
-    let yt = Graph.run (Arr xt) nn |> unpack_arr in
+    let yt = Graph.run (Arr xt) nn |> Algodiff.S.unpack_arr in
     let _, next_i = Dense.Matrix.S.max_i yt in
     Dense.Matrix.S.set nxt_char 0 0 (float_of_int next_i.(1));
     all_char := Dense.Matrix.S.(!all_char @|| nxt_char)
   done;
   Dense.Matrix.S.get_slice [[];[wndsz;-1]] !all_char
   |> Dense.Matrix.S.to_array
-  |> Array.map (fun i -> Owl_nlp.Vocabulary.index2word vocab (int_of_float i))
-  |> Array.fold_left (fun a c -> a ^ c) ""
+  |> Array.fold_left (fun a i -> a ^ Nlp.Vocabulary.index2word vocab (int_of_float i)) ""
   |> Printf.printf "generated text: %s\n"
   |> flush_all
 
@@ -71,7 +69,7 @@ let test nn vocab wndsz tlen x =
 let train () =
   let wndsz = 100 and stepsz = 1 in
   let vocab, x, y = prepare wndsz stepsz in
-  let vocabsz = Owl_nlp.Vocabulary.length vocab in
+  let vocabsz = Nlp.Vocabulary.length vocab in
 
   let network = make_network wndsz vocabsz in
   Graph.print network;
