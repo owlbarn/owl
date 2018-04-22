5fd50149ae8ab50ed6421e3281790bbe27800e9b
jrzhao <rho.ajax@gmail.com>
2018-04-22 22:40:11 +0100

    add openmp to conv2d_trans code

--- src/owl/core/owl_ndarray_conv_impl.h
+++ src/owl/core/owl_ndarray_conv_impl.h
@@ -751,6 +751,9 @@ CAMLprim value FUN_NATIVE (spatial_transpose) (
     }
   }
 
+  #ifdef _OPENMP
+    #pragma omp parallel for schedule(static)
+  #endif /* _OPENMP */
   for (int i = 0; i < output_crb; ++i) {
     int bt = i / output_cr;
     int cr = i % output_cr;
@@ -882,6 +885,9 @@ CAMLprim value FUN_NATIVE (spatial_transpose_backward_kernel) (
     }
   }
 
+  #ifdef _OPENMP
+    #pragma omp parallel for schedule(static)
+  #endif /* _OPENMP */
   for (int i = 0; i < output_crb; ++i) {
     int bt = i / output_cr;
     int cr = i % output_cr;
