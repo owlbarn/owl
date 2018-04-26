54125d32902c31cc46d63d1321949a2ed00b7300
Liang Wang <ryanrhymes@gmail.com>
2018-04-26 22:39:36 +0300

    minor edit

--- src/base/misc/owl_dataframe.ml
+++ src/base/misc/owl_dataframe.ml
@@ -3,4 +3,52 @@
  * Copyright (c) 2016-2017 Liang Wang <liang.wang@cl.cam.ac.uk>
  *)
 
-type t
+
+type elt =
+  | Int    of int
+  | Float  of float
+  | String of string
+
+
+type t = {
+  mutable data : elt array array;
+  mutable head : (string, int) Hashtbl.t;
+  mutable used : int;
+  mutable size : int;
+}
+
+
+let make head_names =
+  let col_num = Array.length head_names in
+  let data = Array.make col_num [||] in
+  let head = Hashtbl.create 64 in
+  Array.iteri (fun i s ->
+    assert (Hashtbl.mem head s = false);
+    Hashtbl.add head s i
+  ) head_names;
+  let used = 0 in
+  let size = 0 in
+  { data; head; used; size }
+
+
+let allocate_space data =
+  Array.map (fun col ->
+    Array.(append col (copy col))
+  ) data
+
+
+let append x row =
+  if x.size = 0 then (
+    let n = 16 in
+    x.data <- Array.(map (make n) row);
+    x.size <- n;
+    x.used <- 1
+  )
+  else (
+    if x.used = x.size then (
+      x.data <- allocate_space x.data;
+      x.size <- Array.length x.data.(0)
+    );
+    Array.iteri (fun i a -> x.data.(i).(x.used) <- a) row;
+    x.used <- x.used + 1
+  )
--- src/owl/core/owl_ndarray_sort_impl.h
+++ src/owl/core/owl_ndarray_sort_impl.h
@@ -10,111 +10,111 @@
 #include <assert.h>
 
 #define SWAPINIT(a, es) swaptype = ((char *)a - (char *)0) % sizeof(long) || \
-	es % sizeof(long) ? 2 : es == sizeof(long)? 0 : 1;
+  es % sizeof(long) ? 2 : es == sizeof(long)? 0 : 1;
 
 #define vecswap(a, b, n) if ((n) > 0) swapfunc((a), (b), (size_t)(n), swaptype)
 
 #define swap(a, b)                                \
   if (swaptype == 0) {                            \
     long t = *(long *)(void *)(a);                \
-		*(long *)(void *)(a) = *(long *)(void *)(b);  \
-		*(long *)(void *)(b) = t;                     \
+    *(long *)(void *)(a) = *(long *)(void *)(b);  \
+    *(long *)(void *)(b) = t;                     \
   } else                                          \
     swapfunc(a, b, es, swaptype)
 
 static OWL_INLINE void swapfunc(char *a, char *b, size_t n, int swaptype) {
-	if (swaptype <= 1)
-		SWAPCODE(long, a, b, n)
-	else
-		SWAPCODE(char, a, b, n)
+  if (swaptype <= 1)
+    SWAPCODE(long, a, b, n)
+  else
+    SWAPCODE(char, a, b, n)
 }
 
 static OWL_INLINE char *med3(char *a, char *b, char *c, char *z,
   int (*cmp)(const void *, const void *, const void *)) {
 
-	return cmp(a, b, z) < 0 ?
-	       (cmp(b, c, z) < 0 ? b : (cmp(a, c, z) < 0 ? c : a ))
+  return cmp(a, b, z) < 0 ?
+         (cmp(b, c, z) < 0 ? b : (cmp(a, c, z) < 0 ? c : a ))
               :(cmp(b, c, z) > 0 ? b : (cmp(a, c, z) < 0 ? a : c ));
 }
 
 void owl_qsort_r(void *a, size_t n, size_t es, void *z,
   int (*cmp)(const void *, const void *, const void *)) {
 
-	char *pa, *pb, *pc, *pd, *pl, *pm, *pn;
-	size_t d, r, s;
-	int swaptype, cmp_result;
+  char *pa, *pb, *pc, *pd, *pl, *pm, *pn;
+  size_t d, r, s;
+  int swaptype, cmp_result;
 
-	assert((a != NULL || n == 0 || es == 0) && (cmp != NULL));
+  assert((a != NULL || n == 0 || es == 0) && (cmp != NULL));
 
-loop:	SWAPINIT(a, es);
-	if (n < 7) {
-		for (pm = (char *) a + es; pm < (char *) a + n * es; pm += es)
-			for (pl = pm; pl > (char *) a && cmp(pl - es, pl, z) > 0; pl -= es)
-				swap(pl, pl - es);
-		return;
-	}
-	pm = (char *) a + (n / 2) * es;
-	if (n > 7) {
-		pl = (char *) a;
-		pn = (char *) a + (n - 1) * es;
-		if (n > 40) {
-			d = (n / 8) * es;
-			pl = med3(pl, pl + d, pl + 2 * d, z, cmp);
-			pm = med3(pm - d, pm, pm + d, z, cmp);
-			pn = med3(pn - 2 * d, pn - d, pn, z, cmp);
-		}
-		pm = med3(pl, pm, pn, z, cmp);
-	}
-	swap(a, pm);
-	pa = pb = (char *) a + es;
+loop:  SWAPINIT(a, es);
+  if (n < 7) {
+    for (pm = (char *) a + es; pm < (char *) a + n * es; pm += es)
+      for (pl = pm; pl > (char *) a && cmp(pl - es, pl, z) > 0; pl -= es)
+        swap(pl, pl - es);
+    return;
+  }
+  pm = (char *) a + (n / 2) * es;
+  if (n > 7) {
+    pl = (char *) a;
+    pn = (char *) a + (n - 1) * es;
+    if (n > 40) {
+      d = (n / 8) * es;
+      pl = med3(pl, pl + d, pl + 2 * d, z, cmp);
+      pm = med3(pm - d, pm, pm + d, z, cmp);
+      pn = med3(pn - 2 * d, pn - d, pn, z, cmp);
+    }
+    pm = med3(pl, pm, pn, z, cmp);
+  }
+  swap(a, pm);
+  pa = pb = (char *) a + es;
 
-	pc = pd = (char *) a + (n - 1) * es;
-	for (;;) {
-		while (pb <= pc && (cmp_result = cmp(pb, a, z)) <= 0) {
-			if (cmp_result == 0) {
-				swap(pa, pb);
-				pa += es;
-			}
-			pb += es;
-		}
-		while (pb <= pc && (cmp_result = cmp(pc, a, z)) >= 0) {
-			if (cmp_result == 0) {
-				swap(pc, pd);
-				pd -= es;
-			}
-			pc -= es;
-		}
-		if (pb > pc)
-			break;
-		swap(pb, pc);
-		pb += es;
-		pc -= es;
-	}
+  pc = pd = (char *) a + (n - 1) * es;
+  for (;;) {
+    while (pb <= pc && (cmp_result = cmp(pb, a, z)) <= 0) {
+      if (cmp_result == 0) {
+        swap(pa, pb);
+        pa += es;
+      }
+      pb += es;
+    }
+    while (pb <= pc && (cmp_result = cmp(pc, a, z)) >= 0) {
+      if (cmp_result == 0) {
+        swap(pc, pd);
+        pd -= es;
+      }
+      pc -= es;
+    }
+    if (pb > pc)
+      break;
+    swap(pb, pc);
+    pb += es;
+    pc -= es;
+  }
 
-	pn = (char *) a + n * es;
-	r = OWL_MIN(pa - (char *) a, pb - pa);
-	vecswap(a, pb - r, r);
-	r = OWL_MIN((size_t)(pd - pc), pn - pd - es);
-	vecswap(pb, pn - r, r);
+  pn = (char *) a + n * es;
+  r = OWL_MIN(pa - (char *) a, pb - pa);
+  vecswap(a, pb - r, r);
+  r = OWL_MIN((size_t)(pd - pc), pn - pd - es);
+  vecswap(pb, pn - r, r);
 
-	r = pb - pa;
-	s = pd - pc;
-	if (r < s) {
-		if (s > es) {
-			if (r > es)
-				owl_qsort_r(a, r / es, es, z, cmp);
-			a = pn - s;
-			n = s / es;
-			goto loop;
-		}
-	} else {
-		if (r > es) {
-			if (s > es)
-				owl_qsort_r(pn - s, s / es, es, z, cmp);
-			n = r / es;
-			goto loop;
-		}
-	}
+  r = pb - pa;
+  s = pd - pc;
+  if (r < s) {
+    if (s > es) {
+      if (r > es)
+        owl_qsort_r(a, r / es, es, z, cmp);
+      a = pn - s;
+      n = s / es;
+      goto loop;
+    }
+  } else {
+    if (r > es) {
+      if (s > es)
+        owl_qsort_r(pn - s, s / es, es, z, cmp);
+      n = r / es;
+      goto loop;
+    }
+  }
 }
 
 
