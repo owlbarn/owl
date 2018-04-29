4e9d05a319fe7cd62b28a203069babba7a84431a
Liang Wang <ryanrhymes@gmail.com>
2018-04-29 14:48:22 +0300

    move io module to base

--- src/base/misc/owl_io.mli
+++ src/base/misc/owl_io.mli
@@ -4,11 +4,20 @@
  *)
 
 
+(** {6 Read and write operations} *)
+
 val read_file : ?trim:bool -> string -> string array
 
 val read_file_string : string -> string
 
 val write_file : string -> string -> unit
+
+val marshal_from_file : string -> 'a
+
+val marshal_to_file : 'a -> string -> unit
+
+
+(** {6 Iteration functions} *)
 
 val iteri_lines_of_file : ?verbose:bool -> (int -> string -> unit) -> string -> unit
 
@@ -17,7 +26,3 @@ val mapi_lines_of_file : (int -> string -> 'a) -> string -> 'a array
 val iteri_lines_of_marshal : ?verbose:bool -> (int -> 'a -> 'b) -> string -> unit
 
 val mapi_lines_of_marshal : (int -> 'a -> 'b) -> string -> 'b array
-
-val marshal_from_file : string -> 'a
-
-val marshal_to_file : 'a -> string -> unit
