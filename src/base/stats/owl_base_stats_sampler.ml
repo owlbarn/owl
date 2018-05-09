9b3f0d4642f066ccabcf4f886707b16aa5970b38
Liang Wang <ryanrhymes@gmail.com>
2018-05-09 21:02:23 +0300

    coding sampler ...

--- src/base/stats/owl_base_stats_sampler.ml
+++ src/base/stats/owl_base_stats_sampler.ml
@@ -4,19 +4,19 @@
  *)
 
 
-op
-
-let metropolis ?burnin ?thin ?step ~inital ~proposal ~pdf nsamples =
+let metropolis ?burnin ?thin ~inital ~proprvs ~proppdf ~pdf nsamples =
   let step = match step with Some a -> a | None -> 0.1 in
   let burnin = match burnin with Some a -> a | None -> 1000 in
   let thin = match thin with Some a -> a | None -> 10 in
 
-  let p = Array.copy inital in
-  let n = burnin + thin * nsamples in
-  let s = Array.make n p in
-  for i = 0 to n - 1 do
-    let p' = Array.map (fun x -> gaussian_rvs ~mu:0. ~sigma:stepsize +. x) p in
-    let y, y' = f p, f p' in
+  let niter = burnin + thin * nsamples in
+  let x = Array.copy inital in
+  let samples = Array.make niter x in
+
+  for i = 0 to niter - 1 do
+    let x' = Array.map proprvs x in
+    let y = f x in
+    let y' = f x' in
     let p' = (
       if y' >= y then p'
       else if std_uniform_rvs () < (y' /. y) then p'
@@ -24,4 +24,6 @@ let metropolis ?burnin ?thin ?step ~inital ~proposal ~pdf nsamples =
     Array.iteri (fun i x -> p.(i) <- x) p';
     if (i >= burnin) && (i mod thin = 0) then
       s.( (i - burnin) / thin ) <- (Array.copy p)
-  done; s
+  done;
+
+  s
