96fd0da54bf9afad55b5dc3b46dd87fe1150c8f5
Liang Wang <ryanrhymes@gmail.com>
2018-05-10 00:43:30 +0300

    coding sampler ...

--- src/owl/stats/owl_stats_sampler.ml
+++ src/owl/stats/owl_stats_sampler.ml
@@ -4,26 +4,42 @@
  *)
 
 
-let metropolis ?burnin ?thin ~inital ~proprvs ~proppdf ~pdf nsamples =
-  let step = match step with Some a -> a | None -> 0.1 in
+let metropolis ?burnin ?thin ~initial ~proprvs ~proppdf ~pdf nsamples =
   let burnin = match burnin with Some a -> a | None -> 1000 in
   let thin = match thin with Some a -> a | None -> 10 in
 
+  let accept = ref 0 in
   let niter = burnin + thin * nsamples in
-  let x = Array.copy inital in
-  let samples = Array.make niter x in
+  let samples = Array.make niter [||] in
+  samples.(0) <- Array.copy initial;
 
-  for i = 0 to niter - 1 do
-    let x' = Array.map proprvs x in
-    let y = f x in
-    let y' = f x' in
-    let p' = (
-      if y' >= y then p'
-      else if std_uniform_rvs () < (y' /. y) then p'
-      else Array.copy p ) in
-    Array.iteri (fun i x -> p.(i) <- x) p';
-    if (i >= burnin) && (i mod thin = 0) then
-      s.( (i - burnin) / thin ) <- (Array.copy p)
+  for i = 1 to niter - 1 do
+    let x  = samples.(i - 1) in
+    let x' = proprvs x in
+    let p  = log (pdf x) in
+    let p' = log (pdf x') in
+    let g  = log (proppdf x x') in
+    let g' = log (proppdf x' x) in
+    let a  = min 0. (p' -. p +. g -. g') in
+    let b =
+      if a >= 0. then true
+      else if log (Owl_stats_dist.std_uniform_rvs ()) < a then true
+      else false
+    in
+    let x' = if b then x' else Array.copy x in
+    if b then accept := !accept + 1;
+    samples.(i) <- x'
   done;
+  Owl_log.info "accept ratio: %g" (float_of_int (!accept) /. float_of_int niter);
+  Owl_utils.Array.filteri (fun i _ ->
+    (i >= burnin) && (i mod thin = 0)
+  ) samples
 
-  s
+
+let gibbs = None
+
+
+let rejection = None
+
+
+let adaptive_rejection = None
