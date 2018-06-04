7952dd9dd219cfe5758260b2b93ac9dc7df41ec5
Liang Wang <ryanrhymes@gmail.com>
2018-06-04 10:56:36 +0300

    Update owl_utils_array.ml

--- src/base/misc/owl_utils_array.ml
+++ src/base/misc/owl_utils_array.ml
@@ -342,17 +342,19 @@ let index_of x a =
 let bsearch ~cmp k bin_edges =
   let rec aux i j =
     if i > j then j
-    else let middle = i + (j - i) / 2 in (* avoid overflow *)
+    else 
+      let middle = i + (j - i) / 2 in (* avoid overflow *)
       match cmp k bin_edges.(middle) with
-      | 0 -> middle
+      | 0            -> middle
       | n when n < 0 -> aux i (middle - 1)
-      | _ -> aux (middle + 1) j in
+      | _            -> aux (middle + 1) j in
   let n = Array.length bin_edges in
   if n = 0 then failwith "empty array"
-  else match cmp bin_edges.(0) k, cmp bin_edges.(n-1) k with
-    | c, _ when c > 0 -> -1
+  else 
+    match cmp bin_edges.(0) k, cmp bin_edges.(n-1) k with
+    | c, _ when c > 0  -> -1
     | _, c when c <= 0 -> n
-    | _ -> aux 0 (n-1)
+    | _                -> aux 0 (n-1)
 
 
 (* ends here *)
