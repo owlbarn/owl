42881a2d1625d49beeec05844295c08c26a3afed
jrzhao <rho.ajax@gmail.com>
2018-06-19 12:35:53 +0100

    optimise repeat opreation

--- src/owl/core/owl_ndarray_repeat.ml
+++ src/owl/core/owl_ndarray_repeat.ml
@@ -7,7 +7,7 @@ open Bigarray
 
 open Owl_core_types
 
-
+(*
 external owl_float32_ndarray_repeat : ('a, 'b) owl_arr -> ('a, 'b) owl_arr -> int -> int -> int -> int -> int -> int -> int -> int -> unit = "stub_float32_ndarray_repeat_byte" "stub_float32_ndarray_repeat_native"
 external owl_float64_ndarray_repeat : ('a, 'b) owl_arr -> ('a, 'b) owl_arr -> int -> int -> int -> int -> int -> int -> int -> int -> unit = "stub_float64_ndarray_repeat_byte" "stub_float64_ndarray_repeat_native"
 external owl_complex32_ndarray_repeat : ('a, 'b) owl_arr -> ('a, 'b) owl_arr -> int -> int -> int -> int -> int -> int -> int -> int -> unit = "stub_complex32_ndarray_repeat_byte" "stub_complex32_ndarray_repeat_native"
@@ -16,6 +16,22 @@ external owl_complex64_ndarray_repeat : ('a, 'b) owl_arr -> ('a, 'b) owl_arr -> 
 
 let _ndarray_repeat
   : type a b. (a, b) kind -> (a, b) owl_arr -> (a, b) owl_arr -> int -> int -> int -> int -> int -> int -> int -> int -> unit
+  = function
+  | Float32   -> owl_float32_ndarray_repeat
+  | Float64   -> owl_float64_ndarray_repeat
+  | Complex32 -> owl_complex32_ndarray_repeat
+  | Complex64 -> owl_complex64_ndarray_repeat
+  | _         -> failwith "_ndarray_repeat: unsupported operation"
+*)
+
+external owl_float32_ndarray_repeat : ('a, 'b) owl_arr -> ('a, 'b) owl_arr -> int array -> int array -> int array -> int array -> int array -> int array -> int -> int -> int -> unit = "stub_float32_ndarray_repeat_byte" "stub_float32_ndarray_repeat_native"
+external owl_float64_ndarray_repeat : ('a, 'b) owl_arr -> ('a, 'b) owl_arr -> int array -> int array -> int array -> int array -> int array -> int array -> int -> int -> int -> unit = "stub_float64_ndarray_repeat_byte" "stub_float64_ndarray_repeat_native"
+external owl_complex32_ndarray_repeat : ('a, 'b) owl_arr -> ('a, 'b) owl_arr -> int array -> int array -> int array -> int array -> int array -> int array -> int -> int -> int -> unit = "stub_complex32_ndarray_repeat_byte" "stub_complex32_ndarray_repeat_native"
+external owl_complex64_ndarray_repeat : ('a, 'b) owl_arr -> ('a, 'b) owl_arr -> int array -> int array -> int array -> int array -> int array -> int array -> int -> int -> int -> unit = "stub_complex64_ndarray_repeat_byte" "stub_complex64_ndarray_repeat_native"
+
+
+let _ndarray_repeat
+  : type a b. (a, b) kind -> (a, b) owl_arr -> (a, b) owl_arr -> int array -> int array -> int array -> int array -> int array -> int array -> int -> int -> int -> unit
   = function
   | Float32   -> owl_float32_ndarray_repeat
   | Float64   -> owl_float64_ndarray_repeat
--- src/owl/core/owl_ndarray_repeat_impl.h
+++ src/owl/core/owl_ndarray_repeat_impl.h
@@ -6,37 +6,48 @@
 #ifdef OWL_ENABLE_TEMPLATE
 
 
-void FUNCTION (c, repeat) (
-  TYPE *x, TYPE *y, int m, int n, int o,
-  int ofsx_m, int incx_m, int incx_n, int incy_m, int incy_n
-) {
-
-  // TODO
-
-}
-
-
 CAMLprim value FUNCTION (stub, repeat_native) (
-  value vX, value vY, value vM, value vN, value vO,
-  value vOFSX_M, value vINCX_M, value vINCX_N, value vINCY_M, value vINCY_N
+  value vX, value vY,
+  value vReps, value vBlock,
+  value vBlock_idx, value vSub_block_idx,
+  value vStride_sub, value vSlice_sub,
+  value vHighest_dim, value vBlock_num, value vSlice_sz
 ) {
 
   struct caml_ba_array *X = Caml_ba_array_val(vX);
-  TYPE *X_data = (TYPE *) X->data;
-
+  TYPE *x = (TYPE *) X->data;
   struct caml_ba_array *Y = Caml_ba_array_val(vY);
-  TYPE *Y_data = (TYPE *) Y->data;
+  TYPE *y = (TYPE *) Y->data;
 
-  int m = Long_val(vM);
-  int n = Long_val(vN);
-  int o = Long_val(vO);
-  int ofsx_m = Long_val(vOFSX_M);
-  int incx_m = Long_val(vINCX_M);
-  int incx_n = Long_val(vINCX_N);
-  int incy_m = Long_val(vINCY_M);
-  int incy_n = Long_val(vINCY_N);
+  int highest_dim = Long_val(vHighest_dim);
+  int block_num   = Long_val(vBlock_num);
+  int slice_sz    = Long_val(vSlice_sz);
 
-  FUNCTION (c, repeat) (X_data, Y_data, m, n, o, ofsx_m, incx_m, incx_n, incy_m, incy_n);
+  int rep_highest = Int_val(Field(vReps, highest_dim));
+
+  for (int i = 0; i < block_num; ++i) {
+    int ofsx = i * slice_sz;
+    int block_idx = Int_val(Field(vSub_block_idx, i));
+    for (int j = 0; j < rep_highest; ++j) {
+      int ofsy = block_idx + j;
+      COPYFUN(slice_sz, x, ofsx, 1, y, ofsy, rep_highest);
+    }
+  }
+
+  for (int d = highest_dim - 1; d >= 0; d--) {
+    int step = Int_val(Field(vStride_sub, d));
+    int block_sz = Int_val(Field(vBlock, d));
+    int repd = Int_val(Field(vReps, d));
+    int sliced = Int_val(Field(vSlice_sub, d));
+
+    for (int s = 0; s < sliced; s++) {
+      int ofsx = Int_val(Field(vSub_block_idx, s * step));
+      for (int j = 1; j < repd; j++) {
+        int ofsy = ofsx + j * block_sz;
+        COPYFUN(block_sz, y, ofsx, 1, y, ofsy, 1);
+      }
+    }
+  }
 
   return Val_unit;
 }
@@ -45,7 +56,7 @@ CAMLprim value FUNCTION (stub, repeat_native) (
 CAMLprim value FUNCTION (stub, repeat_byte) (value * argv, int argn) {
   return FUNCTION (stub, repeat_native) (
     argv[0], argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7],
-    argv[8], argv[9]
+    argv[8], argv[9], argv[10]
   );
 
   return Val_unit;
