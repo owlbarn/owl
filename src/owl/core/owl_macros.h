0cf09916c18bed5649fbe79351d28fe6a056621d
Liang Wang <ryanrhymes@gmail.com>
2018-06-28 12:34:20 +0300

    Update owl_macros.h
    details, details, be consistent with the style!

--- src/owl/core/owl_macros.h
+++ src/owl/core/owl_macros.h
@@ -25,6 +25,17 @@ typedef struct { float r, i; } complex_float;
 typedef struct { double r, i; } complex_double;
 
 
+// Define the stack structure
+
+typedef struct _BLOCK {
+  int h;    // header index of a block
+  int d;    // dimension
+  int ofsx; // corresponding offset of x
+  int tag;  // bool, whether this block still has unexplored children
+} BLOCK;
+
+
+
 // Define macros for interfacing to foreign code
 
 #define Array_length(v)        (Wosize_val(v))
@@ -80,14 +91,6 @@ typedef struct { double r, i; } complex_double;
 #else
   #define OWL_INLINE
 #endif
-
-// define structure of stack in repeat/tile operations
-typedef struct _BLOCK {
-  int h;    // header index of a block
-  int d;    // dimension
-  int ofsx; // corresponding offset of x
-  int tag;  // bool, whether this block still has unexplored children
-} BLOCK;
 
 
 #endif  /* OWL_MACROS_H */
