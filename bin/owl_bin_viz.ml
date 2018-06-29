fbf2852a087069015f480af8a82d82a7c5077b82
Liang Wang <ryanrhymes@gmail.com>
2018-06-29 15:05:58 +0300

    coding owlviz

--- bin/owl_bin_viz.ml
+++ bin/owl_bin_viz.ml
@@ -17,11 +17,19 @@ let print_help () =
 Owl's Computation Graph Visualiser
 
 Usage
-  owlviz -pdf [file]     generate a PDF file from a graph dump
-  owlviz -help           print out help information
+  owlviz -pdf [file]       generate the PDF figure from a graph dump
+  owlviz -dot [file]       generate the dot file from a graph dump
+  owlviz -trace [file]     print out trace of a graph dump on the terminal
+  owlviz -help             print out help information
   "
   in
   print_endline info
+
+
+let print_trace fname =
+  let graph_dump = Owl_io.marshal_from_file fname in
+  let trace_string = "print_trace: not implemented" in
+  print_endline trace_string
 
 
 let dump_dot_file fname =
@@ -39,12 +47,36 @@ let dump_pdf_file fname =
   Sys.command cmd_str |> ignore
 
 
-let _ =
-  if Array.length Sys.argv < 2 then
+let process_dumps fnames =
+  Array.iter (fun fname ->
+    Owl_log.info "processing %s ..." fname;
+    dump_pdf_file fname
+  ) fnames
+
+
+let main args =
+  if Array.length args < 2 then
+    (* nothing, so print out help info *)
     print_help ()
-  else if Sys.argv.(1) = "-help" then
-    print_help ()
-  else if Sys.argv.(1) = "-pdf" then
-    dump_pdf_file Sys.argv.(2)
-  else
-    Owl_log.error "cannot recognise the command."
+  else (
+    (* well-defined switches *)
+    if args.(1).[0] = '-' then (
+      if args.(1) = "-help" then
+        print_help ()
+      else if args.(1) = "-pdf" then
+        dump_pdf_file args.(2)
+      else if args.(1) = "-trace" then
+        print_trace args.(2)
+      else
+        Owl_log.error "cannot recognise the command."
+    )
+    else (
+      (* process a list of graph dumps *)
+      let num_files = Array.length args - 1 in
+      let file_lists = Array.sub args 1 num_files in
+      process_dumps file_lists
+    )
+  )
+
+
+let _ = main Sys.argv
