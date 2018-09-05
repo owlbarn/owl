e993196d58829c1336858d66db1dba702f46e164
Liang Wang <ryanrhymes@gmail.com>
2018-09-05 08:22:28 +0300

    Update owl_stats.mli

--- src/owl/stats/owl_stats.mli
+++ src/owl/stats/owl_stats.mli
@@ -1019,7 +1019,7 @@ val dirichlet_pdf : float array -> alpha:float array -> float
 val dirichlet_logpdf : float array -> alpha:float array -> float
 (** TODO *)
 
-
+val tukey_fences : ?k:float -> float array -> float * float
 (**
 ``tukey_fences ?k x`` returns a tuple of the lower and upper boundaries for
 values that are not outliers. ``k`` defaults to the standard coefficient of ``1.5``.
@@ -1028,7 +1028,6 @@ For first and third quartiles ``Q1`` and `Q3`, the range is computed as follows:
 .. math::
   (Q1 - k*(Q3-Q1), Q3 + k*(Q3-Q1))
 *)
-val tukey_fences : ?k:float -> float array -> float * float
 
 
 (* ends here *)
