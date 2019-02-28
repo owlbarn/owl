f57b691295138017f91abfb98337b1fc94598a76
Marcello Seri <marcello.seri@gmail.com>
2019-02-28 16:25:00 +0100

    owl-aeos: integrate in dune and enable only if ENABLE_OPENMP=1
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- src/aeos/config/configure.ml
+++ src/aeos/config/configure.ml
@@ -5,6 +5,12 @@
 
 module C = Configurator.V1
 
+let bgetenv v =
+  let v' = try Sys.getenv v |> int_of_string with Not_found -> 0 in
+  if v' < 0 || v' > 1 then raise @@
+    Invalid_argument (Printf.sprintf "Invalid value for env variable %s: got %d" v v');
+  v' = 1
+
 let get_os_type c =
   let sys = C.ocaml_config_var c "system" in
   match sys with Some s -> s | None -> ""
@@ -18,44 +24,50 @@ let get_default_cflags _c = [
 
 
 let get_openmp_cflags c =
-  match get_os_type c with
-  | "linux"     -> [ "-fopenmp" ]
-  | "linux_elf" -> [ "-fopenmp" ]
-  | "macosx"    -> [ "-Xpreprocessor"; "-fopenmp" ]
-  | "mingw64"   -> [ "-fopenmp" ]
-  | _           -> []
+  let enable_openmp = bgetenv "ENABLE_OPENMP" in
+  if not enable_openmp then []
+  else
+    match get_os_type c with
+    | "linux"     -> [ "-fopenmp" ]
+    | "linux_elf" -> [ "-fopenmp" ]
+    | "macosx"    -> [ "-Xpreprocessor"; "-fopenmp" ]
+    | "mingw64"   -> [ "-fopenmp" ]
+    | _           -> []
 
 
 let get_default_libs () = ["-lm";]
 
 
 let get_openmp_libs c =
-  match get_os_type c with
-  | "linux"     -> [ "-lgomp" ]
-  | "linux_elf" -> [ "-lgomp" ]
-  | "macosx"    -> [ "-lomp" ]
-  | "mingw64"   -> [ "-lgomp" ]
-  | _           -> []
+  let enable_openmp = bgetenv "ENABLE_OPENMP" in
+  if not enable_openmp then []
+  else
+    match get_os_type c with
+    | "linux"     -> [ "-lgomp" ]
+    | "linux_elf" -> [ "-lgomp" ]
+    | "macosx"    -> [ "-lomp" ]
+    | "mingw64"   -> [ "-lgomp" ]
+    | _           -> []
 
 
 let () =
   C.main ~name:"aeos" (fun c ->
 
-    (* configure link options *)
-    let libs = []
-      @ get_default_libs ()
-      @ get_openmp_libs c
-    in
+      (* configure link options *)
+      let libs = []
+                 @ get_default_libs ()
+                 @ get_openmp_libs c
+      in
 
-    (* configure compile options *)
-    let cflags = []
-      @ get_default_cflags c
-      @ get_openmp_cflags c
-    in
+      (* configure compile options *)
+      let cflags = []
+                   @ get_default_cflags c
+                   @ get_openmp_cflags c
+      in
 
-    (* assemble default config *)
-    let conf : C.Pkg_config.package_conf = { cflags; libs } in
+      (* assemble default config *)
+      let conf : C.Pkg_config.package_conf = { cflags; libs } in
 
-    C.Flags.write_sexp "aeos_c_flags.sexp" conf.cflags;
-    C.Flags.write_sexp "aeos_c_library_flags.sexp" conf.libs
-  )
+      C.Flags.write_sexp "aeos_c_flags.sexp" conf.cflags;
+      C.Flags.write_sexp "aeos_c_library_flags.sexp" conf.libs
+    )
