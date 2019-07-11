6933238b280e605a435888f9fbf72798acfce4c6
Pratap Singh <pratapsingh1729@gmail.com>
2019-07-11 13:07:40 +0530

    improve plot

--- owl-sublinear/plot_mem_acc_tradeoff.ml
+++ owl-sublinear/plot_mem_acc_tradeoff.ml
@@ -28,60 +28,88 @@ let read_data () =
       _ -> acc
     in Array.of_list (read_parse_line [])
   in
-  data
+  Owl.Mat.of_arrays data
 
-let data_to_mats data =
-  let memusage = Owl.Mat.init (Array.length data) 1 (fun i -> data.(i).(0)) in
-  let avgdiffs = Owl.Mat.init (Array.length data) 1 (fun i -> data.(i).(1)) in
-  let meddiffs = Owl.Mat.init (Array.length data) 1 (fun i -> data.(i).(2)) in
-  let l1_diffs = Owl.Mat.init (Array.length data) 1 (fun i -> data.(i).(3)) in
-  let phis     = Owl.Mat.init (Array.length data) 1 (fun i -> data.(i).(4)) in
-  memusage, avgdiffs, meddiffs, l1_diffs, phis
+let mat_to_vars data =
+  let memusage = Owl.Mat.col data 0 in
+  let avgdiffs = Owl.Mat.col data 1 in
+  let meddiffs = Owl.Mat.col data 2 in
+  let l1_diffs = Owl.Mat.col data 3 in
+  memusage, avgdiffs, meddiffs, l1_diffs
 
-let data_to_mats_nozero data =
-  let data' = data 
-              |> Array.to_list
-              |> List.filter (fun arr -> not (Array.mem 0. arr))
-              |> Array.of_list 
-              |> Array.map (Array.map log10)
-  in
-  let memusage = Owl.Mat.init (Array.length data') 1 (fun i -> data'.(i).(0)) in
-  let avgdiffs = Owl.Mat.init (Array.length data') 1 (fun i -> data'.(i).(1)) in
-  let meddiffs = Owl.Mat.init (Array.length data') 1 (fun i -> data'.(i).(2)) in
-  let l1_diffs = Owl.Mat.init (Array.length data') 1 (fun i -> data'.(i).(3)) in
-  let phis     = Owl.Mat.init (Array.length data') 1 (fun i -> data'.(i).(4)) in
-  memusage, avgdiffs, meddiffs, l1_diffs, phis
+let filter_nozero data =
+  Owl.Mat.rows data 
+    (Owl.Mat.filter_rows (Owl.Mat.not_exists (fun x -> x = 0.)) data)
 
-let _ =
+let make_plot data h title r g b =
   let open Owl_plplot.Plot in
-  let data = read_data () in
-  let memusage, avgdiffs, meddiffs, l1_diffs, phis = data_to_mats data in
-  let memusage', avgdiffs', meddiffs', l1_diffs', phis' = data_to_mats_nozero data in
-  let h = create ~m:4 ~n:3 "plots/tradeoff.pdf" in
+  let memusage, avgdiffs, meddiffs, l1_diffs = data |> mat_to_vars in
+  let memusage', avgdiffs', meddiffs', l1_diffs' = 
+    data |> filter_nozero |> Owl.Mat.map (Owl.Maths.log10) |> mat_to_vars in
   set_background_color h 255 255 255;
+  set_title h title;
   subplot h 0 0; set_foreground_color h 0 0 0;
   set_xlabel h "heap words";
   set_ylabel h "mean relative diff";
-  scatter ~h ~spec:[Marker "x"; MarkerSize 3.; RGB (255,0,0)] memusage avgdiffs;
+  scatter ~h ~spec:[Marker "x"; MarkerSize 3.; RGB (r,g,b)] memusage avgdiffs;
   subplot h 1 0; set_foreground_color h 0 0 0;
   set_xlabel h "log(heap words)";
   set_ylabel h "log(mean relative diff)";
-  scatter ~h ~spec:[Marker "x"; MarkerSize 3.; RGB (255,0,0)] memusage' avgdiffs';
+  scatter ~h ~spec:[Marker "x"; MarkerSize 3.; RGB (r,g,b)] memusage' avgdiffs';
   subplot h 0 1; set_foreground_color h 0 0 0;
   set_xlabel h "heap words";
   set_ylabel h "median relative diff";
-  scatter ~h ~spec:[Marker "x"; MarkerSize 3.; RGB (255,0,0)] memusage meddiffs;
+  scatter ~h ~spec:[Marker "x"; MarkerSize 3.; RGB (r,g,b)] memusage meddiffs;
   subplot h 1 1; set_foreground_color h 0 0 0;
   set_xlabel h "log(heap words)";
   set_ylabel h "log(median relative diff)";
-  scatter ~h ~spec:[Marker "x"; MarkerSize 3.; RGB (255,0,0)] memusage' meddiffs';
+  scatter ~h ~spec:[Marker "x"; MarkerSize 3.; RGB (r,g,b)] memusage' meddiffs';
   subplot h 0 2; set_foreground_color h 0 0 0;
   set_xlabel h "heap words";
   set_ylabel h "mean L1 diff";
-  scatter ~h~spec:[Marker "x"; MarkerSize 3.; RGB (255,0,0)] memusage l1_diffs;
+  scatter ~h~spec:[Marker "x"; MarkerSize 3.; RGB (r,g,b)] memusage l1_diffs;
   subplot h 1 2; set_foreground_color h 0 0 0;
   set_xlabel h "log(heap words)";
   set_ylabel h "log(mean L1 diff)";
-  scatter ~h ~spec:[Marker "x"; MarkerSize 3.; RGB (255,0,0)] memusage' l1_diffs';
-  output h
+  scatter ~h ~spec:[Marker "x"; MarkerSize 3.; RGB (r,g,b)] memusage' l1_diffs'
 
+
+let plot_tradeoffs () =
+  let open Owl_plplot.Plot in
+  let data = read_data () in
+  let h = Owl_plplot.Plot.create ~m:2 ~n:3 ("plots/tradeoff.pdf") in
+  make_plot data h "all" 255 0 0; output h
+
+let data_by_phis data phis =
+  let mapfn phi =
+    let ridxs = Owl.Mat.filter_rows (fun r -> Owl.Mat.get r 0 4 = phi) data in
+    phi, Owl.Mat.rows data ridxs
+  in
+  List.map mapfn phis
+
+let plot_by_phis phis rgbs =
+  let open Owl_plplot.Plot in
+  let data = data_by_phis (read_data ()) phis in
+  let h = create ~m:2 ~n:3 ("plots/tradeoff_by_phis.pdf") in
+  let iterfn i (phi, phidata) = 
+    make_plot phidata h "" rgbs.(i).(0) rgbs.(i).(1) rgbs.(i).(2)
+  in
+  List.iteri iterfn data; 
+  let legend_text = phis |> List.map (fun f -> "phi = " ^ (string_of_float f)) |> Array.of_list in
+  subplot h 0 2;
+  set_foreground_color h 0 0 0;
+  legend_on h ~position:NorthEast legend_text;
+  output h 
+
+let rgbs = 
+  [|[|255;  0;  0|];
+    [|  0;255;  0|];
+    [|  0;  0;255|];
+    [|255;  0;255|];
+    [|255;255;  0|];
+    [|  0;255;255|];
+    [|  0;  0;  0|]|] 
+
+let _ = plot_tradeoffs ()
+
+let _ = plot_by_phis [0.05; 0.01; 0.005; 0.001] rgbs
