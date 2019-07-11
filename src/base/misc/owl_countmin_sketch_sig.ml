2c93f0d06b1df1edb2b142d6569a3bc2a783c8d9
Pratap Singh <pratapsingh1729@gmail.com>
2019-07-11 15:47:25 +0530

    fixed incorrect comment in owl_countmin_sketch.ml

--- src/base/misc/owl_countmin_sketch_sig.ml
+++ src/base/misc/owl_countmin_sketch_sig.ml
@@ -2,8 +2,11 @@
  * Cormode and Muthukrishnan. The Count-Min sketch provides an approximate
  * frequency table whose space and time complexity are independent of the
  * number of elements inserted.  It provides the following guarantee:
- * With probability at lesat 1 - delta, the reported frequency of element
- * x will be within a factor (1 + epsilon) of the true frequency.
+ * - Estimated frequency >= True frequency
+ * - With probability at least 1 - delta:
+ *      Estimated frequency - True frequency < epsilon * L
+ * where L is the L1 norm of the data entered, equal to the total number of 
+ * times `incr` has been called.
  * Space usage will be O((1/epsilon) log (1/delta)) and the time to complete
  * `incr` or `count` will be O(log(1/delta)).
  * Refer to http://dimacs.rutgers.edu/~graham/pubs/papers/cm-full.pdf
