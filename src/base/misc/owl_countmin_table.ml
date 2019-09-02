58b77f9102dfd9366b5dee7e145cf5c0a8a6c41f
pratapsingh1729 <pratapsingh1729@gmail.com>
2019-09-02 19:58:08 -0400

    Parallel countmin sketch (#438)
    * first try of building - issues with the examples
    
    * have some examples working
    
    * Updated examples
    
    * update to include changes from review
    
    * fix typo in test_log.ml update
    
    * first implementation of countmin sketch
    
    * added test by comparison against hash table
    
    * added priority queue and module type for sketch
    
    * implemented heavy-hitters algorithm, not yet tested
    
    * adjusted API of heavy hitters sketch, added test for heavy hitters
    
    * fixed bug
    
    * small fixes
    
    * formatted file
    
    * moved modules into src/base/misc
    
    * improved owl_heavyhitters_sketch API
    
    * beginning of memory profiling
    
    * made all sketches polymorphic
    
    * added _sig.ml files and some documentation
    
    * finished profiling script
    
    * added example
    
    * start writing performance comparison
    
    * updated perf comparison script
    
    * added plotting code
    
    * measure performance against most frequent elements
    
    * improve plot
    
    * include plots and log
    
    * finish merging
    
    * update
    
    * add getrusage to countmin_memprof.ml
    
    * try more values of delta
    
    * add clone and merge functionality to owl_countmin_table.ml
    
    * implemented init_from and merge for countmin sketch
    
    * implemented countmin_distributed.ml example for distributed sketch
    
    * adjust plot code
    
    * cleanup

--- src/base/misc/owl_countmin_table.ml
+++ src/base/misc/owl_countmin_table.ml
@@ -1,16 +1,44 @@
 (* A module type for the tables used to implement the CountMin sketch. *)
 module type Sig = sig
-  (* the underlying table *)
+  
+
+  (** {6 Type definition} *)
+
   type t
+  (** The type of count-min tables *)
 
-  (* init l w generates a table with length l and width w, all counters initialized to 0 *)
+
+  (** {6 Core functions} *)
+
   val init : int -> int -> t
+  (** 
+  ``init l w`` generates a table with length ``l`` and width ``w``,
+  all counters initialized to 0.
+  *)
 
-  (* incr i j t increments the counter at length index i and width index j in table t *)
   val incr : int -> int -> t -> unit
+  (**
+  ``incr i j t`` increments the counter at length index ``i`` 
+  and width index ``j`` in table ``t``.
+  *)
 
-  (* get i j t gets the value of the counter at length index i and width index j in table t*)
   val get : int -> int -> t -> int
+  (**
+  ``get i j t`` gets the value of the counter at length index ``i`` 
+  and width index ``j`` in table ``t``.
+  *)
+
+  val clone : t -> t
+  (**
+  ``clone t`` returns a new table with the same contents as ``t``.
+  *)
+
+  val merge : t -> t -> t
+  (**
+  ``merge t1 t2`` merges tables ``t1`` and ``t2`` element-wise.  If ``t1`` and ``t2``
+  have the same dimensions, returns a new table whose elements are the sums of corresponding
+  elements from ``t1`` and ``t2``.  If dimensions do not match, raises ``INVALID_ARGUMENT``.
+  *)
 end
 
 (* Implementation of the CountMin sketch table using OCaml native arrays *)
@@ -20,6 +48,11 @@ module Native : Sig = struct
   let init l w = Array.make_matrix l w 0
   let incr i j t = t.(i).(j) <- t.(i).(j) + 1
   let get i j t = t.(i).(j)
+  let clone = Array.map (Array.map (fun x -> x))
+  let merge t1 t2 = 
+    try Array.map2 (Array.map2 (+)) t1 t2
+    with Invalid_argument _ -> 
+      raise (Owl_exception.INVALID_ARGUMENT "Cannot merge countmin tables of different dimensions")
 end
 
 (* Implementation of the CountMin sketch table using Owl ndarrays *)
@@ -29,4 +62,10 @@ module Owl : Sig = struct
   let init l w = Owl_base_dense_ndarray_s.zeros [| l; w |]
   let incr i j t = Owl_base_dense_ndarray_s.(set t [| i; j |] (get t [| i; j |] +. 1.))
   let get i j t = Owl_base_dense_ndarray_s.get t [| i; j |] |> int_of_float
+  let clone t = Owl_base_dense_ndarray_s.copy t
+  let merge t1 t2 = 
+    let open Owl_base_dense_ndarray_s in
+    if shape t1 = shape t2 then add t1 t2 
+    else 
+      raise (Owl_exception.INVALID_ARGUMENT "Cannot merge countmin tables of different dimensions")
 end
