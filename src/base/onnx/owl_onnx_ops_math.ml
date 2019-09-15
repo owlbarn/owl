c9ddd5aa9d2752c17ebf600c34e6c5be74316168
Liang Wang <liang.wang@nokia.com>
2019-09-15 19:02:49 +0300

    onnx: implement the specification

---
+++ src/base/onnx/owl_onnx_ops_math.ml
@@ -0,0 +1,56 @@
+(*
+ * OWL - OCaml Scientific and Engineering Computing
+ * Copyright (c) 2016-2019 Liang Wang <liang.wang@cl.cam.ac.uk>
+ *)
+
+
+module Add = struct
+
+  type t
+
+  let op_type = "Add"
+
+  let to_owl = raise Owl_exception.(NOT_IMPLEMENTED "onnx")
+
+  let of_owl = raise Owl_exception.(NOT_IMPLEMENTED "onnx")
+
+end
+
+
+module Sub = struct
+
+  type t
+
+  let op_type = "Sub"
+
+  let to_owl = raise Owl_exception.(NOT_IMPLEMENTED "onnx")
+
+  let of_owl = raise Owl_exception.(NOT_IMPLEMENTED "onnx")
+
+end
+
+
+module Mul = struct
+
+  type t
+
+  let op_type = "Mul"
+
+  let to_owl = raise Owl_exception.(NOT_IMPLEMENTED "onnx")
+
+  let of_owl = raise Owl_exception.(NOT_IMPLEMENTED "onnx")
+
+end
+
+
+module Div = struct
+
+  type t
+
+  let op_type = "Div"
+
+  let to_owl = raise Owl_exception.(NOT_IMPLEMENTED "onnx")
+
+  let of_owl = raise Owl_exception.(NOT_IMPLEMENTED "onnx")
+
+end
