0922abaea2ea407e5639ee4d250863807e9d4a60
tachukao <tachu.kao@gmail.com>
2019-12-12 00:51:36 +0000

    forgot .mli file

---
+++ src/base/linalg/owl_base_linalg_d.mli
@@ -0,0 +1,23 @@
+(*
+ * OWL - OCaml Scientific and Engineering Computing
+ * Copyright (c) 2016-2019 Liang Wang <liang.wang@cl.cam.ac.uk>
+ *)
+
+open Bigarray
+
+type elt = float
+
+type mat = Owl_base_dense_matrix_d.mat
+
+type complex_mat = Owl_base_dense_matrix_z.mat
+
+type int32_mat = (int32, int32_elt) Owl_base_dense_matrix_generic.t
+
+include
+  Owl_base_linalg_intf.Common
+    with type elt := elt
+     and type mat := mat
+     and type complex_mat := complex_mat
+     and type int32_mat := int32_mat
+
+include Owl_base_linalg_intf.Real with type elt := elt and type mat := mat
