9d0237d7fa20fecc0d40665152b938d8e3bec56c
Liang Wang <liang.wang@nokia.com>
2019-12-27 00:28:48 +0200

    apply ocamlformat to base/*

--- src/base/owl_base.ml
+++ src/base/owl_base.ml
@@ -3,48 +3,20 @@
  * Copyright (c) 2016-2020 Liang Wang <liang.wang@cl.cam.ac.uk>
  *)
 
-
 module Const = Owl_const
-
-
 module Maths = Owl_base_maths
-
-
 module Stats = Owl_base_stats
-
-
 module Complex = Owl_base_complex
-
-
 module Quadrature = Owl_maths_quadrature
-
-
 module Root = Owl_maths_root
-
-
 module Graph = Owl_graph
-
-
 module Lazy = Owl_lazy
-
-
 module View = Owl_view
-
-
 module Log = Owl_log
-
-
 module Utils = Owl_utils
-
-
 module Computation = Owl_computation
-
-
 module Countmin_sketch = Owl_countmin_sketch
-
-
 module HeavyHitters_sketch = Owl_heavyhitters_sketch
-
 
 (* initialise base library *)
 let _ = Owl_base_stats_prng.self_init ()
