1b77a03565e17caa114ce5a26d41d800a49e0285
Liang Wang <liang.wang@nokia.com>
2019-12-28 20:55:59 +0200

    add missing header

--- src/owl/bindings/owl_lapacke_parser.ml
+++ src/owl/bindings/owl_lapacke_parser.ml
@@ -1,3 +1,8 @@
+(*
+ * OWL - OCaml Scientific and Engineering Computing
+ * Copyright (c) 2016-2020 Liang Wang <liang.wang@cl.cam.ac.uk>
+ *)
+
 let copyright =
   "(*\n\
   \ * OWL - OCaml Scientific and Engineering Computing\n\
