39d90ed692832457cd611b6e0c4710a3f21b012d
jzstark <rho.ajax@gmail.com>
2020-11-17 23:26:09 +0800

    adding fft2 and ifft2

--- src/owl/fftpack/owl_fft_generic.mli
+++ src/owl/fftpack/owl_fft_generic.mli
@@ -39,3 +39,12 @@ val irfft
 ``irfft ~axis ~n x`` is the inverse function of ``rfft``. Note the ``n`` parameter
 is used to specified the size of output.
  *)
+
+val fft2 : (Complex.t, 'a) t -> (Complex.t, 'a) t
+(**
+``fft2 x`` performs 2-dimensional FFT on a complex input. The return is not scaled.
+ *)
+
+val ifft2 : (Complex.t, 'a) t -> (Complex.t, 'a) t
+(**
+``ifft2 x`` performs inverse 2-dimensional FFT on a complex input. *)
