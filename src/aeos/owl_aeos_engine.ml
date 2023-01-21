0e113dd680ccf171a7ed0cd54896d87faa24b09b
jzstark <rho.ajax@gmail.com>
2023-01-21 22:24:59 +0800

    remove aeos

--- src/aeos/owl_aeos_engine.ml
+++
@@ -1,6 +0,0 @@
-(*
- * OWL - OCaml Scientific Computing
- * Copyright (c) 2016-2022 Liang Wang <liang@ocaml.xyz>
- *)
-
-let eval tuners = Array.iter Owl_aeos_tuners.tuning tuners
--- src/aeos/owl_aeos_types.ml
+++
@@ -1,8 +0,0 @@
-(*
- * OWL - OCaml Scientific Computing
- * Copyright (c) 2016-2022 Liang Wang <liang@ocaml.xyz>
- *)
-
-open Bigarray
-
-type ('a, 'b) owl_arr = ('a, 'b, c_layout) Genarray.t
